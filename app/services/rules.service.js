5b0f1cb97db25a8ef42be4d5a5cbab917d9b1775
Stuart Adair <stu@silverka.co.uk>
2020-10-14 16:36:59 +0100

    Rearrange rules service functions into order of use

--- app/services/rules.service.js
+++ app/services/rules.service.js
@@ -24,18 +24,18 @@ class RulesService {
     return Got.post(path, options)
   }
 
-  // Year suffix is '_2000_01' for 2000, '_2001_02' for 2001 etc.
-  // ie. the year and the last two digits of the following year
-  static _yearSuffix (year) {
-    const nextYearDigits = (year + 1).toString().slice(2)
-    return `_${year}_${nextYearDigits}`
-  }
-
   // generate the path for the specified regime, year and ruleset
   static _makeRulesPath (regime, year) {
     const { application, ruleset } = RulesServiceConfig.endpoints[regime]
     const suffix = this._yearSuffix(year)
     return `${application}/${ruleset}${suffix}`
+  }
+
+  // Year suffix is '_2000_01' for 2000, '_2001_02' for 2001 etc.
+  // ie. the year and the last two digits of the following year
+  static _yearSuffix (year) {
+    const nextYearDigits = (year + 1).toString().slice(2)
+    return `_${year}_${nextYearDigits}`
   }
 }
 
