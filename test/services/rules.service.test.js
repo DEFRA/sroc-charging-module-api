733c05198370edb7a40816a53e38096c3b63ea66
Stuart Adair <stu@silverka.co.uk>
2020-10-15 11:16:37 +0100

    Additional tests for year/regime urls

--- test/services/rules.service.test.js
+++ test/services/rules.service.test.js
@@ -1,22 +1,50 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, afterEach, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
-const nock = require('nock')
+const Nock = require('nock')
 
 const RulesService = require('../../app/services/rules.service')
 const RulesServiceHelper = require('../support/helpers/rules.service.helper')
 
 describe('Rules service', () => {
-  it('calls the service', async () => {
-    const WRLS = RulesServiceHelper.allRulesData('wrls')
+  afterEach(async () => {
+    Nock.cleanAll()
+  })
 
-    nock(RulesServiceHelper.url, { encodedQueryParams: true })
-      .post(`/${WRLS.application}/${WRLS.ruleset}_2020_21`, WRLS.request)
-      .reply(200, WRLS.response)
+  it('calls the rules service API', async () => {
+    const wrls = RulesServiceHelper.allRulesData('wrls')
 
-    const result = await RulesService.call('wrls', 2020, WRLS.request)
+    Nock(RulesServiceHelper.url, { encodedQueryParams: true })
+      .post(`/${wrls.application}/${wrls.ruleset}_2020_21`, wrls.request)
+      .reply(200, wrls.response)
 
-    expect(result.body).to.equal(WRLS.response)
+    const result = await RulesService.call('wrls', 2020, wrls.request)
+
+    expect(result.body).to.equal(wrls.response)
+  })
+
+  describe('calls different endpoints', () => {
+    beforeEach(async () => {
+      // Intercept all requests in this test suite as we just want to capture the urls
+      Nock(RulesServiceHelper.url, { encodedQueryParams: true })
+        .post(() => true)
+        .reply(200)
+        .persist()
+    })
+
+    it('for different regimes', async () => {
+      const { requestUrl: requestUrlWrls } = await RulesService.call('wrls', 2020, {})
+      const { requestUrl: requestUrlCfd } = await RulesService.call('cfd', 2020, {})
+
+      expect(requestUrlWrls).to.not.equal(requestUrlCfd)
+    })
+
+    it('for different years', async () => {
+      const { requestUrl: requestUrl2019 } = await RulesService.call('wrls', 2019, {})
+      const { requestUrl: requestUrl2020 } = await RulesService.call('wrls', 2020, {})
+
+      expect(requestUrl2019).to.not.equal(requestUrl2020)
+    })
   })
 })
