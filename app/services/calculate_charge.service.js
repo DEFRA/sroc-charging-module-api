2becb59bd6f6bae03b2df4c79be8438de029315d
Stuart Adair <stu@silverka.co.uk>
2020-10-16 14:16:15 +0100

    Create Charge Service

---
+++ app/services/calculate_charge.service.js
@@ -0,0 +1,12 @@
+const RulesService = require('./rules.service')
+
+// This service passes a translator object to the rules service
+// The response from the rules service is used to create a new presenter which is returned
+class CalculateChargeService {
+  static async call (translator, Presenter) {
+    const response = await RulesService.call(translator)
+    return new Presenter(response)
+  }
+}
+
+module.exports = CalculateChargeService
