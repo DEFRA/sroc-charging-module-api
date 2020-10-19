6a2858ceb7b1588e0749bffc1293b44ab3b677fa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-19 23:06:49 +0100

    Add index.js to the test helpers
    Gone a bit crazy for these with this change. But again, adding an `index.js` means we can require the helpers in one line and use destructuring to actually grab the helpers we need.

---
+++ test/support/helpers/index.js
@@ -0,0 +1,9 @@
+const AuthorisationHelper = require('./authorisation.helper')
+const DatabaseHelper = require('./database.helper')
+const RulesServiceHelper = require('./rules_service.helper')
+
+module.exports = {
+  AuthorisationHelper,
+  DatabaseHelper,
+  RulesServiceHelper
+}
