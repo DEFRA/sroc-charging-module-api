72c7b2298c1575f9564162624f5c367876cdb7c2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-22 21:31:51 +0100

    Add authentication feature test
    First, the use of the term `feature`. Authentication isn't something specific to a plugin, hook, model etc. It's a 'feature' of the service itself. As such tests for this generic features if the service need a home and 'features' seems appropriate. For example its a term used as part of BDD and tools like Cucumber.
    
    This adds our first authentication feature test which aims to cover all the aspects of authenticating with the API including expired JWTs, incorrectly signed JWTs, and public and non-public paths.
    
    N.B. We don't currently have anything but admin and public paths. We'll cover non-admin/non-public paths when we finally implement one!

---
+++ test/features/authentication.test.js
@@ -0,0 +1,134 @@
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, before, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// For running our service
+const { deployment } = require('../../server')
+
+// Test helpers
+const { AuthorisationHelper, ClientHelper, DatabaseHelper, RouteHelper } = require('../support/helpers')
+
+// Things we need to stub
+const JsonWebToken = require('jsonwebtoken')
+
+describe('Authenticating with the API', () => {
+  let server
+  let authToken
+
+  describe('When accessing a public route', () => {
+    before(async () => {
+      server = await deployment()
+      RouteHelper.addPublicRoute(server)
+    })
+
+    it('returns a success response', async () => {
+      const options = {
+        method: 'GET',
+        url: '/test/public'
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).to.equal(200)
+    })
+  })
+
+  describe('When accessing an /admin only route', () => {
+    before(async () => {
+      await DatabaseHelper.clean()
+      await ClientHelper.addAdminClient()
+
+      server = await deployment()
+      RouteHelper.addAdminRoute(server)
+    })
+
+    afterEach(async () => {
+      Sinon.restore()
+    })
+
+    describe('but I do not have a bearer token', () => {
+      it('returns a 401 error', async () => {
+        const options = {
+          method: 'GET',
+          url: '/test/admin'
+        }
+
+        const response = await server.inject(options)
+
+        expect(response.statusCode).to.equal(401)
+      })
+    })
+
+    describe('and I have a valid bearer token', () => {
+      before(async () => {
+        authToken = AuthorisationHelper.adminToken()
+
+        Sinon
+          .stub(JsonWebToken, 'verify')
+          .returns(AuthorisationHelper.decodeToken(authToken))
+      })
+
+      it('returns a success response', async () => {
+        const options = {
+          method: 'GET',
+          url: '/test/admin',
+          headers: { authorization: `Bearer ${authToken}` }
+        }
+
+        const response = await server.inject(options)
+
+        expect(response.statusCode).to.equal(200)
+      })
+    })
+
+    describe('but I have an invalid bearer token', () => {
+      before(async () => {
+        authToken = AuthorisationHelper.adminToken()
+      })
+
+      describe('because it has expired', () => {
+        before(async () => {
+          Sinon
+            .stub(JsonWebToken, 'verify')
+            .throws(AuthorisationHelper.tokenExpiredError())
+        })
+
+        it('returns a 401 error', async () => {
+          const options = {
+            method: 'GET',
+            url: '/test/admin',
+            headers: { authorization: `Bearer ${authToken}` }
+          }
+
+          const response = await server.inject(options)
+
+          expect(response.statusCode).to.equal(401)
+        })
+      })
+
+      describe('because it was not signed by our AWS Cognito instance', () => {
+        before(async () => {
+          Sinon
+            .stub(JsonWebToken, 'verify')
+            .throws(AuthorisationHelper.tokenInvalidSignatureError())
+        })
+
+        it('returns a 401 error', async () => {
+          const options = {
+            method: 'GET',
+            url: '/test/admin',
+            headers: { authorization: `Bearer ${authToken}` }
+          }
+
+          const response = await server.inject(options)
+
+          expect(response.statusCode).to.equal(401)
+        })
+      })
+    })
+  })
+})
