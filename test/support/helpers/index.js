ce7096c5f03179be850ff5fb8444021c3e3fdb44
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-22 21:30:41 +0100

    Update the helpers index

--- test/support/helpers/index.js
+++ test/support/helpers/index.js
@@ -1,9 +1,13 @@
 const AuthorisationHelper = require('./authorisation.helper')
+const ClientHelper = require('./client.helper')
 const DatabaseHelper = require('./database.helper')
+const RouteHelper = require('./route.helper')
 const RulesServiceHelper = require('./rules_service.helper')
 
 module.exports = {
   AuthorisationHelper,
+  ClientHelper,
   DatabaseHelper,
+  RouteHelper,
   RulesServiceHelper
 }
