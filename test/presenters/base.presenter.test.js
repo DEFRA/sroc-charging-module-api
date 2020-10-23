fb69d7a8fc0f00436f9cd6c8897f3d6fcf4e95ce
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-10-23 14:59:54 +0100

    Remove only() tag - oops!

--- test/presenters/base.presenter.test.js
+++ test/presenters/base.presenter.test.js
@@ -11,7 +11,7 @@ const { expect } = Code
 // Thing under test
 const { BasePresenter } = require('../../app/presenters')
 
-describe.only('Base presenter', () => {
+describe('Base presenter', () => {
   afterEach(async () => {
     Sinon.restore()
   })
