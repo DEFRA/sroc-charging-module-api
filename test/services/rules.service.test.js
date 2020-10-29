e1d85b2b36a6705e3d898ced0d330b4135556a33
Stuart Adair <stu@silverka.co.uk>
2020-10-29 09:39:53 +0000

    Update calculate charge service based on recent discussion

--- test/services/rules.service.test.js
+++ test/services/rules.service.test.js
@@ -14,8 +14,8 @@ const { RulesServiceHelper } = require('../support/helpers')
 // Thing under test
 const { RulesService } = require('../../app/services')
 
-// Wraps regime, financial year and charge params in a dummy translator object for passing to rules service
-const dummyTranslator = (regime, financialYear, chargeParams = {}) => ({ regime, financialYear, chargeParams })
+// Wraps regime, financial year and charge params in a dummy presenter object for passing to rules service
+const dummyPresenter = (regime, financialYear, chargeParams = {}) => ({ regime, financialYear, chargeParams })
 
 describe('Rules service', () => {
   afterEach(async () => {
@@ -27,9 +27,9 @@ describe('Rules service', () => {
     Nock(RulesServiceHelper.url)
       .post(`/${wrls.application}/${wrls.ruleset}_2020_21`, wrls.request)
       .reply(200, wrls.response)
-    const translator = dummyTranslator('wrls', 2020, wrls.request)
+    const presenter = dummyPresenter('wrls', 2020, wrls.request)
 
-    const result = await RulesService.call(translator)
+    const result = await RulesService.call(presenter)
 
     expect(result.body).to.equal(wrls.response)
   })
@@ -44,21 +44,21 @@ describe('Rules service', () => {
     })
 
     it('for different regimes', async () => {
-      const translatorWrls = dummyTranslator('wrls', 2020)
-      const translatorCfd = dummyTranslator('cfd', 2020)
+      const presenterWrls = dummyPresenter('wrls', 2020)
+      const presenterCfd = dummyPresenter('cfd', 2020)
 
-      const { requestUrl: requestUrlWrls } = await RulesService.call(translatorWrls)
-      const { requestUrl: requestUrlCfd } = await RulesService.call(translatorCfd)
+      const { requestUrl: requestUrlWrls } = await RulesService.call(presenterWrls)
+      const { requestUrl: requestUrlCfd } = await RulesService.call(presenterCfd)
 
       expect(requestUrlWrls).to.not.equal(requestUrlCfd)
     })
 
     it('for different years', async () => {
-      const translator2019 = dummyTranslator('wrls', 2019)
-      const translator2020 = dummyTranslator('wrls', 2020)
+      const presenter2019 = dummyPresenter('wrls', 2019)
+      const presenter2020 = dummyPresenter('wrls', 2020)
 
-      const { requestUrl: requestUrl2019 } = await RulesService.call(translator2019)
-      const { requestUrl: requestUrl2020 } = await RulesService.call(translator2020)
+      const { requestUrl: requestUrl2019 } = await RulesService.call(presenter2019)
+      const { requestUrl: requestUrl2020 } = await RulesService.call(presenter2020)
 
       expect(requestUrl2019).to.not.equal(requestUrl2020)
     })
