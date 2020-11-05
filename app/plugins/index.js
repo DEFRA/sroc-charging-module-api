758974e87fc06cc87f036cfb692b9d94c70d8f8a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-05 13:38:22 +0000

    Add plugin and integrate with service

--- app/plugins/index.js
+++ app/plugins/index.js
@@ -6,6 +6,7 @@ const DisinfectPlugin = require('./disinfect.plugin')
 const HpalDebugPlugin = require('./hpal_debug.plugin')
 const HapiNowAuthPlugin = require('./hapi_now_auth.plugin')
 const HapiPinoPlugin = require('./hapi_pino.plugin')
+const InvalidCharactersPlugin = require('./invalid_characters.plugin')
 const RouterPlugin = require('./router.plugin')
 const UnescapePlugin = require('./unescape.plugin')
 
@@ -16,6 +17,7 @@ module.exports = {
   HpalDebugPlugin,
   HapiNowAuthPlugin,
   HapiPinoPlugin,
+  InvalidCharactersPlugin,
   RouterPlugin,
   UnescapePlugin
 }
---
+++ app/plugins/invalid_characters.plugin.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Boom = require('@hapi/boom')
+
+/**
+ * Converts an object to a JSON string and then tests if it contains any of the following characters: ? £ ≤ ≥
+ *
+ * @param {Object} obj The object you wish to check for invalid characters
+ * @returns {boolean} `true` if the object contains an invalid character, else `false`
+ */
+const invalidCharacters = obj => {
+  const jsonObj = JSON.stringify(obj)
+
+  if (jsonObj.match(/[?£\u2014\u2264\u2265]/)) {
+    return true
+  }
+
+  return false
+}
+
+const InvalidCharactersPlugin = {
+  name: 'invalid_characters',
+  register: (server, _options) => {
+    server.ext('onPostAuth', (request, h) => {
+      if (!request.payload) {
+        return h.continue
+      }
+
+      if (invalidCharacters(request.payload)) {
+        throw Boom.badData('We cannot accept any request that contains the following characters: ? £ ≤ ≥')
+      }
+
+      return h.continue
+    })
+  }
+}
+
+module.exports = InvalidCharactersPlugin
