1af844ce0e683c109949b36616b0381588674a2d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-06 16:27:44 +0000

    Expand error message
    Why say something in 2 words when you can say it in 11!

--- app/hooks/on_request.hook.js
+++ app/hooks/on_request.hook.js
@@ -5,7 +5,7 @@ const Boom = require('@hapi/boom')
 const onRequest = (req, h) => {
   if (req.method === 'post' && !req.payload) {
     // return HTTP 400
-    return Boom.badRequest('No payload')
+    return Boom.badRequest('The request is invalid because it does not contain a payload.')
   }
 
   return h.continue
