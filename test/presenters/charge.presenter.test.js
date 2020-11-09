ed19bdf8f5543d1423652101c3c5ae8710e28040
Stuart Adair <stu@silverka.co.uk>
2020-11-09 16:52:48 +0000

    Remove unused Sinon from charge.presenter.test

--- test/presenters/charge.presenter.test.js
+++ test/presenters/charge.presenter.test.js
@@ -3,19 +3,14 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
-const Sinon = require('sinon')
 
-const { describe, it, afterEach } = exports.lab = Lab.script()
+const { describe, it } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
 const { ChargePresenter } = require('../../app/presenters')
 
 describe('Charge presenter', () => {
-  afterEach(async () => {
-    Sinon.restore()
-  })
-
   describe('returns chargeValue', () => {
     it('negative value when chargeCredit is true', async () => {
       const testData = { chargeValue: 100, chargeCredit: true }
