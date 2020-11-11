ae3deb25c59298749f452721ffb31aefd36e50d6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-11 16:35:49 +0000

    Add custom plugin to handle 'cleaning' payloads (#40)
    We're starting to [add validation](https://github.com/DEFRA/sroc-charging-module-api/pull/34) with gusto. But a common occurrence is to `trim()` values prior to applying validation.
    
    ```javascript
    chargeCategoryCode: Joi.string().trim().required(),
    source: Joi.string().trim().required()
    ```
    
    This change adds a new custom plugin which hooks into the [Hapi lifecycle](https://futurestud.io/downloads/hapi/request-lifecycle) and automatically cleans payloads before we have to deal with them.
    
    By cleaning we mean
    
    - remove any null or empty properties from the payload
    - remove any whitespace at the start and end of String values
    
    **Change of intention**
    
    This started out intending to use [Disinfect](https://github.com/genediazjr/disinfect) to do this for us. We added Disinfect to block malicious content. However, Disinfect also has the ability to automatically remove empty payload keys as well as removing any whitespace.
    
    However, we found that
    
    - the whitespace removal is for properties that are completely whitespace. It doesn't trim values
    - both it and the `deleteEmpty` functions only work on top-level properties. Nested objects were not catered for.

---
+++ test/features/clean_payloads.test.js
@@ -0,0 +1,126 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before } = exports.lab = Lab.script()
+const { expect } = Code
+
+// For running our service
+const { deployment } = require('../../server')
+
+// Test helpers
+const { RouteHelper } = require('../support/helpers')
+
+const options = payload => {
+  return {
+    method: 'POST',
+    url: '/test/post',
+    payload: payload
+  }
+}
+
+describe('Cleaning data in requests', () => {
+  let server
+
+  // Create server before each test
+  before(async () => {
+    server = await deployment()
+    RouteHelper.addPublicPostRoute(server)
+  })
+
+  describe('When a POST request contains properties that are only whitespace', () => {
+    it('removes them', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001',
+        customerName: ' '
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload).to.not.contain('customerName')
+    })
+
+    it('removes them from nested objects', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001',
+        details: {
+          firstName: 'Bert',
+          lastName: ' '
+        }
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload.details).to.not.contain('lastName')
+    })
+  })
+
+  describe('When a POST request contains properties that are empty', () => {
+    it('removes them', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001',
+        customerName: ''
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload).to.not.contain('customerName')
+    })
+
+    it('removes them from nested objects', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001',
+        details: {
+          firstName: 'Bert',
+          lastName: ''
+        }
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload.details).to.not.contain('lastName')
+    })
+  })
+
+  describe('When a POST request contains properties containing extra whitespace', () => {
+    it('removes it', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001 ',
+        customerName: ' Bert & Ernie Ltd '
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload.reference).to.equal('BESESAME001')
+      expect(responsePayload.customerName).to.equal('Bert & Ernie Ltd')
+    })
+
+    it('removes it from nested objects', async () => {
+      const requestPayload = {
+        reference: 'BESESAME001',
+        details: {
+          firstName: 'Bert',
+          lastName: ' Ernie '
+        }
+      }
+
+      const response = await server.inject(options(requestPayload))
+      const responsePayload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(responsePayload.details.lastName).to.equal('Ernie')
+    })
+  })
+})
