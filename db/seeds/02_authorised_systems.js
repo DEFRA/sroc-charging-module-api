5ed1dff64d25b27c9d34e8cf6a25058e0a61d8ac
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-17 12:10:36 +0000

    Update seed files to use async / await pattern (#51)
    This is the pattern we now use in our code rather than direct Promises. So should make it a little easier to understand and more consistent with the rest of our other code.

--- db/seeds/02_authorised_systems.js
+++ db/seeds/02_authorised_systems.js
@@ -2,18 +2,15 @@
 
 const config = require('../../config/authentication.config')
 
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('authorised_systems').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('authorised_systems').insert([
-        {
-          client_id: config.adminClientId,
-          name: 'admin',
-          admin: true,
-          status: 'active'
-        }
-      ])
-    })
+exports.seed = async function (knex) {
+  await knex('authorised_systems').del()
+
+  await knex('authorised_systems').insert(
+    {
+      client_id: config.adminClientId,
+      name: 'admin',
+      admin: true,
+      status: 'active'
+    }
+  )
 }
