b32e8a7bf1bf3199de6556ff0de785aa88f60901
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-17 10:57:02 +0000

    Update clean payloads feature test
    As we now have the ObjectCleanerService tests we don't need to low level detail tests against the API. But we still want to get a sense of what the plugin and the API is doing with a request.
    
    Hence we have updated it to this one new test that covers all the cleaning operations.

--- test/features/clean_payloads.test.js
+++ test/features/clean_payloads.test.js
@@ -30,26 +30,45 @@ describe('Cleaning data in requests', () => {
     RouteHelper.addPublicPostRoute(server)
   })
 
-  describe('When a POST request contains properties that are only whitespace', () => {
-    it('removes them', async () => {
+  describe('When a POST request contains a mess!', () => {
+    it('cleans it up', async () => {
       const requestPayload = {
-        reference: 'BESESAME001',
-        customerName: ' '
+        reference: '  BESESAME001  ',
+        codes: ['AB1', ' BD2', '', '  ', 'CD3  '],
+        description: '',
+        preferences: [true, false, true],
+        details: {
+          active: false,
+          orders: [
+            {
+              id: '123',
+              pickers: [],
+              orderDate: '2012-04-23T18:25:43.511Z',
+              lines: [
+                { pos: 1, picked: true, item: ' widget ' },
+                { pos: 2, picked: false, item: ' widget ' }
+              ]
+            }
+          ]
+        }
       }
 
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload).to.not.contain('customerName')
-    })
-
-    it('removes them from nested objects', async () => {
-      const requestPayload = {
+      const expectedResponse = {
         reference: 'BESESAME001',
+        codes: ['AB1', 'BD2', 'CD3'],
+        preferences: [true, false, true],
         details: {
-          firstName: 'Bert',
-          lastName: ' '
+          active: false,
+          orders: [
+            {
+              id: '123',
+              orderDate: '2012-04-23T18:25:43.511Z',
+              lines: [
+                { pos: 1, picked: true, item: 'widget' },
+                { pos: 2, picked: false, item: 'widget' }
+              ]
+            }
+          ]
         }
       }
 
@@ -57,179 +76,7 @@ describe('Cleaning data in requests', () => {
       const responsePayload = JSON.parse(response.payload)
 
       expect(response.statusCode).to.equal(200)
-      expect(responsePayload.details).to.not.contain('lastName')
-    })
-
-    it('removes them from arrays', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        codes: ['whoop', '  ', '  ', 'is']
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.codes.length).to.equal(2)
-      expect(responsePayload.codes[0]).to.equal('whoop')
-      expect(responsePayload.codes[1]).to.equal('is')
-    })
-
-    it('removes them from the objects in arrays', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        customers: [
-          { name: 'Big bird' },
-          { name: '  ' },
-          { name: 'Elmo' }
-        ]
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.customers.length).to.equal(2)
-      expect(responsePayload.customers[0].name).to.equal('Big bird')
-      expect(responsePayload.customers[1].name).to.equal('Elmo')
-    })
-  })
-
-  describe('When a POST request contains properties that are empty', () => {
-    it('removes them', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        customerName: ''
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload).to.not.contain('customerName')
-    })
-
-    it('removes them from nested objects', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        details: {
-          firstName: 'Bert',
-          lastName: ''
-        }
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.details).to.not.contain('lastName')
-    })
-
-    it('removes them from arrays', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        codes: ['whoop', '', '', 'is']
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.codes.length).to.equal(2)
-      expect(responsePayload.codes[0]).to.equal('whoop')
-      expect(responsePayload.codes[1]).to.equal('is')
-    })
-  })
-
-  describe('When a POST request contains properties containing extra whitespace', () => {
-    it('removes it', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001 ',
-        customerName: ' Bert & Ernie Ltd '
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.reference).to.equal('BESESAME001')
-      expect(responsePayload.customerName).to.equal('Bert & Ernie Ltd')
-    })
-
-    it('removes it from nested objects', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        details: {
-          firstName: 'Bert',
-          lastName: ' Ernie '
-        }
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.details.lastName).to.equal('Ernie')
-    })
-
-    it('removes it from arrays', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        codes: ['whoop', 'there ', ' it', 'is']
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.codes[1]).to.equal('there')
-      expect(responsePayload.codes[2]).to.equal('it')
-    })
-  })
-
-  describe('When a POST request contains boolean properties that are false', () => {
-    it('keeps them', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        customerName: 'Bert & Ernie Ltd',
-        newCustomer: false
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload).to.contain('newCustomer')
-    })
-
-    it('keeps them in nested objects', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        details: {
-          firstName: 'Bert',
-          lastName: 'Ernie',
-          newCustomer: false
-        }
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload.details).to.contain('newCustomer')
-    })
-
-    it('keeps them in arrays', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        preferences: [true, false, true]
-      }
-
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
-
-      expect(response.statusCode).to.equal(200)
-      expect(responsePayload).to.equal(requestPayload)
+      expect(responsePayload).to.equal(expectedResponse)
     })
   })
 })
