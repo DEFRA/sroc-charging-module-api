ce097f7a4ec477c736f4c7bb33c9fb46f9993bee
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-18 20:11:51 +0000

    Remove try/catch from controller
    After an [investigation into how Hapi handles errors](https://github.com/DEFRA/sroc-charging-module-api/pull/56) we now understand that any error thrown during the context of a request+response if left unhandled will automatically be picked up by Hapi and converted to a `Boom.internal()` (a 500). So we don't need to include code which catches errors and converts them to Boom ones ourselves.
    
    This means we can remove the try/catches in our controllers!

--- app/controllers/admin/authorised_systems.controller.js
+++ app/controllers/admin/authorised_systems.controller.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const Boom = require('@hapi/boom')
 const { AuthorisedSystemModel } = require('../../models')
 const { CreateAuthorisedSystemService } = require('../../services')
 
@@ -18,19 +17,8 @@ class AuthorisedSystemController {
   }
 
   static async create (req, h) {
-    try {
-      const result = await CreateAuthorisedSystemService.call(req.payload)
-      return h.response(result).code(201)
-    } catch (error) {
-      // TODO: Think we can drop try/catch altogether and use a global hook to manage our errors and ensure they are
-      // boomed!
-      // https://medium.com/@andv/hapi-transforming-an-internal-server-error-occured-into-correct-boom-errors-1a2a72e6ffff
-      if (Boom.isBoom(error)) {
-        throw error
-      } else {
-        throw Boom.boomify(error)
-      }
-    }
+    const result = await CreateAuthorisedSystemService.call(req.payload)
+    return h.response(result).code(201)
   }
 }
 
