9eaa49087dcc7608d65abc30044c7ede15bedf11
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-18 10:39:14 +0000

    Update auth. system & regime with many-to-many rel
    This change tells Objection about the relationship between authorised system and regime (and we know it works from our efforts in https://github.com/DEFRA/sroc-charging-module-api/pull/48 !)

--- app/models/regime.model.js
+++ app/models/regime.model.js
@@ -6,6 +6,24 @@ class RegimeModel extends BaseModel {
   static get tableName () {
     return 'regimes'
   }
+
+  static get relationMappings () {
+    return {
+      authorisedSystems: {
+        relation: Model.ManyToManyRelation,
+        modelClass: 'authorised_system.model',
+        join: {
+          from: 'regimes.id',
+          through: {
+            // authorised_systems_regimes is a join table
+            from: 'authorised_systems_regimes.regime_id',
+            to: 'authorised_systems_regimes.authorised_system_id'
+          },
+          to: 'authorised_systems.id'
+        }
+      }
+    }
+  }
 }
 
 module.exports = RegimeModel
