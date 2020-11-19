4bfba9275b7628081c9d206f5b312e8f2414a02e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-19 15:34:34 +0000

    Add new authorisation service

---
+++ app/services/authorisation.service.js
@@ -0,0 +1,72 @@
+'use strict'
+
+/**
+ * @module AuthorisationService
+ */
+
+/**
+ * Determine if a request is authorised to proceed
+ *
+ * Used by the `AuthorisationPlugin` to determine if a request can proceed. It has to cover a number of scenarios
+ *
+ * - requests to a public endpoint like `/status`
+ * - requests to 'admin' only endpoints like `/admin/regimes`
+ * - requests to a regime endpoint like `/v1/wrls/billruns`
+ *
+ * You don't need to be authenticated to access a public endpoint. So in those cases it handles both `user` and
+ * `regimeSlug` being `null`.
+ *
+ * Only users where the `admin` flag is set to `true` can access our 'admin' endpoints. And you can only create admins
+ * by directly adding them to the database! However, they can also access any regime endpoint irrespective of what
+ * regime is specified. In these cases it handles `user` being instantiated but the `regimeSlug` being `null`.
+ *
+ * The final scenario is the most common. This is system users who will have authenticated successfully but their
+ * `admin` flag will be false. They will be linked to a regime, but not necessarily the one being requested.
+ *
+ * In all cases it returns an `Object` which callers can use to determine if the request is authorised. It also contains
+ * the matched instance of `RegimeModel` if the slug was specified and matched.
+ *
+ * ```
+ * { authorised: false, regime: MatchedRegime }
+ * ```
+ *
+ * @param {module:AuthorisedSystemModel} [user=null] An instance of `AuthorisedSystemModel` which represents the 'user'
+ *  making the request
+ * @param {string} [regimeSlug=null] The short-code (slug) for the regime the request is for.
+ * @returns {Object} Returns an object which states whether the request is authorised and an instance of `RegimeModel`
+ * if one was specified and matched
+ */
+class AuthorisationService {
+  static async go (user = null, regimeSlug = null) {
+    const result = { authorised: false, regime: null }
+    if (this._endpointHasNoRegime(regimeSlug) || this._userIsAnAdmin(user)) {
+      result.authorised = true
+    } else {
+      const regime = await this._userAuthorisedRegime(user, regimeSlug)
+      if (regime) {
+        result.authorised = true
+        result.regime = regime
+      }
+    }
+
+    return result
+  }
+
+  static _userIsAnAdmin (user) {
+    return user.admin
+  }
+
+  static _endpointHasNoRegime (regimeSlug) {
+    return (!regimeSlug)
+  }
+
+  static async _userAuthorisedRegime (user, regimeSlug) {
+    const result = await user
+      .$relatedQuery('regimes')
+      .findOne({ slug: regimeSlug })
+
+    return result
+  }
+}
+
+module.exports = AuthorisationService
--- app/services/index.js
+++ app/services/index.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const AuthorisationService = require('./authorisation.service')
 const CalculateChargeService = require('./calculate_charge.service')
 const CognitoJwtToPemService = require('./cognito_jwt_to_pem.service')
 const ObjectCleaningService = require('./object_cleaning.service')
 const RulesService = require('./rules.service')
 
 module.exports = {
+  AuthorisationService,
   CalculateChargeService,
   CognitoJwtToPemService,
   ObjectCleaningService,
