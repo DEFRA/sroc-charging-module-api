72b5e06ba210e1a7393c5539b91ca2af301c9cab
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-25 22:12:17 +0000

    Add new list auth systems service

--- app/services/index.js
+++ app/services/index.js
@@ -3,6 +3,7 @@
 const AuthorisationService = require('./authorisation.service')
 const CalculateChargeService = require('./calculate_charge.service')
 const CognitoJwtToPemService = require('./cognito_jwt_to_pem.service')
+const ListAuthorisedSystemsService = require('./list_authorised_systems.service')
 const ListRegimesService = require('./list_regimes.service')
 const ObjectCleaningService = require('./object_cleaning.service')
 const RulesService = require('./rules.service')
@@ -12,6 +13,7 @@ module.exports = {
   AuthorisationService,
   CalculateChargeService,
   CognitoJwtToPemService,
+  ListAuthorisedSystemsService,
   ListRegimesService,
   ObjectCleaningService,
   RulesService,
