c3a5b5c76a2f9a24786a6d980f99bbf41afcffd2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-27 16:55:46 +0000

    Add bill_runs table migration

---
+++ db/migrations/20201127163958_create_bill_runs.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const tableName = 'bill_runs'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .createTable(tableName, table => {
+      // Primary Key
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+
+      // Data
+      table.uuid('regime_id').notNullable()
+      table.string('region').notNullable()
+
+      // Automatic timestamps
+      table.timestamps(false, true)
+    })
+
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON ${tableName}
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `)
+}
+
+exports.down = async function (knex) {
+  return knex
+    .schema
+    .dropTableIfExists(tableName)
+}
