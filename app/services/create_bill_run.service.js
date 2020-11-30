3dbfd6c7beca572f0e701c093cb6e478c61aa196
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-30 14:08:12 +0000

    Add documentation to the service

--- app/services/create_bill_run.service.js
+++ app/services/create_bill_run.service.js
@@ -1,9 +1,26 @@
 'use strict'
 
+/**
+ * @module CreateBillRunService
+ */
+
 const { BillRunModel } = require('../models')
 const { BillRunTranslator } = require('../translators')
 const { JsonPresenter } = require('../presenters')
 
+/**
+ * Creates a new bill run record
+ *
+ * The service handles validating and translating the request to the API and then creating a new bill run for the
+ * selected regime. It then returns the new bill run as the result.
+ *
+ * @param {Object} payload The payload from the API request
+ * @param {module:AuthorisedSystemModel} authorisedSystem Instance of `AuthorisedSystemModel' representing the
+ *  authenticated user
+ * @param {module:RegimeModel} regime Instance of `RegimeModel` representing the regime we are creating the bill run for
+ *
+ * @returns {Object} Details of the newly created bill run record
+ */
 class CreateBillRunService {
   static async go (payload, authorisedSystem, regime) {
     const translator = new BillRunTranslator(payload)
