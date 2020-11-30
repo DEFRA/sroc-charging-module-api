87e9343d95fa21c85a76052a053605af78b23e34
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-11-30 16:07:07 +0000

    Implement new not supported controller
    This covers the implementation and tests for the new not supported controller.

---
+++ app/controllers/not_supported.controller.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Boom = require('@hapi/boom')
+
+class NotSupportedController {
+  static async index (_req, _h) {
+    throw Boom.resourceGone('This version of the resource is no longer available. There may be an updated version.')
+  }
+}
+
+module.exports = NotSupportedController
