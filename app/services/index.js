71b2834abb3af99607442d51e47f8205eb21d939
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-01 22:04:42 +0000

    Add create bill run service to index
    Think this change got dropped when rebasing the branch on `main`.

--- app/services/index.js
+++ app/services/index.js
@@ -3,6 +3,7 @@
 const AuthorisationService = require('./authorisation.service')
 const CalculateChargeService = require('./calculate_charge.service')
 const CognitoJwtToPemService = require('./cognito_jwt_to_pem.service')
+const CreateBillRunService = require('./create_bill_run.service')
 const ListAuthorisedSystemsService = require('./list_authorised_systems.service')
 const ListRegimesService = require('./list_regimes.service')
 const ObjectCleaningService = require('./object_cleaning.service')
@@ -14,6 +15,7 @@ module.exports = {
   AuthorisationService,
   CalculateChargeService,
   CognitoJwtToPemService,
+  CreateBillRunService,
   ListAuthorisedSystemsService,
   ListRegimesService,
   ObjectCleaningService,
