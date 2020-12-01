4b3741aecdc9716aa69b96867599963d21bdf1a3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-01 09:03:06 +0000

    Update calculate charge controller test
    This was actually where we first started making changes. They started with first setting the tests up to actually work with the authorisation plugin. This meant ensuring we created a system user and recognised regime to go with it.
    
    This then moved into making the outline of the test more consistent with others, for example, moving the database clean to the `beforeEach()` block rather than the `afterEach()` block.
    
    But then we noticed some funnies. Though we were passing in a `fixture` value to `testFixtures()` we were never actually using it. When we spotted this we realised though all tests were named differently, because of the way they were written they were all running the eact same thing (`S126ProrataCreditFixtures`).
    
    It's ideal if a fellow developer can look at a test in isolation and understand what is goin on. That's why we tend to make our tests even more explicit and verbose than the code.
    
    So the final change was to move some of the test setup out of `testFixtures()` and back into the tests themselves even if it does lead to duplication.

--- test/controllers/calculate_charge.controller.test.js
+++ test/controllers/calculate_charge.controller.test.js
@@ -6,87 +6,99 @@ const Code = require('@hapi/code')
 const Sinon = require('sinon')
 const Nock = require('nock')
 
-const { describe, it, before, beforeEach, afterEach } = exports.lab = Lab.script()
+const { describe, it, before, beforeEach, after, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // For running our service
 const { deployment } = require('../../server')
 
 // Test helpers
-const { AuthorisationHelper, AuthorisedSystemHelper, RulesServiceHelper, DatabaseHelper } = require('../support/helpers')
+const {
+  AuthorisationHelper,
+  AuthorisedSystemHelper,
+  DatabaseHelper,
+  RegimeHelper,
+  RulesServiceHelper
+} = require('../support/helpers')
 
 // Things we need to stub
 const JsonWebToken = require('jsonwebtoken')
 
 const { SimpleFixtures, S127S130Fixtures, S126ProrataCreditFixtures } = require('../support/fixtures/wrls/calculate_charge')
 
-describe('Calculate charge controller: POST /v2/{regime}/calculate-charge', () => {
+describe('Calculate charge controller', () => {
+  const clientID = '1234546789'
   let server
-
-  // Create auth token for stubbing authorisation
-  const nonAdminClientId = 'k7ehotrs1fqer7hoaslv7ilmr'
-  const authToken = AuthorisationHelper.nonAdminToken(nonAdminClientId)
+  let authToken
 
   before(async () => {
-    // Create server before tests
     server = await deployment()
-  })
+    authToken = AuthorisationHelper.nonAdminToken(clientID)
 
-  beforeEach(async () => {
-    // Stub authorisation
-    await AuthorisedSystemHelper.addSystem(nonAdminClientId, 'wrls')
     Sinon
       .stub(JsonWebToken, 'verify')
       .returns(AuthorisationHelper.decodeToken(authToken))
   })
 
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    const regime = await RegimeHelper.addRegime('wrls', 'WRLS')
+    await AuthorisedSystemHelper.addSystem(clientID, 'system1', [regime])
+  })
+
+  after(async () => {
+    Sinon.restore()
+  })
+
   afterEach(async () => {
     Nock.cleanAll()
-    Sinon.restore()
-    await DatabaseHelper.clean()
   })
 
-  it('handles example 1 (simple case)', async () => {
-    await testFixtures(SimpleFixtures, authToken, server)
-  })
+  describe('Calculate a charge: POST /v2/{regimeId}/calculate-charge', () => {
+    const options = (token, payload) => {
+      return {
+        method: 'POST',
+        url: '/v2/wrls/calculate-charge',
+        headers: { authorization: `Bearer ${token}` },
+        payload: payload
+      }
+    }
 
-  it('handles example 2 (includes S127 and S130)', async () => {
-    await testFixtures(S127S130Fixtures, authToken, server)
-  })
+    it('handles example 1 (simple case)', async () => {
+      mockRulesService(SimpleFixtures.rulesServiceRequest, SimpleFixtures.rulesServiceResponse)
 
-  it('handles example 3 (S126, a pro-rata calculation and a credit)', async () => {
-    await testFixtures(S126ProrataCreditFixtures, authToken, server)
+      const response = await server.inject(options(authToken, SimpleFixtures.calculateChargeRequest))
+      const payload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(payload).to.equal(SimpleFixtures.calculateChargeResponse)
+    })
+
+    it('handles example 2 (includes S127 and S130)', async () => {
+      mockRulesService(S127S130Fixtures.rulesServiceRequest, S127S130Fixtures.rulesServiceResponse)
+
+      const response = await server.inject(options(authToken, S127S130Fixtures.calculateChargeRequest))
+      const payload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(payload).to.equal(S127S130Fixtures.calculateChargeResponse)
+    })
+
+    it('handles example 3 (S126, a pro-rata calculation and a credit)', async () => {
+      mockRulesService(S126ProrataCreditFixtures.rulesServiceRequest, S126ProrataCreditFixtures.rulesServiceResponse)
+
+      const response = await server.inject(options(authToken, S126ProrataCreditFixtures.calculateChargeRequest))
+      const payload = JSON.parse(response.payload)
+
+      expect(response.statusCode).to.equal(200)
+      expect(payload).to.equal(S126ProrataCreditFixtures.calculateChargeResponse)
+    })
   })
 })
 
-async function testFixtures (fixture, authToken, server) {
-  const {
-    rulesServiceRequest,
-    rulesServiceResponse,
-    calculateChargeRequest,
-    calculateChargeResponse
-  } = S126ProrataCreditFixtures
-
-  mockRulesService(rulesServiceRequest, rulesServiceResponse)
-
-  const options = calculateChargeOptions(authToken, calculateChargeRequest)
-  const response = await server.inject(options)
-  const payload = JSON.parse(response.payload)
-
-  expect(response.statusCode).to.equal(200)
-  expect(payload).to.equal(calculateChargeResponse)
-}
 function mockRulesService (request, response) {
   Nock(RulesServiceHelper.url, { encodedQueryParams: true })
     .post('/TEST_WRLS_SRoC_RuleApp/WRLS_SRoC_RuleSet_2021_22', request)
     .reply(200, response)
-}
-
-const calculateChargeOptions = (authToken, payload) => {
-  return {
-    method: 'POST',
-    url: '/v2/srocWrls/calculate-charge',
-    headers: { authorization: `Bearer ${authToken}` },
-    payload
-  }
 }
