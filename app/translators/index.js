52dd20f4c11c38ae03ff90bd23ae54d91d279c62
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-03 11:14:34 +0000

    Implement the translator
    This covers all the validations found in the charging module API.
    
    You will also see that there is duplication between it and the SROCChargeTranslator. This needs to be refactored out but as we are in the middle of a spike this is some 'tech debt' we're willing to stick on the credit card until the spike is complete. Then it will be task one to resolve.

--- app/translators/index.js
+++ app/translators/index.js
@@ -5,11 +5,13 @@ const BaseTranslator = require('./base.translator')
 const BillRunTranslator = require('./bill_run.translator')
 const RulesServiceTranslator = require('./rules_service.translator')
 const SrocChargeTranslator = require('./sroc_charge.translator')
+const TransactionTranslator = require('./transaction.translator')
 
 module.exports = {
   AuthorisedSystemTranslator,
   BaseTranslator,
   BillRunTranslator,
   RulesServiceTranslator,
-  SrocChargeTranslator
+  SrocChargeTranslator,
+  TransactionTranslator
 }
