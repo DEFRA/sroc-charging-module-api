0fb3eded901acaa45d6b5614d1a8fc30ae09f608
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 00:37:10 +0000

    Add create bill run transaction endpoint
    Add to the existing bill run controller a new endpoint to handle creating a transaction.

--- app/controllers/presroc/bill_runs.controller.js
+++ app/controllers/presroc/bill_runs.controller.js
@@ -1,10 +1,21 @@
 'use strict'
 
-const { CreateBillRunService } = require('../../services')
+const { CreateBillRunService, CreateBillRunTransactionService } = require('../../services')
 
 class BillRunsController {
   static async create (req, h) {
     const result = await CreateBillRunService.go(req.payload, req.auth.credentials.user, req.app.regime)
+
+    return h.response(result).code(201)
+  }
+
+  static async createTransaction (req, h) {
+    const result = await CreateBillRunTransactionService.go(
+      req.payload,
+      req.params.billRunId,
+      req.auth.credentials.user,
+      req.app.regime
+    )
 
     return h.response(result).code(201)
   }
