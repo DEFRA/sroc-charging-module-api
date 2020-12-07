827c6444d8bd0174b4b767773ece4cde8353d8e1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 00:09:38 +0000

    Drop add bill run transaction controller
    At this point we can add it to the billruns controller and assess whether its doing too much in the future.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,6 @@ const BaseCalculateChargeController = require('./base_calculate_charge.controlle
 
 const NotSupportedController = require('./not_supported.controller')
 
-const PresrocAddBillRunTransactionController = require('./presroc/add_bill_run_transaction.controller')
 const PresrocBillRunsController = require('./presroc/bill_runs.controller')
 const PresrocTransactionsController = require('./presroc/transactions.controller')
 const PresrocCalculateChargeController = require('./presroc/calculate_charge.controller')
@@ -25,7 +24,6 @@ module.exports = {
   AuthorisedSystemsController,
   BaseCalculateChargeController,
   DatabaseController,
-  PresrocAddBillRunTransactionController,
   PresrocBillRunsController,
   PresrocTransactionsController,
   PresrocCalculateChargeController,
--- app/controllers/presroc/add_bill_run_transaction.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-class PresrocAddBillRunTransactionController {
-  static async create (_req, _h) {
-    return 'hello, pre-sroc add bill run transaction'
-  }
-}
-
-module.exports = PresrocAddBillRunTransactionController
