7b06d6bdef95b42749632974277a0932d81b3e6d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 00:33:27 +0000

    Add pre sroc rule service translator
    And just like there is a different to how we present the data to the rules service for pre-sroc, there is also a difference to what data we get back. So this adds a pre-sroc rules service translator to handle it.

--- app/translators/index.js
+++ app/translators/index.js
@@ -3,6 +3,7 @@
 const AuthorisedSystemTranslator = require('./authorised_system.translator')
 const BaseTranslator = require('./base.translator')
 const BillRunTranslator = require('./bill_run.translator')
+const PreRulesServiceTranslator = require('./presroc_rules_service.translator')
 const SrocChargeTranslator = require('./sroc_charge.translator')
 const SrocRulesServiceTranslator = require('./sroc_rules_service.translator')
 const TransactionTranslator = require('./transaction.translator')
@@ -11,6 +12,7 @@ module.exports = {
   AuthorisedSystemTranslator,
   BaseTranslator,
   BillRunTranslator,
+  PreRulesServiceTranslator,
   SrocChargeTranslator,
   SrocRulesServiceTranslator,
   TransactionTranslator
---
+++ app/translators/presroc_rules_service.translator.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const BaseTranslator = require('./base.translator')
+const Joi = require('joi')
+
+class PreRulesServiceTranslator extends BaseTranslator {
+  constructor (data) {
+    console.log(`PRS ${JSON.stringify(data)}`)
+    // The rules service returns the data we need in a WRLSChargingResponse object within the response object
+    super(data.WRLSChargingResponse)
+
+    // We retain a copy of the actual response for audit purposes
+    this.calculation = JSON.stringify(data)
+
+    this.chargeValue = this._convertToPence(this._data.chargeValue)
+    this.sucFactor = this._convertToPence(this._data.sucFactor)
+
+    // Charge element agreement is determined based on rules service response
+    this.lineAttr10 = this._determineChargeElementAgreement()
+  }
+
+  _schema () {
+    return Joi.object({
+      chargeValue: Joi.number().required(),
+      sucFactor: Joi.number(),
+      sourceFactor: Joi.number(),
+      seasonFactor: Joi.number(),
+      lossFactor: Joi.number(),
+      abatementAdjustment: Joi.string().required(),
+      s127Agreement: Joi.string().allow(null),
+      s130Agreement: Joi.string().allow(null),
+      eiucSourceFactor: Joi.number(),
+      eiucFactor: Joi.number()
+    }).options({ stripUnknown: true })
+  }
+
+  _translations () {
+    return {
+      chargeValue: 'chargeValue',
+      sucFactor: 'lineAttr4',
+      sourceFactor: 'lineAttr6',
+      seasonFactor: 'lineAttr7',
+      lossFactor: 'lineAttr8',
+      s130Agreement: 'lineAttr9',
+      eiucSourceFactor: 'lineAttr13',
+      eiucFactor: 'lineAttr14'
+    }
+  }
+
+  // The data used for the charge element agreement depends on what the rules service returns
+  _determineChargeElementAgreement () {
+    // If a value is returned indicating an adjustment under Section 127 then use it
+    if (this._data.s127Agreement) {
+      return this._data.s127Agreement
+    }
+
+    // If a value is returned indicating an adjustment under Section 126 then use it
+    if (this._data.abatementAdjustment !== 'S126 x 1.0') {
+      return this._data.abatementAdjustment
+    }
+
+    // Otherwise, return null
+    return null
+  }
+
+  _convertToPence (value) {
+    const floatValue = parseFloat(value)
+
+    if (isNaN(floatValue)) {
+      return null
+    }
+
+    return Math.round(floatValue * 100)
+  }
+}
+
+module.exports = PreRulesServiceTranslator
