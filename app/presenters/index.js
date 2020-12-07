e9ef3326bb4d678327045a4f6e41aff6af3ea6de
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 00:31:08 +0000

    Add a presroc rule service presenter
    There are differences to how we present the data to the rules service depending on whether its a pre-sroc or sroc transaction.
    
    So this adds a pre-sroc version.

--- app/presenters/index.js
+++ app/presenters/index.js
@@ -3,11 +3,13 @@
 const BasePresenter = require('./base.presenter')
 const ChargePresenter = require('./charge.presenter')
 const JsonPresenter = require('./json.presenter')
+const PresrocRulesServicePresenter = require('./presroc_rules_service.presenter')
 const SrocRulesServicePresenter = require('./sroc_rules_service.presenter')
 
 module.exports = {
   BasePresenter,
   ChargePresenter,
   JsonPresenter,
+  PresrocRulesServicePresenter,
   SrocRulesServicePresenter
 }
