b3e2d445e6ea1a08cf25fa6f6a4314431ba9fbd6
Stuart Adair <stu@silverka.co.uk>
2020-12-10 16:01:20 +0000

    Update bill_runs table with bill_run_number

---
+++ db/migrations/20201210145745_alter_bill_runs.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const tableName = 'transactions'
+const columnName = 'bill_run_number'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .alterTable(tableName, table => {
+      // Add column
+      table.integer(columnName)
+    })
+}
+
+exports.down = async function (knex) {
+  await knex
+    .schema
+    .alterTable(tableName, table => {
+      // Drop the column we added
+      table.dropColumn(columnName)
+    })
+}
