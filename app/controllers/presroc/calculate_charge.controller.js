43a5556a53e1e00972575a881b473d2da02a0324
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-11 09:59:58 +0000

    Update controller to fit our current pattern
    We have a pattern of doing all 'work' in a service in order to allow us to unit test without having to make requests to a Hapi instance.
    
    Again, we need to update the actual service before any of these changes will work!

--- app/controllers/presroc/calculate_charge.controller.js
+++ app/controllers/presroc/calculate_charge.controller.js
@@ -1,31 +1,12 @@
 'use strict'
 
-const { ChargeModel } = require('../../models')
 const { CalculateChargeService } = require('../../services')
-const { SrocChargeTranslator, RulesServiceTranslator } = require('../../translators')
-const { ChargePresenter, RulesServicePresenter } = require('../../presenters')
 
 class CalculateChargeController {
-  static async calculate (req, _h) {
-    const charge = CalculateChargeController._createCharge(req.payload)
-    const rulesServiceResponse = await CalculateChargeController._presentRequest(charge, req.app.regime.slug)
-    Object.assign(charge, rulesServiceResponse)
-    return CalculateChargeController._presentResponse(charge)
-  }
+  static async calculate (req, h) {
+    const result = await CalculateChargeService.go(req.payload, req.app.regime)
 
-  static _createCharge (payload) {
-    const translatedRequest = new SrocChargeTranslator(payload)
-    return new ChargeModel(translatedRequest)
-  }
-
-  static _presentRequest (charge, regime) {
-    const requestPresenter = new RulesServicePresenter({ ...charge, regime })
-    return CalculateChargeService.go(requestPresenter.go(), RulesServiceTranslator)
-  }
-
-  static _presentResponse (charge) {
-    const responsePresenter = new ChargePresenter(charge)
-    return responsePresenter.go()
+    return h.response(result).code(200)
   }
 }
 
