d5e5fdbcf55ca97d46e2f6297eee76a5feb4ad45
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-11 15:13:20 +0000

    Just return response body from ruleservice
    Prior to this change we were always returning the full response we got from calling the rules service. We only actually cared about the body. To clean things up we now just return the body.
    
    This also gives us a bit more flexibility in the service in how we call the rules service. For example, we are likely to want to put in a retry mechanism and inspect the messages it returns in future updates.

--- app/services/rules.service.js
+++ app/services/rules.service.js
@@ -21,20 +21,28 @@ class RulesService {
     const requestOptions = this._requestOptions(url, chargeParams, username, password)
     const proxyOptions = httpProxy ? this._proxyOptions(httpProxy) : ''
 
-    return Got.post(path, { ...requestOptions, ...proxyOptions })
+    return this._callRulesService(path, requestOptions, proxyOptions)
   }
 
   // generate the path for the specified regime, year and ruleset
   static _makeRulesPath (rulesetId, regime, year) {
     const { application, ruleset } = RulesServiceConfig.endpoints[regime].rulesets[rulesetId]
     const suffix = this._yearSuffix(year)
+
     return `${application}/${ruleset}${suffix}`
+  }
+
+  static async _callRulesService (path, requestOptions, proxyOptions) {
+    const response = await Got.post(path, { ...requestOptions, ...proxyOptions })
+
+    return response.body
   }
 
   // Year suffix is '_2000_01' for 2000, '_2001_02' for 2001 etc.
   // ie. the year and the last two digits of the following year
   static _yearSuffix (year) {
     const nextYearDigits = (year + 1).toString().slice(2)
+
     return `_${year}_${nextYearDigits}`
   }
 
