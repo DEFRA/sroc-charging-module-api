45cc65c368d32cea8584ee3794f122902033c44b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-12 17:27:16 +0000

    Update rules translator test use new fixtures
    This update the existing test to use our new fixtures.

--- test/translators/rules_service.translator.test.js
+++ test/translators/rules_service.translator.test.js
@@ -8,56 +8,57 @@ const { describe, it } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
-const { WRLSChargingResponse: testResponse } = require('../support/fixtures/wrls/rules_service_response.json')
+const rulesServiceFixture = require('../support/fixtures/calculate_charge/wrls/simple_rules_service.json')
 
 // Thing under test
 const { RulesServiceTranslator } = require('../../app/translators')
 
-describe('Rules Service translator', () => {
-  describe('chargeValue', () => {
-    it('is translated to pence instead of pounds and pence', async () => {
-      const data = {
-        ...testResponse,
-        chargeValue: 123.45
-      }
+// Assigning an object is by reference so we need to clone the fixture to allow us to alter it in each test.
+// Because the fixture is multi level we can't use the spread operator or Object.assign as these only do shallow
+// clones. Using JSON.parse & JSON.stringify is a quick way of doing a deep clone, though not advised for large
+// objects
+// https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/
+const cloneFixture = () => {
+  return JSON.parse(JSON.stringify(rulesServiceFixture))
+}
 
-      const testTranslator = new RulesServiceTranslator({ WRLSChargingResponse: data })
+describe('Rules Service translator', () => {
+  describe("the 'chargeValue' property", () => {
+    it('is translated to pence instead of pounds and pence', async () => {
+      const data = cloneFixture()
+      data.WRLSChargingResponse.chargeValue = 123.45
+
+      const testTranslator = new RulesServiceTranslator(data)
 
       expect(testTranslator.chargeValue).to.equal(12345)
     })
   })
 
-  describe('lineAttr10', () => {
+  describe("the 'lineAttr10' property", () => {
     it('returns S127 value if present', async () => {
-      const data = {
-        ...testResponse,
-        abatementAdjustment: 'S126 x 0.5',
-        s127Agreement: 'S127 x 0.5'
-      }
+      const data = cloneFixture()
+      data.WRLSChargingResponse.abatementAdjustment = 'S126 x 0.5'
+      data.WRLSChargingResponse.s127Agreement = 'S127 x 0.5'
 
-      const testTranslator = new RulesServiceTranslator({ WRLSChargingResponse: data })
+      const testTranslator = new RulesServiceTranslator(data)
 
       expect(testTranslator.lineAttr10).to.equal('S127 x 0.5')
     })
 
     it('returns S126 value if it indicates adjustment', async () => {
-      const data = {
-        ...testResponse,
-        abatementAdjustment: 'S126 x 0.5'
-      }
+      const data = cloneFixture()
+      data.WRLSChargingResponse.abatementAdjustment = 'S126 x 0.5'
 
-      const testTranslator = new RulesServiceTranslator({ WRLSChargingResponse: data })
+      const testTranslator = new RulesServiceTranslator(data)
 
       expect(testTranslator.lineAttr10).to.equal('S126 x 0.5')
     })
 
     it("returns null if S126 value doesn't indicate adjustment", async () => {
-      const data = {
-        ...testResponse,
-        abatementAdjustment: 'S126 x 1.0'
-      }
+      const data = cloneFixture()
+      data.WRLSChargingResponse.abatementAdjustment = 'S126 x 1.0'
 
-      const testTranslator = new RulesServiceTranslator({ WRLSChargingResponse: data })
+      const testTranslator = new RulesServiceTranslator(data)
 
       expect(testTranslator.lineAttr10).to.equal(null)
     })
