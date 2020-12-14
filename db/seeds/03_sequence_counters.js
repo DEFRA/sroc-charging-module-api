1c8f18588aa0965c180dfce93026d8ee9e228fd8
Stuart Adair <stu@silverka.co.uk>
2020-12-14 16:29:27 +0000

    Seed sequence_counters table

---
+++ db/seeds/03_sequence_counters.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const tableName = 'sequence_counters'
+const regions = ['A', 'B', 'E', 'N', 'S', 'T', 'W', 'Y']
+
+exports.seed = async function (knex) {
+  // Clear existing table
+  await knex(tableName).del()
+
+  // Get all regime ids
+  const regimes = await knex.select().from('regimes')
+
+  // Create an entry for every region in every regime
+  for (const regime of regimes) {
+    for (const region of regions) {
+      await knex(tableName).insert([{ regime_id: regime.id, region }])
+    }
+  }
+}
