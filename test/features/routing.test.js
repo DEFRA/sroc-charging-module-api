6f9b2337e1ea165dbc3f7631937234464de26499
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-16 19:30:47 +0000

    Add tests to confirm options are working

---
+++ test/features/routing.test.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before } = exports.lab = Lab.script()
+const { expect } = Code
+
+// For running our service
+const { deployment } = require('../../server')
+
+// Test helpers
+const { RouteHelper } = require('../support/helpers')
+
+const options = (url) => {
+  return {
+    method: 'GET',
+    url: url
+  }
+}
+
+describe('Routing requests to the API', () => {
+  let server
+
+  // Create server before each test
+  before(async () => {
+    server = await deployment()
+    RouteHelper.addPublicPostRoute(server)
+  })
+
+  describe('When a request path has a trailing slash', () => {
+    it('still matches the route', async () => {
+      const response = await server.inject(options('/test/public/'))
+
+      expect(response.statusCode).to.equal(200)
+    })
+  })
+
+  describe('When a request path uses a different case', () => {
+    it('still matches the route', async () => {
+      const response = await server.inject(options('/test/PUBLIC'))
+
+      expect(response.statusCode).to.equal(200)
+    })
+  })
+})
