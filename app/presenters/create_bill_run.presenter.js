a8a82b0cb122820813543950474d06cb24732fcd
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2020-12-17 12:04:02 +0000

    Create Bill Run presenter (#97)
    The last part of adding bill run numbers to the create bill run endpoint is to define create_bill_run.presenter which we use to return to the end user the bill run id and number.
    
    This change implements this and updates existing tests:
    
    - bill_runs.controller now only tests for the info returned;
    - create_bill_run.service now pulls the bill run from the database and tests its properties, rather than relying on all the properties being returned.

---
+++ app/presenters/create_bill_run.presenter.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * @module CreateBillRunPresenter
+ */
+
+const BasePresenter = require('./base.presenter')
+
+/**
+ * Handles formatting the data into the response we send to clients after a create bill run request.
+ */
+class CreateBillRunPresenter extends BasePresenter {
+  _presentation (data) {
+    return {
+      billRun: {
+        id: data.id,
+        billRunNumber: data.billRunNumber
+      }
+    }
+  }
+}
+
+module.exports = CreateBillRunPresenter
