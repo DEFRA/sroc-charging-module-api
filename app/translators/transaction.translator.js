c92c3d37221756269ebc323f473a79a6b66f218c
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2020-12-18 16:09:24 +0000

    Transaction translator (#104)
    This change updates transaction.translator to remove validation duplicated in create_charge.translator. As part of this it amends base.translator to not error if data is passed in that isn't validated.

--- app/translators/transaction.translator.js
+++ app/translators/transaction.translator.js
@@ -2,55 +2,8 @@
 
 const BaseTranslator = require('./base.translator')
 const Joi = require('joi')
-const Boom = require('@hapi/boom')
 
 class TransactionTranslator extends BaseTranslator {
-  constructor (data) {
-    super(data)
-
-    this.lineAttr3 = this._prorataDays()
-    this.chargeFinancialYear = this._financialYear(this.chargePeriodStart)
-
-    // Additional post-getter validation to ensure periodStart and periodEnd are in the same financial year
-    this._validateFinancialYear()
-  }
-
-  _translations () {
-    return {
-      region: 'region',
-      customerReference: 'customerReference',
-      batchNumber: 'regimeValue1',
-      licenceNumber: 'lineAttr1',
-      chargePeriod: 'lineAttr2',
-      chargeElementId: 'regimeValue3',
-      areaCode: 'lineAreaCode',
-      lineDescription: 'lineDescription',
-      newLicence: 'newLicence',
-      clientId: 'clientId',
-      ruleset: 'ruleset',
-      periodStart: 'chargePeriodStart',
-      periodEnd: 'chargePeriodEnd',
-      credit: 'chargeCredit',
-      billableDays: 'regimeValue4',
-      authorisedDays: 'regimeValue5',
-      volume: 'lineAttr5',
-      source: 'regimeValue6',
-      season: 'regimeValue7',
-      loss: 'regimeValue8',
-      section130Agreement: 'regimeValue9',
-      section126Agreement: 'regimeValue10',
-      section126Factor: 'regimeValue11',
-      section127Agreement: 'regimeValue12',
-      twoPartTariff: 'regimeValue16',
-      compensationCharge: 'regimeValue17',
-      eiucSource: 'regimeValue13',
-      waterUndertaker: 'regimeValue14',
-      regionalChargingArea: 'regimeValue15',
-      prorataDays: 'lineAttr3',
-      chargeFinancialYear: 'chargeFinancialYear'
-    }
-  }
-
   _schema () {
     return Joi.object({
       region: Joi.string().uppercase().valid(...this._validRegions()),
@@ -62,89 +15,48 @@ class TransactionTranslator extends BaseTranslator {
       areaCode: Joi.string().uppercase().valid(...this._validAreas()),
       lineDescription: Joi.string().max(240).required(),
       newLicence: Joi.boolean().default(false),
-      clientId: Joi.string().allow('', null),
-      // Set a new field called ruleset. This will be used to determine which ruleset to query in the rules service
-      ruleset: Joi.string().default('presroc'),
-      periodStart: Joi.date().less(Joi.ref('periodEnd')).min('01-APR-2014').required(),
-      periodEnd: Joi.date().required(),
-      credit: Joi.boolean().required(),
-      billableDays: Joi.number().integer().min(0).max(366).required(),
-      authorisedDays: Joi.number().integer().min(0).max(366).required(),
-      volume: Joi.number().min(0),
-      source: Joi.string().required(), // validated in rules service
-      season: Joi.string().required(), // validated in rules service
-      loss: Joi.string().required(), // validated in rules service
-      section130Agreement: Joi.boolean().required(),
-      section126Factor: Joi.number().allow(null).empty(null).default(1.0),
-      section127Agreement: Joi.boolean().required(),
-      twoPartTariff: Joi.boolean().required(),
-      compensationCharge: Joi.boolean().required(),
-      eiucSource: Joi.when('compensationCharge', { is: true, then: Joi.string().required() }), // validated in the rules service
-      waterUndertaker: Joi.boolean().required(),
-      regionalChargingArea: Joi.string().required() // validated in the rules service
+      clientId: Joi.string().allow('', null)
     })
   }
 
-  _validateFinancialYear () {
-    const schema = Joi.object({
-      chargePeriodEndFinancialYear: Joi.number().equal(this.chargeFinancialYear)
-    })
-
-    const data = {
-      chargePeriodEndFinancialYear: this._financialYear(this.chargePeriodEnd)
+  _translations () {
+    return {
+      ruleset: 'ruleset',
+      region: 'region',
+      customerReference: 'customerReference',
+      periodStart: 'periodStart',
+      periodEnd: 'periodEnd',
+      financialYear: 'financialYear',
+      newLicence: 'newLicence',
+      clientId: 'clientId',
+      credit: 'credit',
+      areaCode: 'lineAreaCode',
+      lineDescription: 'lineDescription',
+      licenceNumber: 'lineAttr1',
+      chargePeriod: 'lineAttr2',
+      prorataDays: 'lineAttr3',
+      volume: 'lineAttr5',
+      batchNumber: 'regimeValue1',
+      chargeElementId: 'regimeValue3',
+      billableDays: 'regimeValue4',
+      authorisedDays: 'regimeValue5',
+      source: 'regimeValue6',
+      season: 'regimeValue7',
+      loss: 'regimeValue8',
+      section130Agreement: 'regimeValue9',
+      section126Agreement: 'regimeValue10',
+      section126Factor: 'regimeValue11',
+      section127Agreement: 'regimeValue12',
+      eiucSource: 'regimeValue13',
+      waterUndertaker: 'regimeValue14',
+      regionalChargingArea: 'regimeValue15',
+      twoPartTariff: 'regimeValue16',
+      compensationCharge: 'regimeValue17'
     }
-
-    const { error } = schema.validate(data)
-
-    if (error) {
-      throw Boom.badData(error)
-    }
-  }
-
-  /**
-   * Returns the calculated financial year for a given date
-   *
-   * If the date is January to March then the financial year is the previous year. Otherwise, the financial year is the
-   * current year.
-   *
-   * For example, if the date is 01-MAR-2022 then the financial year will be 2021. If the it's 01-MAY-2022 then the
-   * financial year will be 2022.
-   *
-   * @param {String} date
-   * @returns {Number} The calculated financial year
-  */
-  _financialYear (date) {
-    const chargePeriodDate = new Date(date)
-    const month = chargePeriodDate.getMonth()
-    const year = chargePeriodDate.getFullYear()
-
-    return (month <= 2 ? year - 1 : year)
-  }
-
-  /**
-   * Returns billable and authorised day values as a specially formatted string.
-   *
-   * For example, if billable days is 12 and authorised days is 6 it will return `012/006`.
-   *
-   * @returns {String} Billable days and authorised days as a formatted string
-   */
-  _prorataDays () {
-    return `${this._padNumber(this.regimeValue4)}/${this._padNumber(this.regimeValue5)}`
   }
 
   _validRegions () {
     return ['A', 'B', 'E', 'N', 'S', 'T', 'W', 'Y']
-  }
-
-  /**
-   * Return a number as a string, padded to 3 digits with leading zeroes
-   *
-   * For example, `_padNumber(3)` will return `003`.
-   *
-   * @returns {Number} the number padded with leading zeroes
-   */
-  _padNumber (number) {
-    return number.toString().padStart(3, '0')
   }
 
   _validAreas () {
