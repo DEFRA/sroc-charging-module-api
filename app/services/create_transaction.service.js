d1b76f5be0e79438685715d8331def83817347d4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-21 17:19:09 +0000

    Assign calculate charge result to translator

--- app/services/create_transaction.service.js
+++ app/services/create_transaction.service.js
@@ -12,14 +12,28 @@ class CreateTransactionService {
     const translator = new TransactionTranslator(payload)
     const calculatedCharge = await this._calculateCharge(translator, regime)
 
+    this._applyCalculatedCharge(translator, calculatedCharge)
+
     return 'not done yet'
   }
 
   static _calculateCharge (translator, regime) {
     // The CalculateChargeService expects data to be presented using the original request properties, for example,
     // `waterUndertaker` not `regimeValue14`. `validatedData` on the translator gives us access to the data in this
-    // original format which is why we pass it in. The underlying data itself though remains the same though!
+    // original format which is why we pass it in. The underlying data itself though remains the same!
     return CalculateChargeService.go(translator.validatedData, regime)
+  }
+
+  /**
+   * Assign properties of the calculated charge to the translator object
+   *
+   * The translator represents our transaction until we persist it. A transaction encompasses properties we get from the
+   * client making the request and the results we get back from the charge service. This is why we assign the calculated
+   * charge to the translator. It gives us a complete representation of the transaction ready for persisting to the
+   * database.
+   */
+  static _applyCalculatedCharge (translator, calculatedCharge) {
+    Object.assign(translator, calculatedCharge)
   }
 }
 
