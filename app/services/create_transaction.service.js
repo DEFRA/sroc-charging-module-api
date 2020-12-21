602606fe8320e88d1d3ecf4ba1acf06382694e35
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-21 15:21:11 +0000

    Add skeleton service

---
+++ app/services/create_transaction.service.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/**
+ * @module CreateTransactionService
+ */
+
+class CreateTransactionService {
+  static async go (payload, billRunId, authorisedSystem, regime) {
+    return 'not done yet'
+  }
+}
+
+module.exports = CreateTransactionService
