501e2a57d759a78e2d81d52c1e758abb8097b3ae
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-21 17:12:50 +0000

    Pass validated transaction data to charge service
    This gets us another test passing. This time we confirm we can rely on the `CalculateChargeTranslator` in the `CalculateChargeService` to tell us if anything in the request is invalid. Though the check is done somewhere else, we confirm the end result; that the user sees a validation error.

--- app/services/create_transaction.service.js
+++ app/services/create_transaction.service.js
@@ -5,12 +5,21 @@
  */
 
 const { TransactionTranslator } = require('../translators')
+const CalculateChargeService = require('./calculate_charge.service')
 
 class CreateTransactionService {
   static async go (payload, billRunId, authorisedSystem, regime) {
     const translator = new TransactionTranslator(payload)
+    const calculatedCharge = await this._calculateCharge(translator, regime)
 
     return 'not done yet'
+  }
+
+  static _calculateCharge (translator, regime) {
+    // The CalculateChargeService expects data to be presented using the original request properties, for example,
+    // `waterUndertaker` not `regimeValue14`. `validatedData` on the translator gives us access to the data in this
+    // original format which is why we pass it in. The underlying data itself though remains the same though!
+    return CalculateChargeService.go(translator.validatedData, regime)
   }
 }
 
