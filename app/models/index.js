ed1e0bab86a8df21a8f1c6cf1befba7ccb4f01b0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-22 08:39:31 +0000

    Add transaction to index.js

--- app/models/index.js
+++ app/models/index.js
@@ -5,11 +5,13 @@ const BaseModel = require('./base.model')
 const BillRunModel = require('./bill_run.model')
 const RegimeModel = require('./regime.model')
 const SequenceCounterModel = require('./sequence_counter.model')
+const TransactionModel = require('./transaction.model')
 
 module.exports = {
   AuthorisedSystemModel,
   BaseModel,
   BillRunModel,
   RegimeModel,
-  SequenceCounterModel
+  SequenceCounterModel,
+  TransactionModel
 }
