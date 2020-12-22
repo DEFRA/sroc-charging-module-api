0915d566fc9a521aa509ab2f9b71a31cc65029be
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-22 20:38:23 +0000

    Add regime ID to bill run translator

--- app/translators/bill_run.translator.js
+++ app/translators/bill_run.translator.js
@@ -6,12 +6,14 @@ const Joi = require('joi')
 class BillRunTranslator extends BaseTranslator {
   _translations () {
     return {
+      regimeId: 'regimeId',
       region: 'region'
     }
   }
 
   _schema () {
     return Joi.object({
+      regimeId: Joi.string().required(),
       region: Joi.string().uppercase().valid(...this._validRegions())
     })
   }
