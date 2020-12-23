e460c199fc97e12b67c0099ec306a5e23bc15aec
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-23 09:41:12 +0000

    Simplify create transaction call
    By just getting the rules service result and assigning it to our translator we have an instance we can pass as is to the insert query rather than having to detail each field.
    
    As part of this we have moved the transaction translator to its own method to make it clearer what's going on.

--- app/services/create_transaction.service.js
+++ app/services/create_transaction.service.js
@@ -10,7 +10,7 @@ const CalculateChargeService = require('./calculate_charge.service')
 
 class CreateTransactionService {
   static async go (payload, billRunId, authorisedSystem, regime) {
-    const translator = new TransactionTranslator(payload)
+    const translator = this._translateRequest(payload, billRunId, authorisedSystem, regime)
     const calculatedCharge = await this._calculateCharge(translator, regime)
 
     this._applyCalculatedCharge(translator, calculatedCharge)
@@ -20,11 +20,20 @@ class CreateTransactionService {
     return JSON.stringify(transaction)
   }
 
+  static _translateRequest (payload, billRunId, authorisedSystem, regime) {
+    return new TransactionTranslator({
+      ...payload,
+      billRunId,
+      regimeId: regime.id,
+      authorisedSystemId: authorisedSystem.id
+    })
+  }
+
   static _calculateCharge (translator, regime) {
     // The CalculateChargeService expects data to be presented using the original request properties, for example,
     // `waterUndertaker` not `regimeValue14`. `validatedData` on the translator gives us access to the data in this
     // original format which is why we pass it in. The underlying data itself though remains the same!
-    return CalculateChargeService.go(translator.validatedData, regime)
+    return CalculateChargeService.go(translator.validatedData, regime, false)
   }
 
   /**
@@ -36,57 +45,14 @@ class CreateTransactionService {
    * database.
    */
   static _applyCalculatedCharge (translator, calculatedCharge) {
-    Object.assign(translator, calculatedCharge.calculation)
+    Object.assign(translator, calculatedCharge)
   }
 
   static _create (billRunId, authorisedSystem, regime, translator) {
     return TransactionModel.transaction(async trx => {
       const transaction = await TransactionModel.query(trx)
         .insert({
-          billRunId: billRunId,
-          createdBy: authorisedSystem.id,
-          regimeId: regime.id,
-          ruleset: translator.ruleset,
-          region: translator.region,
-          customerReference: translator.customerReference,
-          chargePeriodStart: translator.chargePeriodStart,
-          chargePeriodEnd: translator.chargePeriodEnd,
-          chargeFinancialYear: translator.chargeFinancialYear,
-          newLicence: translator.newLicence,
-          clientId: translator.clientId,
-          chargeValue: translator.chargeValue,
-          chargeCredit: translator.chargeCredit,
-          lineAreaCode: translator.lineAreaCode,
-          lineDescription: translator.lineDescription,
-          lineAttr1: translator.lineAttr1,
-          lineAttr2: translator.lineAttr2,
-          lineAttr3: translator.lineAttr3,
-          lineAttr4: translator.lineAttr4,
-          lineAttr5: translator.lineAttr5,
-          lineAttr6: translator.lineAttr6,
-          lineAttr7: translator.lineAttr7,
-          lineAttr8: translator.lineAttr8,
-          lineAttr9: translator.lineAttr9,
-          lineAttr10: translator.lineAttr10,
-          lineAttr13: translator.lineAttr13,
-          lineAttr14: translator.lineAttr14,
-          regimeValue1: translator.regimeValue1,
-          regimeValue3: translator.regimeValue3,
-          regimeValue4: translator.regimeValue4,
-          regimeValue5: translator.regimeValue5,
-          regimeValue6: translator.regimeValue6,
-          regimeValue7: translator.regimeValue7,
-          regimeValue8: translator.regimeValue8,
-          regimeValue9: translator.regimeValue9,
-          regimeValue10: translator.regimeValue10,
-          regimeValue11: translator.regimeValue11,
-          regimeValue12: translator.regimeValue12,
-          regimeValue13: translator.regimeValue13,
-          regimeValue14: translator.regimeValue14,
-          regimeValue15: translator.regimeValue15,
-          regimeValue16: translator.regimeValue16,
-          regimeValue17: translator.regimeValue17,
-          chargeCalculation: translator.chargeCalculation
+          ...translator
         })
         .returning('*')
 
