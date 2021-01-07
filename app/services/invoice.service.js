288345b4d9e8c39647c980768479c9d4c98ccf62
Stuart Adair <stu@silverka.co.uk>
2021-01-07 12:50:21 +0000

    Add invoice service

---
+++ app/services/invoice.service.js
@@ -0,0 +1,46 @@
+'use strict'
+
+/**
+ * @module InvoiceService
+ */
+
+const { InvoiceModel } = require('../models')
+
+class InvoiceService {
+  static async go (transaction) {
+    const invoice = await this._invoice(transaction.billRunId, transaction.customerReference, transaction.chargeFinancialYear)
+
+    this._populateStats(invoice, transaction)
+
+    return invoice
+  }
+
+  static _invoice (billRunId, customerReference, financialYear) {
+    return InvoiceModel.query()
+      .findOrInsert(
+        {
+          bill_run_id: billRunId,
+          customer_reference: customerReference,
+          financial_year: financialYear
+        }
+      )
+  }
+
+  static _populateStats (invoice, transaction) {
+    if (transaction.credit) {
+      invoice.creditCount += 1
+      invoice.creditValue += transaction.chargeValue
+    } else if (transaction.chargeValue === 0) {
+      invoice.zeroCount += 1
+    } else {
+      invoice.debitCount += 1
+      invoice.debitValue += transaction.chargeValue
+    }
+
+    if (transaction.newLicence) {
+      invoice.newLicenceCount += 1
+    }
+  }
+}
+
+module.exports = InvoiceService
