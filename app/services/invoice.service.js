d41d0f84ea79a33f77c57e331d490d757b85b7ef
Stuart Adair <stu@silverka.co.uk>
2021-01-08 10:15:02 +0000

    Refactor invoice service and document

--- app/services/invoice.service.js
+++ app/services/invoice.service.js
@@ -5,14 +5,26 @@
  */
 
 const { InvoiceModel } = require('../models')
+const Hoek = require('@hapi/hoek')
 
 class InvoiceService {
+/**
+* Accepts a transaction and creates an entry in the Invoices table if one doesn't already exist for the transaction's
+* bill run/customer/financial year. It updates the count and value stats as per the transaction details and returns the
+* resulting invoice.
+*
+* Note that the updated stats are _not_ saved back to the database; it is up to the caller to do this.
+*
+* @param {Object} A transaction object
+* @returns {Object} An invoice object
+*/
+
   static async go (transaction) {
     const invoice = await this._invoice(transaction.billRunId, transaction.customerReference, transaction.chargeFinancialYear)
 
-    this._populateStats(invoice, transaction)
+    const updatedInvoice = this._updateStats(invoice, transaction)
 
-    return invoice
+    return updatedInvoice
   }
 
   static _invoice (billRunId, customerReference, financialYear) {
@@ -26,20 +38,24 @@ class InvoiceService {
       )
   }
 
-  static _populateStats (invoice, transaction) {
+  static _updateStats (invoice, transaction) {
+    const updatedInvoice = Hoek.clone(invoice)
+
     if (transaction.chargeCredit) {
-      invoice.creditCount += 1
-      invoice.creditValue += transaction.chargeValue
+      updatedInvoice.creditCount += 1
+      updatedInvoice.creditValue += transaction.chargeValue
     } else if (transaction.chargeValue === 0) {
-      invoice.zeroCount += 1
+      updatedInvoice.zeroCount += 1
     } else {
-      invoice.debitCount += 1
-      invoice.debitValue += transaction.chargeValue
+      updatedInvoice.debitCount += 1
+      updatedInvoice.debitValue += transaction.chargeValue
     }
 
     if (transaction.newLicence) {
-      invoice.newLicenceCount += 1
+      updatedInvoice.newLicenceCount += 1
     }
+
+    return updatedInvoice
   }
 }
 
