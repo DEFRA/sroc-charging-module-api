599b6cb6adf66bc5a644865208d75e676a974efb
Stuart Adair <stu@silverka.co.uk>
2021-01-14 16:45:00 +0000

    Update summary stats in bill run service

--- app/services/bill_run.service.js
+++ app/services/bill_run.service.js
@@ -10,7 +10,10 @@ const { BillRunModel } = require('../models')
 
 class BillRunService {
   /**
-  * Finds the matching bill run and determines if a transaction can be added to it.
+  * Finds the matching bill run, determines if a transaction can be added to it and updates updates the count and value
+  * stats as per the transaction details.
+  *
+  * Note that the updated stats are _not_ saved back to the database; it is up to the caller to do this.
   *
   * @param {Object} transaction translator belonging to the bill run to find and assess
   * @returns {module:BillRunModel} a `BillRunModel` if found else it will throw a `Boom` error
@@ -19,6 +22,7 @@ class BillRunService {
     const billRun = await BillRunModel.query().findById(transaction.billRunId)
 
     this._validateBillRun(billRun, transaction.billRunId)
+    this._updateStats(billRun, transaction)
 
     return billRun
   }
@@ -30,6 +34,22 @@ class BillRunService {
 
     if (!billRun.$editable()) {
       throw Boom.badData(`Bill run ${billRun.id} cannot be edited because its status is ${billRun.status}.`)
+    }
+  }
+
+  static _updateStats (billRun, transaction) {
+    if (transaction.chargeCredit) {
+      billRun.creditCount += 1
+      billRun.creditValue += transaction.chargeValue
+    } else if (transaction.chargeValue === 0) {
+      billRun.zeroCount += 1
+    } else {
+      billRun.debitCount += 1
+      billRun.debitValue += transaction.chargeValue
+    }
+
+    if (transaction.newLicence) {
+      billRun.newLicenceCount += 1
     }
   }
 }
--- test/services/bill_run.service.test.js
+++ test/services/bill_run.service.test.js
@@ -9,6 +9,7 @@ const { expect } = Code
 
 // Test helpers
 const { BillRunHelper, DatabaseHelper } = require('../support/helpers')
+const { BillRunModel } = require('../../app/models')
 
 // Thing under test
 const { BillRunService } = require('../../app/services')
@@ -16,6 +17,12 @@ const { BillRunService } = require('../../app/services')
 describe('Bill Run service', () => {
   const authorisedSystemId = '6fd613d8-effb-4bcd-86c7-b0025d121692'
   const regimeId = '4206994c-5db9-4539-84a6-d4b6a671e2ba'
+  const dummyTransaction = {
+    customerReference: 'CUSTOMER_REFERENCE',
+    chargeFinancialYear: 2021,
+    chargeCredit: false,
+    chargeValue: 5678
+  }
   let billRun
 
   beforeEach(async () => {
@@ -28,11 +35,67 @@ describe('Bill Run service', () => {
     })
 
     it('returns the matching bill run', async () => {
-      const transaction = { billRunId: billRun.id }
+      const transaction = { ...dummyTransaction, billRunId: billRun.id }
 
       const result = await BillRunService.go(transaction)
 
       expect(result.id).to.equal(billRun.id)
+    })
+
+    describe('When a debit transaction is supplied', () => {
+      it('correctly calculates the summary', async () => {
+        const transaction = { ...dummyTransaction, billRunId: billRun.id }
+
+        const result = await BillRunService.go(transaction)
+
+        expect(result.debitCount).to.equal(1)
+        expect(result.debitValue).to.equal(transaction.chargeValue)
+      })
+    })
+
+    describe('When a credit transaction is supplied', () => {
+      it('correctly calculates the summary', async () => {
+        const transaction = { ...dummyTransaction, billRunId: billRun.id, chargeCredit: true }
+
+        const result = await BillRunService.go(transaction)
+
+        expect(result.creditCount).to.equal(1)
+        expect(result.creditValue).to.equal(transaction.chargeValue)
+      })
+    })
+
+    describe('When a zero value transaction is supplied', () => {
+      it('correctly calculates the summary', async () => {
+        const transaction = { ...dummyTransaction, billRunId: billRun.id, chargeValue: 0 }
+
+        const result = await BillRunService.go(transaction)
+
+        expect(result.zeroCount).to.equal(1)
+      })
+    })
+
+    describe('When a new licence transaction is supplied', () => {
+      it('correctly sets the new licence flag', async () => {
+        const transaction = { ...dummyTransaction, billRunId: billRun.id, newLicence: true }
+
+        const result = await BillRunService.go(transaction)
+
+        expect(result.newLicenceCount).to.equal(1)
+      })
+    })
+
+    describe('When two transactions are created', () => {
+      it('correctly calculates the summary', async () => {
+        const transaction = { ...dummyTransaction, billRunId: billRun.id }
+        const firstResult = await BillRunService.go(transaction)
+        // We save the invoice with stats to the database as this isn't done by BillRunService
+        await BillRunModel.query().update(firstResult)
+
+        const secondResult = await BillRunService.go(transaction)
+
+        expect(secondResult.debitCount).to.equal(2)
+        expect(secondResult.debitValue).to.equal(transaction.chargeValue * 2)
+      })
     })
   })
 
@@ -41,7 +104,7 @@ describe('Bill Run service', () => {
 
     describe('because no matching bill run exists', () => {
       it('throws an error', async () => {
-        const transaction = { billRunId: unknownBillRunId }
+        const transaction = { ...dummyTransaction, billRunId: unknownBillRunId }
 
         const err = await expect(BillRunService.go(transaction)).to.reject()
 
@@ -56,7 +119,7 @@ describe('Bill Run service', () => {
       })
 
       it('throws an error', async () => {
-        const transaction = { billRunId: billRun.id }
+        const transaction = { ...dummyTransaction, billRunId: billRun.id }
 
         const err = await expect(BillRunService.go(transaction)).to.reject()
 
