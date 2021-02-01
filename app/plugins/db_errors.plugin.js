8a604c870398249d33e0a49a88a75234286c7238
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-01 00:20:21 +0000

    Update plugin to use new service
    We can remove all the logic and requires(), and just let the service do its work.

--- app/plugins/db_errors.plugin.js
+++ app/plugins/db_errors.plugin.js
@@ -1,36 +1,10 @@
 'use strict'
 
 /**
- * @module CleanPayloadPlugin
+ * @module DbErrorsPlugin
  */
 
-const Boom = require('@hapi/boom')
-const { UniqueViolationError } = require('db-errors')
-
-const parseError = (error, data) => {
-  if (error instanceof UniqueViolationError) {
-    return handleUniqueViolationError(error, data)
-  } else {
-    return error
-  }
-}
-
-const handleUniqueViolationError = (error, data) => {
-  console.log(JSON.stringify(error))
-  const regimeId = data.params.regimeId
-  let message
-
-  if (error.constraint === 'transactions_regime_id_client_id_unique') {
-    message = `A transaction with Client ID '${data.payload.clientId}' for Regime '${regimeId}' already exists.`
-  } else {
-    message = `${error.name} - ${error.nativeError.detail}`
-  }
-
-  return new Boom.Boom(
-    message,
-    { statusCode: 409 }
-  )
-}
+const { DbErrorsService } = require('../services')
 
 const DbErrorsPlugin = {
   name: 'db_errors',
@@ -47,7 +21,7 @@ const DbErrorsPlugin = {
         params: request.params
       }
 
-      return parseError(response, data)
+      return DbErrorsService.go(response, data)
     })
   }
 }
