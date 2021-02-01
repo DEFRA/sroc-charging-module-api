a7bc20fd205046603bae24ee8111717d64fd6eaa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-01 22:47:46 +0000

    Fix unit tests for GitHub CI workflow
    All the tests were running fine locally but that was because the env vars `GIT_COMMIT` and `DOCKER_TAG` exist. Stubbing them via Sinon caused an error when the tests were run in an environment where they don't exist.
    
    This change replaces Sinon with a process of first cloning `process.env`, making your changes in each test, and restoring it using the clone afterwards (see https://stackoverflow.com/a/42304479/6117745).
    
    Doing it this way rather than adding the env vars to the CI workflow means the plugin should be more stable as it does not depend on the env vars existing.

--- test/features/version_info.test.js
+++ test/features/version_info.test.js
@@ -3,7 +3,6 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
-const Sinon = require('sinon')
 
 const { describe, it, before, beforeEach, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
@@ -12,7 +11,7 @@ const { expect } = Code
 const { deployment } = require('../../server')
 
 // Test helpers
-const { RouteHelper } = require('../support/helpers')
+const { GeneralHelper, RouteHelper } = require('../support/helpers')
 
 const options = {
   method: 'GET',
@@ -20,6 +19,16 @@ const options = {
 }
 
 describe('Add version information to all responses', () => {
+  // We need to 'stub' process.env. However, if a property does not already exist Sinon, the tool we normally use,
+  // throws an error
+  //
+  //  Cannot stub non-existent property GIT_COMMIT
+  //
+  // Locally it's likely the env vars will be set, and we could set them in our CI workflow. However, we think the
+  // VersionInfoPlugin is more robust if our tests prove it will work as expected whether the env vars exist or not. So,
+  // instead we have gone with a method where we clone the existing process.env prior to running our tests, and restore
+  // it after each one to ensure the changes don't leak into other tests.
+  const env = GeneralHelper.cloneObject(process.env)
   let server
 
   // Create server before each test
@@ -29,14 +38,14 @@ describe('Add version information to all responses', () => {
   })
 
   afterEach(async () => {
-    Sinon.restore()
+    process.env = env
   })
 
   describe('When a request is made', () => {
     describe('if the version environment variables are present', () => {
       beforeEach(async () => {
-        Sinon.stub(process.env, 'GIT_COMMIT').value('abc123')
-        Sinon.stub(process.env, 'DOCKER_TAG').value('v0.1.0-99-gaabc123')
+        process.env.GIT_COMMIT = 'abc123'
+        process.env.DOCKER_TAG = 'v0.1.0-99-gaabc123'
       })
 
       it('includes git commit details in a response header', async () => {
@@ -56,8 +65,8 @@ describe('Add version information to all responses', () => {
 
     describe('if the version environment variables are not present', () => {
       beforeEach(async () => {
-        Sinon.stub(process.env, 'GIT_COMMIT').value('')
-        Sinon.stub(process.env, 'DOCKER_TAG').value('')
+        delete process.env.GIT_COMMIT
+        delete process.env.DOCKER_TAG
       })
 
       it("sets the git commit details in a response header to 'unknown'", async () => {
