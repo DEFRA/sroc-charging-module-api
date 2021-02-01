6d4838cd8b0733192c6a33f7939c94f626d2102a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-01 23:13:29 +0000

    Add documentation for plugin
    Covers two keys things
    
    - why we have the plugin
    - the reason for our choice of name for the custom headers
    
    The last one might be important to anyone who sees the headers and throws RFC 6648 at us!

--- app/plugins/version_info.plugin.js
+++ app/plugins/version_info.plugin.js
@@ -1,6 +1,38 @@
 'use strict'
 
 /**
+ * Add git commit and docker tag details as headers to all responses
+ *
+ * With the access we have to our AWS ECS environment we cannot say for certain which docker image is being run. We can
+ * make an assumption base on when the task definition was last updated and what image tag was selected in the Jenkins
+ * job.
+ *
+ * But it's only an assumption. Plus, there are times, for example after deploying an update that a response might have
+ * come from either the old or new image used for the ECS containers.
+ *
+ * So, we have created this plugin to read a git commit hash and a docker tag from env vars set when the image was
+ * built. This information is then included in two customer headers we add to _every_ response from the API. The
+ * delivery team can then use this information to help with debugging and investigating issues, and to confirm if a new
+ * version has been successfully deployed.
+ *
+ * ## To `x-` or not to `x-`
+ *
+ * There is a "convention" of prefixing custom headers with an `x-`.
+ * {@link http://tools.ietf.org/html/rfc6648|RFC 6648} deprecated that recommendation in 2012 and stated that custom
+ * headers _SHOULD NOT_ use `x-`. The argument is that if a custom header becomes a standard removing the `x-` breaks
+ * backwards compatibility.
+ *
+ * However, many have argued they never expect their custom headers to become standard. They are purely for use within
+ * their own domain. Using `x-` puts them at less risk of conflicting with a customer header that does become a standard
+ * in the future. Examples include
+ * {@link https://cloud.google.com/load-balancing/docs/custom-headers|Google documentation} which still recommends using
+ * `x-`. This {@link https://stackoverflow.com/q/3561381/6117745|stackoverflow post} includes detailed notes on the
+ * topic of whether to add them or not.
+ *
+ * After reading through them, we have come to the decision to prefix our custom headers with `x-cma`. To new developers
+ * and clients of the API aware of the convention it will be clear these are something custom we are adding to our
+ * responses. We are also 100% confident these will never become standard, so the risk of breaking backwards
+ * compatibility does not apply. The `-cma-` indicates it's a custom header set by us.
  * @module VersionInfoPlugin
  */
 
