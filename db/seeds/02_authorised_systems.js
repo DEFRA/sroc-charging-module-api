87731324ba9809a9da5f97126681af86fa472b41
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-03 22:56:24 +0000

    Update authorised_systems seed script
    Update the script which seeds the admin user to also automatically create the regime authorisations for it.

--- db/seeds/02_authorised_systems.js
+++ db/seeds/02_authorised_systems.js
@@ -4,13 +4,27 @@ const config = require('../../config/authentication.config')
 
 exports.seed = async function (knex) {
   await knex('authorised_systems').del()
+  await knex('authorised_systems_regimes').del()
 
-  await knex('authorised_systems').insert(
-    {
+  const result = await knex('authorised_systems')
+    .returning('id')
+    .insert({
       client_id: config.adminClientId,
       name: 'admin',
       admin: true,
       status: 'active'
-    }
-  )
+    })
+
+  const adminUserId = result[0]
+
+  // Get all regimes for their ID's
+  const regimes = await knex.select().from('regimes')
+
+  // Authorise the admin user for all regimes
+  for (const regime of regimes) {
+    await knex('authorised_systems_regimes').insert({
+      authorised_system_id: adminUserId,
+      regime_id: regime.id
+    })
+  }
 }
