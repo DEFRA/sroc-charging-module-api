c8b5a1fc6034432a22a5a1dd5f41c38a3a25fc8c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-08 15:07:30 +0000

    Add presenter implementation

---
+++ test/presenters/bill_run_status.presenter.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const { GeneralHelper } = require('../support/helpers')
+
+// Thing under test
+const { BillRunStatusPresenter } = require('../../app/presenters')
+
+describe('Bill run status Presenter', () => {
+  it("returns the 'status' of the bill run", () => {
+    const data = {
+      id: GeneralHelper.uuid4(),
+      region: 'A',
+      bill_run_number: 100001,
+      status: 'intialised'
+    }
+
+    const presenter = new BillRunStatusPresenter(data)
+
+    expect(presenter.go()).to.equal({
+      status: data.status
+    })
+  })
+})
