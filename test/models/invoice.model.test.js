d137bf29c0939bb02886527c0a7961dbf1ea3d1e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-09 11:12:32 +0000

    Add new InvoiceModel deminimis modifier test
    Hopefully with this new model based unit test we can see more clearly the current scenario.
    
    Both tests are failing because the current modifier is returning one of the other invoices.

---
+++ test/models/invoice.model.test.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const {
+  DatabaseHelper,
+  GeneralHelper,
+  InvoiceHelper
+} = require('../support/helpers')
+
+// Thing under test
+const { InvoiceModel } = require('../../app/models')
+
+describe.only('Invoice Model', () => {
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('Query modifiers', () => {
+    const billRunId = GeneralHelper.uuid4()
+
+    describe('#Deminimis', () => {
+      describe('when there is a mix of invoices', () => {
+        let deminimisInvoice
+
+        beforeEach(async () => {
+          deminimisInvoice = await InvoiceHelper.addInvoice(billRunId, 'CMA0000001', 2020, 0, 0, 1, 350, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000002', 2020, 0, 0, 1, 501, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000003', 2020, 1, 350, 0, 0, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000004', 2020, 1, 501, 0, 0, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000005', 2020, 0, 0, 0, 0, 1)
+        })
+
+        it("only returns those which are 'deminimis'", async () => {
+          const results = await InvoiceModel.query().modify('deminimis')
+
+          expect(results.length).to.equal(1)
+          expect(results[0].id).to.equal(deminimisInvoice.id)
+        })
+      })
+
+      describe('when there no matching invoices', () => {
+        beforeEach(async () => {
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000002', 2020, 0, 0, 1, 501, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000003', 2020, 1, 350, 0, 0, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000004', 2020, 1, 501, 0, 0, 0)
+          await InvoiceHelper.addInvoice(billRunId, 'CMA0000005', 2020, 0, 0, 0, 0, 1)
+        })
+
+        it('returns nothing', async () => {
+          const results = await InvoiceModel.query().modify('deminimis')
+
+          expect(results.length).to.equal(0)
+        })
+      })
+    })
+  })
+})
