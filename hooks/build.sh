77e79ab2a605a28194848f7ae530879a8ba41947
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-18 11:48:58 +0000

    Update build hook
    Adding `chown` to the copy commands has got DockerHub building. So, we are making final changes to the build hook to make it ready for merging to `main`.

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build --target production . --build-arg GIT_COMMIT=$SOURCE_COMMIT --build-arg DOCKER_TAG=$DOCKER_TAG
+repo_name="environmentagency/charging-module-api"
+
+git_image_tag="${repo_name}:${DOCKER_TAG}"
+
+docker build --target production -t $DOCKER_TAG . --build-arg GIT_COMMIT=$SOURCE_COMMIT --build-arg DOCKER_TAG=$git_image_tag
