dd981b8e37a2c25855450b548427f877a1f12817
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-19 13:08:54 +0000

    Add database migrations
    These migrations handle the renaming of the columns in the DB.

---
+++ db/migrations/20210219130108_alter_licences.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const tableName = 'licences'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .alterTable(tableName, table => {
+      table.renameColumn('credit_count', 'credit_line_count')
+      table.renameColumn('credit_value', 'credit_line_value')
+      table.renameColumn('debit_count', 'debit_line_count')
+      table.renameColumn('debit_value', 'debit_line_value')
+      table.renameColumn('zero_count', 'zero_line_count')
+    })
+}
+
+exports.down = async function (knex) {
+  await knex
+    .schema
+    .alterTable(tableName, table => {
+      table.renameColumn('credit_line_count', 'credit_count')
+      table.renameColumn('credit_line_value', 'credit_value')
+      table.renameColumn('debit_line_count', 'debit_count')
+      table.renameColumn('debit_line_value', 'debit_value')
+      table.renameColumn('zero_line_count', 'zero_count')
+    })
+}
