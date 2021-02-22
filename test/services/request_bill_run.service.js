aa3355f44d82acdbba2fb721b0a907b80bbc5b49
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-22 15:34:01 +0000

    Add next test - find matching bill run

--- test/services/request_bill_run.service.js
+++ test/services/request_bill_run.service.js
@@ -4,28 +4,41 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
+const { BillRunHelper, DatabaseHelper, GeneralHelper } = require('../support/helpers')
 
 // Thing under test
 const { RequestBillRunService } = require('../../app/services')
 
-describe.only('Request bill run service', () => {
-  describe("When the request is 'bill run' related", () => {
-    it("returns 'true'", async () => {
-      const result = await RequestBillRunService.go('/test/wrls/bill-runs/12345')
+describe('Request bill run service', () => {
+  let billRun
 
-      expect(result).to.be.true()
-    })
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
   })
 
   describe("When the request is 'bill run' related", () => {
-    it("returns 'false'", async () => {
+    describe("and is for a valid 'bill run'", () => {
+      beforeEach(async () => {
+        billRun = await BillRunHelper.addBillRun(GeneralHelper.uuid4(), GeneralHelper.uuid4())
+      })
+
+      it('returns the matching bill run', async () => {
+        const result = await RequestBillRunService.go(`/test/wrls/bill-runs/${billRun.id}`)
+
+        expect(result.id).to.equal(billRun.id)
+      })
+    })
+  })
+
+  describe("When the request isn't 'bill run' related", () => {
+    it("returns 'null'", async () => {
       const result = await RequestBillRunService.go('/test/wrls/invoice-runs/12345')
 
-      expect(result).to.be.false()
+      expect(result).to.be.null()
     })
   })
 })
