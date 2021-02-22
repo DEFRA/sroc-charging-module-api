6eea11f172c5ba6a913e7ba7a08a9c892877dcb7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-22 17:44:58 +0000

    Update plugin to use service
    This gets our original feature test passing but does break some others. Tha is to be expected.

--- app/plugins/bill_run.plugin.js
+++ app/plugins/bill_run.plugin.js
@@ -1,13 +1,13 @@
 'use strict'
 
+const { RequestBillRunService } = require('../services')
+
 const BillRunPlugin = {
   name: 'bill_run',
   register: (server, _options) => {
-    server.ext('onPreHandler', (request, h) => {
-      const billRunRegex = new RegExp(/\/bill-runs\//i)
-      if (billRunRegex.test(request.path)) {
-        console.log("It's a bill run")
-      }
+    server.ext('onPreHandler', async (request, h) => {
+      const { billRunId } = request.params
+      request.app.billRun = await RequestBillRunService.go(request.path, request.method, request.app.regime, billRunId)
 
       return h.continue
     })
