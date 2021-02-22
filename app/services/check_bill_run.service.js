31e3714da281fafb8415d603c7a538531fda70c5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-22 15:20:55 +0000

    Implement logic for determining if bill run
    We only want the service to get involved if the path is for `/bill-runs`.

--- app/services/check_bill_run.service.js
+++ app/services/check_bill_run.service.js
@@ -6,7 +6,13 @@
 
 class CheckBillRunService {
   static async go (path) {
-    return true
+    return this._billRunRelated(path)
+  }
+
+  static _billRunRelated (path) {
+    const billRunRegex = new RegExp(/\/bill-runs/i)
+
+    return billRunRegex.test(path)
   }
 }
 
