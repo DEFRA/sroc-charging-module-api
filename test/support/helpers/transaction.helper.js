f3ba773094272156180a540d85294de7b36149dd
Stuart Adair <stu@silverka.co.uk>
2021-02-23 17:31:14 +0000

    Transaction helper refactor

--- test/support/helpers/transaction.helper.js
+++ test/support/helpers/transaction.helper.js
@@ -11,7 +11,7 @@ const { presroc: chargeFixtures } = require('../fixtures/calculate_charge')
 
 class TransactionHelper {
   /**
-   * Create a transaction. If a overrides does not specify licenceId and invoiceId then a new licence will be created
+   * Create a transaction. If overrides does not specify licenceId and invoiceId then a new licence will be created
    * (which in turn will create a new invoice).
    *
    * @param {string} billRunId Id of an actual bill run. We have a foreign key constraint which forces this requirement
@@ -26,7 +26,8 @@ class TransactionHelper {
       .insert({
         ...this._defaultSimpleTransaction(billRunId),
         ...this._defaultSimpleCharge(),
-        ...await this._licenceAndInvoice(billRunId, overrides),
+        // If a licence id isn't specified in overrides then create a new licence and invoice and include their ids
+        ...!overrides.licenceId ? await this._newLicenceAndInvoice(billRunId, overrides) : {},
         ...overrides
       })
       .returning('*')
@@ -48,22 +49,16 @@ class TransactionHelper {
   }
 
   /**
-   * Returns licence and invoice ids. If addTransaction has been called with licenceId and invoiceId in overrides then
-   * these values will be returned. Otherwise, a new licence will be created which in turn will create a new invoice,
-   * and their ids will be returned.
+   * Creates a new licence (and in turn a new invoice) and returns their ids.
    */
-  static async _licenceAndInvoice (billRunId, overrides) {
-    const licence = overrides.licenceId
-      ? overrides.licenceId
-      : await LicenceHelper.addLicence(billRunId, 'LICENCE', null, requestFixtures.simple.customerReference, 2019)
-
-    const invoiceId = overrides.invoiceId
-      ? overrides.invoiceId
-      : licence.invoiceId
+  static async _newLicenceAndInvoice (billRunId, overrides) {
+    const { id: licenceId, invoiceId } = await LicenceHelper.addLicence(
+      billRunId, 'LICENCE', null, requestFixtures.simple.customerReference, 2019
+    )
 
     return {
-      licenceId: licence.id,
-      invoiceId: invoiceId
+      licenceId,
+      invoiceId
     }
   }
 }
