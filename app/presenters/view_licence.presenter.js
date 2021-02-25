aed8beab2d8b66e7c45a9cac9bc3f73aefb09b24
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-25 09:56:22 +0000

    Add view licence presenter (#230)
    https://trello.com/c/r9UClZob
    
    As part of working towards having a view invoice endpoint, we need to add a `ViewLicencePresenter`. Eventually, the view invoice endpoint will return a response that includes invoice, licence and transaction details. We'll need a presenter for each level to enable that though.
    
    So, this change adds a `ViewLicencePresenter` which is intended to be used by a `ViewInvoicePresenter`.

---
+++ app/presenters/view_licence.presenter.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * @module ViewLicencePresenter
+ */
+
+const BasePresenter = require('./base.presenter')
+const ViewTransactionPresenter = require('./view_transaction.presenter')
+
+/**
+ * Handles formatting the licence data into the response we send to clients when a GET request is received
+ */
+class ViewLicencePresenter extends BasePresenter {
+  _presentation (data) {
+    return {
+      id: data.id,
+      licenceNumber: data.licenceNumber,
+      creditLineCount: data.creditLineCount,
+      creditLineValue: data.creditLineValue,
+      debitLineCount: data.debitLineCount,
+      debitLineValue: data.debitLineValue,
+      zeroLineCount: data.zeroLineCount,
+      subjectToMinimumChargeCount: data.subjectToMinimumChargeCount,
+      netTotal: data.netTotal,
+      transactions: data.transactions.map(transaction => {
+        const presenter = new ViewTransactionPresenter(transaction)
+        return presenter.go()
+      })
+    }
+  }
+}
+
+module.exports = ViewLicencePresenter
