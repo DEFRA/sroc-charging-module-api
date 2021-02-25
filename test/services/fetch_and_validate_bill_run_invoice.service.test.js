0560ab11d0147810ee19dcf901e5cb026daa40a6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-02-25 11:24:41 +0000

    Add bill run invoice validator (#236)
    https://trello.com/c/r9UClZob
    https://trello.com/c/VV4vIldJ
    
    We are in the process of adding 2 new endpoints related to invoices
    
    - view an invoice `GET /v2/{regimeId}/bill-runs/{billRunId}/invoices/{invoiceId}`
    - delete an invoice `DELETE /v2/{regimeId}/bill-runs/{billRunId}/invoices/{invoiceId}`
    
    In both cases, we need to confirm that the invoice ID exists. But also that it is linked to the bill run specified. This is how we ensure that a client system cannot delete an invoice to which they don't have access.
    
    Every bill run related request is checked to ensure
    
    - the client system is authorised to access the specified regime
    - the bill run specified is linked to that regime
    
    As long as we check that the invoice is linked to the bill run, it means we are also checking that the client system has access to the invoice.
    
    So, because we know we are going to need to do the same checks in 2 different places this change adds a new validation service. It will
    
    - find the ID (and throw a `404` if not found)
    - check it is linked to the bill run (and throw a `401` if it's not)

---
+++ test/services/fetch_and_validate_bill_run_invoice.service.test.js
@@ -0,0 +1,74 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const {
+  AuthorisedSystemHelper,
+  BillRunHelper,
+  DatabaseHelper,
+  GeneralHelper,
+  InvoiceHelper,
+  RegimeHelper
+} = require('../support/helpers')
+
+// Thing under test
+const { FetchAndValidateBillRunInvoiceService } = require('../../app/services')
+
+describe('Fetch and Validate Bill Run Invoice service', () => {
+  let billRun
+  let authorisedSystem
+  let regime
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    regime = await RegimeHelper.addRegime('wrls', 'WRLS')
+    authorisedSystem = await AuthorisedSystemHelper.addSystem('1234546789', 'system1', [regime])
+  })
+
+  describe('When a valid bill run ID is supplied', () => {
+    beforeEach(async () => {
+      billRun = await BillRunHelper.addBillRun(authorisedSystem.id, regime.id)
+    })
+
+    describe('and a valid invoice ID', () => {
+      it("returns the validated 'invoice'", async () => {
+        const invoice = await InvoiceHelper.addInvoice(billRun.id, 'CUSTOMER REFERENCE', 2020)
+
+        const result = await FetchAndValidateBillRunInvoiceService.go(billRun.id, invoice.id)
+
+        expect(result.id).to.equal(invoice.id)
+      })
+    })
+
+    describe('and an invalid invoice ID', () => {
+      describe('because it is unknown', () => {
+        it('throws an error', async () => {
+          const unknownInvoiceId = GeneralHelper.uuid4()
+          const err = await expect(FetchAndValidateBillRunInvoiceService.go(billRun.id, unknownInvoiceId)).to.reject()
+
+          expect(err).to.be.an.error()
+          expect(err.output.payload.message).to.equal(`Invoice ${unknownInvoiceId} is unknown.`)
+        })
+      })
+      describe('because it is not linked to the bill run', () => {
+        it('throws an error', async () => {
+          const otherBillRun = await BillRunHelper.addBillRun(authorisedSystem.id, regime.id)
+          const invoice = await InvoiceHelper.addInvoice(otherBillRun.id, 'CUSTOMER REFERENCE', 2020)
+          const err = await expect(FetchAndValidateBillRunInvoiceService.go(billRun.id, invoice.id)).to.reject()
+
+          expect(err).to.be.an.error()
+          expect(err.output.payload.message).to.equal(
+            `Invoice ${invoice.id} is not linked to bill run ${billRun.id}.`
+          )
+        })
+      })
+    })
+  })
+})
