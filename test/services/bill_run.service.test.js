3e0c27a6005fad475491b0a233513abf25461304
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-01 23:45:02 +0000

    Update BillRunService tests
    Need to update it to as the service now expects an instance of bill run. We also drop validation tests the service will no longer cover.

--- test/services/bill_run.service.test.js
+++ test/services/bill_run.service.test.js
@@ -39,14 +39,14 @@ describe('Bill Run service', () => {
     })
 
     it('returns the matching bill run', async () => {
-      const result = await BillRunService.go(transaction)
+      const result = await BillRunService.go(billRun, transaction)
 
       expect(result.id).to.equal(billRun.id)
     })
 
     describe('When a debit transaction is supplied', () => {
       it('correctly calculates the summary', async () => {
-        const result = await BillRunService.go(transaction)
+        const result = await BillRunService.go(billRun, transaction)
 
         expect(result.debitLineCount).to.equal(1)
         expect(result.debitLineValue).to.equal(transaction.chargeValue)
@@ -57,7 +57,7 @@ describe('Bill Run service', () => {
       it('correctly calculates the summary', async () => {
         transaction.chargeCredit = true
 
-        const result = await BillRunService.go(transaction)
+        const result = await BillRunService.go(billRun, transaction)
 
         expect(result.creditLineCount).to.equal(1)
         expect(result.creditLineValue).to.equal(transaction.chargeValue)
@@ -68,7 +68,7 @@ describe('Bill Run service', () => {
       it('correctly calculates the summary', async () => {
         transaction.chargeValue = 0
 
-        const result = await BillRunService.go(transaction)
+        const result = await BillRunService.go(billRun, transaction)
 
         expect(result.zeroLineCount).to.equal(1)
       })
@@ -80,7 +80,7 @@ describe('Bill Run service', () => {
       })
 
       it('correctly sets the subject to minimum charge flag', async () => {
-        const result = await BillRunService.go(transaction)
+        const result = await BillRunService.go(billRun, transaction)
 
         expect(result.subjectToMinimumChargeCount).to.equal(1)
       })
@@ -91,11 +91,11 @@ describe('Bill Run service', () => {
         })
 
         it('correctly calculates the total for a debit', async () => {
-          const firstResult = await BillRunService.go(transaction)
+          const firstResult = await BillRunService.go(billRun, transaction)
           // We save the invoice with stats to the database as this isn't done by BillRunService
           await BillRunModel.query().update(firstResult)
 
-          const secondResult = await BillRunService.go(transaction)
+          const secondResult = await BillRunService.go(billRun, transaction)
 
           expect(secondResult.subjectToMinimumChargeCount).to.equal(2)
           expect(secondResult.subjectToMinimumChargeDebitValue).to.equal(transaction.chargeValue * 2)
@@ -104,11 +104,11 @@ describe('Bill Run service', () => {
         it('correctly calculates the total for a credit', async () => {
           transaction.chargeCredit = true
 
-          const firstResult = await BillRunService.go(transaction)
+          const firstResult = await BillRunService.go(billRun, transaction)
           // We save the invoice with stats to the database as this isn't done by BillRunService
           await BillRunModel.query().update(firstResult)
 
-          const secondResult = await BillRunService.go(transaction)
+          const secondResult = await BillRunService.go(billRun, transaction)
 
           expect(secondResult.subjectToMinimumChargeCount).to.equal(2)
           expect(secondResult.subjectToMinimumChargeCreditValue).to.equal(transaction.chargeValue * 2)
@@ -119,11 +119,11 @@ describe('Bill Run service', () => {
     describe('When two transactions are created', () => {
       it('correctly calculates the summary', async () => {
         transaction.billRunId = billRun.id
-        const firstResult = await BillRunService.go(transaction)
+        const firstResult = await BillRunService.go(billRun, transaction)
         // We save the invoice with stats to the database as this isn't done by BillRunService
         await BillRunModel.query().update(firstResult)
 
-        const secondResult = await BillRunService.go(transaction)
+        const secondResult = await BillRunService.go(billRun, transaction)
 
         expect(secondResult.debitLineCount).to.equal(2)
         expect(secondResult.debitLineValue).to.equal(transaction.chargeValue * 2)
@@ -132,36 +132,6 @@ describe('Bill Run service', () => {
   })
 
   describe('When an invalid bill run ID is supplied', () => {
-    const unknownBillRunId = GeneralHelper.uuid4()
-
-    describe('because no matching bill run exists', () => {
-      it('throws an error', async () => {
-        const transaction = { ...dummyTransaction, billRunId: unknownBillRunId }
-
-        const err = await expect(BillRunService.go(transaction)).to.reject()
-
-        expect(err).to.be.an.error()
-        expect(err.output.payload.message).to.equal(`Bill run ${unknownBillRunId} is unknown.`)
-      })
-    })
-
-    describe('because the bill run is not editable', () => {
-      beforeEach(async () => {
-        billRun = await BillRunHelper.addBillRun(authorisedSystemId, regimeId, 'A', 'billed')
-      })
-
-      it('throws an error', async () => {
-        const transaction = { ...dummyTransaction, billRunId: billRun.id }
-
-        const err = await expect(BillRunService.go(transaction)).to.reject()
-
-        expect(err).to.be.an.error()
-        expect(err.output.payload.message)
-          .to
-          .equal(`Bill run ${billRun.id} cannot be edited because its status is billed.`)
-      })
-    })
-
     describe('because the bill run is for a different region', () => {
       beforeEach(async () => {
         billRun = await BillRunHelper.addBillRun(authorisedSystemId, regimeId, 'W')
@@ -170,7 +140,7 @@ describe('Bill Run service', () => {
       it('throws an error', async () => {
         const transaction = { ...dummyTransaction, billRunId: billRun.id }
 
-        const err = await expect(BillRunService.go(transaction)).to.reject()
+        const err = await expect(BillRunService.go(billRun, transaction)).to.reject()
 
         expect(err).to.be.an.error()
         expect(err.output.payload.message)
