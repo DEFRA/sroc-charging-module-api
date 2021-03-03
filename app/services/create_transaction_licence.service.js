13530b6bcdcd5a6a8c13a4ff3d8e33516cfc6dcf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-03 23:36:59 +0000

    Update documentation

--- app/services/create_transaction_licence.service.js
+++ app/services/create_transaction_licence.service.js
@@ -9,14 +9,29 @@ const CreateTransactionTallyService = require('./create_transaction_tally.servic
 
 class CreateTransactionLicenceService {
   /**
-  * Accepts a transaction and creates an entry in the Licences table if one doesn't already exist for the transaction's
-  * invoice/licence/customer ref/financial year. It updates the count and value stats as per the transaction details
-  * and returns the resulting licence.
+  * Creates or finds the licence record then generates and returns a 'patch' object to be used update it based on values
+  * in the transaction
   *
-  * Note that the updated stats are _not_ saved back to the database; it is up to the caller to do this.
+  * The service accepts a transaction and creates an entry in the `licences` table if one doesn't already exist for the
+  * transaction's 'invoice/licence/customer ref/financial year' combo. It then generates a 'patch' object intended to be
+  * used in a call to `LicenceModel.query().patch()`. The 'patch' object has 2 properties
   *
-  * @param {Object} A transaction object
-  * @returns {Object} A licence object
+  * - the ID of the licence to update (determined by either the fetched or created `licence`)
+  * - a child object specifiying which fields to update and how
+  *
+  * A full example would be
+  *
+  * ```
+  * const patchObject = await CreateTransactionLicenceService.go(transaction)
+  * await LicenceModel.query().findById(patchObject.id).patch(patchObject.update)
+  * ```
+  *
+  * Note - Our experience is that patching a record in this way is more performant than updating the instance and
+  * calling `$patch()` on it.
+  *
+  * @param {module:TransactionTranslator} transaction translator representing the transaction to be added
+  *
+  * @returns {Object} an object that contains the ID of the licence to be updated, and the updates to be applied
   */
 
   static async go (transaction) {
