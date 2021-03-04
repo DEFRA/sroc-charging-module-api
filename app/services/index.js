f264ee2a0e59dc5fbdc18f3a8d04087ac399f615
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-03-04 11:13:59 +0000

    Create Customer Details service (#262)
    https://trello.com/c/i7tD1V78/1922-m-change-customer-details-v2
    
    This change implements CreateCustomerDetailsService, which accepts a payload along with a regime object and creates an entry in the customers table.

--- app/services/index.js
+++ app/services/index.js
@@ -8,6 +8,7 @@ const CalculateMinimumChargeService = require('./calculate_minimum_charge.servic
 const CognitoJwtToPemService = require('./cognito_jwt_to_pem.service')
 const CreateAuthorisedSystemService = require('./create_authorised_system.service')
 const CreateBillRunService = require('./create_bill_run.service')
+const CreateCustomerDetailsService = require('./create_customer_details.service')
 const CreateMinimumChargeAdjustmentService = require('./create_minimum_charge_adjustment.service')
 const CreateTransactionBillRunService = require('./create_transaction_bill_run.service')
 const CreateTransactionInvoiceService = require('./create_transaction_invoice.service')
@@ -40,6 +41,7 @@ module.exports = {
   CalculateMinimumChargeService,
   CognitoJwtToPemService,
   CreateAuthorisedSystemService,
+  CreateCustomerDetailsService,
   CreateMinimumChargeAdjustmentService,
   CreateBillRunService,
   CreateTransactionBillRunService,
