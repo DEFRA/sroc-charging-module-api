c5b55868a2dd93141d70382fcf3f8d313a1eebd5
Stuart Adair <stu@silverka.co.uk>
2021-03-08 17:06:08 +0000

    Validate bill run

--- app/services/delete_bill_run.service.js
+++ app/services/delete_bill_run.service.js
@@ -4,6 +4,8 @@
  * @module DeleteInvoiceService
  */
 
+const Boom = require('@hapi/boom')
+
 const { BillRunModel } = require('../models')
 
 class DeleteBillRunService {
@@ -13,10 +15,17 @@ class DeleteBillRunService {
    * @param {@module:BillRunModel} billRun The bill run to be deleted.
    */
   static async go (billRun) {
-    // First ensure we can delete the bill run!
+    this._validate(billRun)
+
     await BillRunModel
       .query()
       .deleteById(billRun.id)
+  }
+
+  static _validate (billRun) {
+    if (billRun.$billed()) {
+      throw Boom.conflict(`Bill run ${billRun.id} has a status of 'billed'.`)
+    }
   }
 }
 
