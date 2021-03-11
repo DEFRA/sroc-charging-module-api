f7b50038fe6155a293cc5913f3e3d7f3378a13a6
Stuart Adair <stu@silverka.co.uk>
2021-03-11 13:19:22 +0000

    Add filesystem mocking to test

--- test/services/generate_transaction_file.service.test.js
+++ test/services/generate_transaction_file.service.test.js
@@ -4,8 +4,10 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script()
 const { expect } = Code
+
+const mockFs = require('mock-fs')
 
 const fs = require('fs')
 const path = require('path')
@@ -18,6 +20,17 @@ const { GenerateTransactionFileService } = require('../../app/services')
 describe('Generate Transaction File service', () => {
   const filename = 'test.txt'
   const filenameWithPath = path.join(temporaryFilePath, filename)
+
+  beforeEach(async () => {
+    // Create mock in-memory file system to avoid temp files being dropped in our filesystem
+    mockFs({
+      tmp: { }
+    })
+  })
+
+  afterEach(async () => {
+    mockFs.restore()
+  })
 
   it('creates a file with expected content', async () => {
     await GenerateTransactionFileService.go(filename)
