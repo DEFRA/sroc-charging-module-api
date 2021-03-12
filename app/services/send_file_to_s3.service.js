c6bf8ba64891997f47b1bb023d62ca85971617ea
Stuart Adair <stu@silverka.co.uk>
2021-03-12 10:42:01 +0000

    SendFileToS3Service stub

---
+++ app/services/send_file_to_s3.service.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * @module SendFileToS3Service
+ */
+
+class SendFileToS3Service {
+  /**
+   * Send a file to the AWS S3 bucket
+  */
+  static async go (regime, billRun) {
+
+  }
+}
+
+module.exports = SendFileToS3Service
