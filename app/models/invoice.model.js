06375bd2984e5d77b9292092682db2fac37db484
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-14 23:51:32 +0000

    Add supporting methods to Invoice and LicenceModel
    To generating the `UPSERT` statements we need to know specifics about the record we are 'upserting'. What is the name of the fields that will cause the `ON CONFLICT` constraint to fire? What fields need to be populated in the `INSERT` statement?
    
    The `CreateTransactionUpsertService` won't know this. But it will know which record type it is creating it for via its `modelClass` param. Rather than have the service, for example, determine the fields used in the constraint for each class, it just asks the class to tell it.

--- app/models/invoice.model.js
+++ app/models/invoice.model.js
@@ -114,6 +114,40 @@ class InvoiceModel extends BaseModel {
   }
 
   /**
+   * Returns an array of column names that are used for the unique constraint which groups invoices to transactions
+   *
+   * For each grouping of customer reference and financial year in a bill run we generate an 'invoice' and link the
+   * relevant transactions to it.
+   *
+   * We need this information when adding transactions to help generate the 'UPSERT' query we use to either create the
+   * invoice record for the first time, or update it as requests are received.
+   *
+   * @returns {string[]} an array of field names
+   */
+  static get transactionConstraintFields () {
+    return ['bill_run_id', 'customer_reference', 'financial_year']
+  }
+
+  /**
+   * Returns an object that contains the minimum (base) properties and values needed when inserting a new invoice
+   *
+   * Built for when adding a transaction to a bill run. We also create an `invoice` record for each customer reference
+   * and financial year grouping in a bill run. We need to do this as part of a PostgreSQL 'UPSERT' call in order to
+   * support new concurrent requests for the same invoice.
+   *
+   * It contains the base properties that must be set when the invoice record is first inserted into the DB.
+   *
+   * @return {Object} object that can built on and used with an Objection or Knex `.insert()` call
+   */
+  static createBaseOnInsertObject (transaction) {
+    return {
+      billRunId: transaction.billRunId,
+      customerReference: transaction.customerReference,
+      financialYear: transaction.chargeFinancialYear
+    }
+  }
+
+  /**
    * netTotal method provides the net total of the invoice (debit value - credit value)
    */
   $netTotal () {
--- app/models/licence.model.js
+++ app/models/licence.model.js
@@ -42,6 +42,40 @@ class LicenceModel extends BaseModel {
   }
 
   /**
+   * Returns an array of column names that are used for the unique constraint which groups licences to invoices
+   *
+   * For each grouping of invoice ID and licence number in a bill run we generate a 'licence' and link the
+   * relevant transactions to it.
+   *
+   * We need this information when adding transactions to help generate the 'UPSERT' query we use to either create the
+   * licence record for the first time, or update it as requests are received.
+   *
+   * @returns {string[]} an array of field names
+   */
+  static get transactionConstraintFields () {
+    return ['invoice_id', 'licence_number']
+  }
+
+  /**
+   * Returns an object that contains the minimum (base) properties and values needed when inserting a new licence
+   *
+   * Built for when adding a transaction to a bill run. We also create an `licence` record for each invoice ID
+   * and licence number grouping in a bill run. We need to do this as part of a PostgreSQL 'UPSERT' call in order to
+   * support new concurrent requests for the same licence.
+   *
+   * It contains the base properties that must be set when the licence record is first inserted into the DB.
+   *
+   * @return {Object} object that can built on and used with an Objection or Knex `.insert()` call
+   */
+  static createBaseOnInsertObject (transaction) {
+    return {
+      billRunId: transaction.billRunId,
+      invoiceId: transaction.invoiceId,
+      licenceNumber: transaction.lineAttr1
+    }
+  }
+
+  /**
    * netTotal method provides the net total of the licence (debit value - credit value)
    */
   $netTotal () {
