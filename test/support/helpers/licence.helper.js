55edd10213a7eb72390d39a4b54fb0df7cf036b8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-16 14:22:56 +0000

    Update LicenceHelper

--- test/support/helpers/licence.helper.js
+++ test/support/helpers/licence.helper.js
@@ -14,6 +14,16 @@ class LicenceHelper {
    * @param {string} [invoiceId] Id to use for the `invoice_id` field. If empty then a new invoice will be created.
    * @param {string} [customerReference] Customer reference to use if an invoice is to be created.
    * @param {integer} [financialYear] Financial year to use if an invoice is to be created.
+   * @param {integer} [creditLineCount] Number of credits in the licence.
+   * @param {integer} [creditLineValue] Total value of credits in the licence.
+   * @param {integer} [debitLineCount] Number of debits in the licence.
+   * @param {integer} [debitLineValue] Total value of debits in the licence.
+   * @param {integer} [zeroLineCount] Number of zero value transactions in the licence.
+   * @param {integer} [subjectToMinimumChargeCount] Number of transactions flagged as 'miniumum charge' in the licence.
+   * @param {integer} [subjectToMinimumChargeCreditValue] Total value of minimum charge credit transactions in the
+   *  licence.
+   * @param {integer} [subjectToMinimumChargeDebitValue] Total value of minimum charge debit transactions in the
+   *  licence.
    *
    * @returns {module:LicenceModel} The newly created instance of `LicenceModel`.
    */
@@ -22,13 +32,29 @@ class LicenceHelper {
     licenceNumber,
     invoiceId = '',
     customerReference = '',
-    financialYear = ''
+    financialYear = '',
+    creditLineCount = 0,
+    creditLineValue = 0,
+    debitLineCount = 0,
+    debitLineValue = 0,
+    zeroLineCount = 0,
+    subjectToMinimumChargeCount = 0,
+    subjectToMinimumChargeCreditValue = 0,
+    subjectToMinimumChargeDebitValue = 0
   ) {
     return LicenceModel.query()
       .insert({
         billRunId,
         licenceNumber,
-        invoiceId: await this._invoiceId(invoiceId, billRunId, customerReference, financialYear)
+        invoiceId: await this._invoiceId(invoiceId, billRunId, customerReference, financialYear),
+        creditLineCount,
+        creditLineValue,
+        debitLineCount,
+        debitLineValue,
+        zeroLineCount,
+        subjectToMinimumChargeCount,
+        subjectToMinimumChargeCreditValue,
+        subjectToMinimumChargeDebitValue
       })
       .returning('*')
   }
