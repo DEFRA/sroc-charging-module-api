cfb5d652917596442bff5953762b01bf04526201
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-17 21:40:44 +0000

    Implement base upsert functionality
    Add to the `BaseUpsertModel` our shared UPSERT functionality as well as define the methods we expect the extending class to implement.
    
    We also add some tests for the base model's functions to confirm they will throw errors if not overriden. The rest of the base class we expect to be tested through the classes that extend it, which we are comfortable with.

--- app/models/base_upsert.model.js
+++ app/models/base_upsert.model.js
@@ -7,6 +7,41 @@
 const BaseModel = require('./base.model')
 
 class BaseUpsertModel extends BaseModel {
+  static async updateTally (transaction, trx) {
+    const { CreateTransactionTallyService } = require('../services')
+
+    const tallyObject = CreateTransactionTallyService.go(transaction, this.tableName)
+    Object.assign(tallyObject.insertData, this._baseOnInsertObject(transaction))
+
+    const sql = `${this.knexQuery().insert(tallyObject.insertData).toQuery()}
+      ON CONFLICT (${this._onConflictContraints().join(', ')})
+      DO UPDATE SET ${tallyObject.updateStatements.join(', ')}
+      RETURNING id;`
+
+    const result = await this._applyUpsert(sql, trx)
+
+    return result.rows[0].id
+  }
+
+  static _baseOnInsertObject (_transaction) {
+    throw new Error("Extending class must implement '_baseOnInsertObject()'")
+  }
+
+  static _onConflictContraints () {
+    throw new Error("Extending class must implement '_onConflictContraints()'")
+  }
+
+  static async _applyUpsert (sql, trx) {
+    let result
+
+    if (trx) {
+      result = await this.knex().raw(sql).transacting(trx)
+    } else {
+      result = await this.knex().raw(sql)
+    }
+
+    return result
+  }
 }
 
 module.exports = BaseUpsertModel
