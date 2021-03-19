f59570f7f849861adcf70452aa317247e59cbe91
Stuart Adair <stu@silverka.co.uk>
2021-03-19 09:34:27 +0000

    Fix small merge issue

--- app/services/index.js
+++ app/services/index.js
@@ -30,7 +30,6 @@ const ObjectCleaningService = require('./object_cleaning.service')
 const RequestBillRunService = require('./request_bill_run.service')
 const RulesService = require('./rules.service')
 const SendBillRunReferenceService = require('./send_bill_run_reference.service')
-const SendTransactionFileService = require('./send_transaction_file.service')
 const SendFileToS3Service = require('./send_file_to_s3.service')
 const SendTransactionFileService = require('./send_transaction_file.service')
 const ShowAuthorisedSystemService = require('./show_authorised_system.service')
@@ -70,7 +69,6 @@ module.exports = {
   NextFileReferenceService,
   NextTransactionReferenceService,
   SendBillRunReferenceService,
-  SendTransactionFileService,
   SendFileToS3Service,
   SendTransactionFileService,
   ShowAuthorisedSystemService,
