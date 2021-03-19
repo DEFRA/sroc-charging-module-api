b34e7345efd4dace9ba5538e23ea7f3d511108ed
Stuart Adair <stu@silverka.co.uk>
2021-03-19 17:13:24 +0000

    Create DeleteFileService

---
+++ test/services/delete_file.service.test.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+const mockFs = require('mock-fs')
+
+const fs = require('fs')
+const path = require('path')
+
+// Thing under test
+const { DeleteFileService } = require('../../app/services')
+
+describe('Delete File service', () => {
+  let filenameWithPath
+
+  beforeEach(() => {
+    filenameWithPath = path.join('testFolder', 'testFile')
+
+    mockFs({
+      testFolder: {
+        testFile: 'test content'
+      }
+    })
+  })
+
+  afterEach(() => {
+    mockFs.restore()
+  })
+
+  describe('When a valid file is specified', () => {
+    it('deletes the file', async () => {
+      await DeleteFileService.go(filenameWithPath)
+
+      const fileExists = fs.existsSync(filenameWithPath)
+      expect(fileExists).to.be.false()
+    })
+  })
+
+  describe('When an error occurs', () => {
+    it('throws an error', async () => {
+      const fakeFile = 'FAKE_FILE'
+
+      const err = await expect(DeleteFileService.go(fakeFile)).to.reject()
+
+      expect(err).to.be.an.error()
+      expect(err.message).to.equal(`Error: ENOENT: no such file or directory, unlink '${fakeFile}'`)
+    })
+  })
+})
