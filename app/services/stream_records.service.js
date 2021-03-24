289bc3585d00fdfefb887234eb08b0d0f831fc73
Stuart Adair <stu@silverka.co.uk>
2021-03-24 11:18:46 +0000

    StreamRecordsService amendments

--- app/services/stream_records.service.js
+++ app/services/stream_records.service.js
@@ -5,7 +5,7 @@
  */
 
 /**
- * Returns a stream of database records when given an Ojbection QueryBuilder object.
+ * Returns a stream of database records when given an Objection QueryBuilder object.
  *
  * Note that we must pass in a query and not the result of a query -- therefore it should be instantiated _without_
  * `await` ie:
@@ -13,11 +13,13 @@
  *  StreamRecordsService.go(query)
  *
  * @param {module:QueryBuilder} query Objection QueryBuilder object of the query you wish to stream.
- * @returns {} A stream of database records.
+ * @returns {ReadableStream} A stream of database records.
  */
 class StreamRecordsService {
-  static async go (query) {
-    return query.toKnexQuery().stream()
+  static go (query) {
+    return query
+      .toKnexQuery()
+      .stream()
   }
 }
 
