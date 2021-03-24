baa81fd6e023261e57d30513300f2fa6f41fd653
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-03-24 22:42:38 +0000

    Add documentation to NotifierPlugin

--- app/plugins/notifier.plugin.js
+++ app/plugins/notifier.plugin.js
@@ -1,6 +1,11 @@
 'use strict'
 
 /**
+ * Add an instance of `Notifier` to the `request.app` as part of every request.
+ *
+ * This makes writing to the log and sending notifications to Errbit more accesible, easier to use, and more consistent
+ * in the controllers and their underlying services.
+ *
  * @module NotifierPlugin
  */
 
