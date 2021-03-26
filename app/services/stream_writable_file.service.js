667313067ad93076d088970b4ae2dedc7a36330a
Stuart Adair <stu@silverka.co.uk>
2021-03-26 14:52:36 +0000

    Comment amendment

--- app/services/stream_writable_file.service.js
+++ app/services/stream_writable_file.service.js
@@ -6,6 +6,7 @@
 
 const fs = require('fs')
 
+class StreamWritableFileService {
 /**
  * Returns a Writable stream which writes data supplied to it to a file.
  *
@@ -13,12 +14,6 @@ const fs = require('fs')
  * @param {boolean} [append] Whether incoming data should be appended to an existing file. Defaults to `false`.
  * @returns {WritableStream} A Writable stream.
  */
-
-/**
-   * Writable stream that writes to a given file. Defaults to overwriting the content of the existing file; pass 'true'
-   * as the second parameter to append the data instead.
-   */
-class StreamWritableFileService {
   static go (filenameWithPath, append = false) {
     return fs.createWriteStream(filenameWithPath, append ? { flags: 'a' } : {})
   }
