e5e1e1df6302721eb6bed50ee6e80f0170bb567c
Stuart Adair <stu@silverka.co.uk>
2021-03-26 16:18:18 +0000

    Accidentally edited and pushed the wrong tests 🙈

--- test/services/streams/stream_readable_records.service.test.js
+++ test/services/streams/stream_readable_records.service.test.js
@@ -14,6 +14,7 @@ const {
   BillRunHelper,
   DatabaseHelper,
   GeneralHelper,
+  StreamHelper,
   TransactionHelper
 } = require('../../support/helpers')
 
@@ -24,22 +25,32 @@ const { StreamReadableRecordsService } = require('../../../app/services')
 
 describe.only('Stream Readable Records service', () => {
   let billRun
+  let transaction
 
   beforeEach(async () => {
     await DatabaseHelper.clean()
 
     billRun = await BillRunHelper.addBillRun(GeneralHelper.uuid4(), GeneralHelper.uuid4())
 
-    await TransactionHelper.addTransaction(billRun.id)
+    transaction = await TransactionHelper.addTransaction(billRun.id)
   })
 
   describe('When a valid query builder object specified', () => {
     it('returns a stream', async () => {
       const query = TransactionModel.query().select('*')
 
-      const result = await StreamReadableRecordsService.go(query)
+      const result = StreamReadableRecordsService.go(query)
 
       expect(result).to.be.an.instanceof(stream.Stream)
+    })
+
+    it('streams the correct data', async () => {
+      const query = TransactionModel.query().select('*')
+
+      const readableStream = StreamReadableRecordsService.go(query)
+      const result = await StreamHelper.returnReadableStreamData(readableStream)
+
+      expect(result[0].id).to.equal(transaction.id)
     })
   })
 })
