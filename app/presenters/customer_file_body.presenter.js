3a3cef5167ef470019a030866aca06ba1df2be2f
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-04-08 17:50:02 +0100

    Presenters for writing customer files (#347)
    https://trello.com/c/CSrOSDTP/1942-s-develop-generate-customer-files-v2
    
    This branch creates presenters which will transform data in the customer table into the format required for customer files.
    
    It also introduces PresenterHelper with a generateNumberedColumns method which can be used to generate the arrays of column names (eg. ['col01', 'col02', 'col03'] used in all of the file presenter tests, and refactors the existing transaction file tests to use it.

---
+++ app/presenters/customer_file_body.presenter.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/**
+ * @module CustomerFileBodyPresenter
+ */
+
+const BasePresenter = require('./base.presenter')
+
+/**
+ * Formats data for the body of a customer file.
+ *
+ * Note that data.index is added by StreamTransformUsingPresenter and is not part of the data originally read from the
+ * source customer record.
+ *
+ * With reference to the existing v1 charging module customer file presenter:
+ * https://github.com/DEFRA/charging-module-api/blob/main/app/schema/pre_sroc/wrls/customer_file_presenter.js
+ */
+
+class CustomerFileBodyPresenter extends BasePresenter {
+  _presentation (data) {
+    return {
+      col01: 'D',
+      col02: this._leftPadZeroes(data.index, 7),
+      col03: data.customerReference,
+      col04: data.customerName,
+      col05: data.addressLine1,
+      col06: this._cleanseNull(data.addressLine2),
+      col07: this._cleanseNull(data.addressLine3),
+      col08: this._cleanseNull(data.addressLine4),
+      col09: this._cleanseNull(data.addressLine5),
+      col10: this._cleanseNull(data.addressLine6),
+      col11: this._handlePostcode(data.postcode)
+    }
+  }
+
+  /**
+   * Returns the trimmed postcode, or '.' if the postcode is either null or '' after trimming
+   */
+  _handlePostcode (postcode) {
+    return postcode?.trim() ? postcode.trim() : '.'
+  }
+}
+
+module.exports = CustomerFileBodyPresenter
---
+++ app/presenters/customer_file_tail.presenter.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/**
+ * @module CustomerFileTailPresenter
+ */
+
+const BasePresenter = require('./base.presenter')
+
+/**
+ * Formats data for the tail of a customer file.
+ *
+ * Note that data.index is added by StreamTransformUsingPresenter and is not part of the data originally read from the
+ * source customer record.
+ *
+ * With reference to the existing v1 charging module customer file presenter:
+ * https://github.com/DEFRA/charging-module-api/blob/main/app/schema/pre_sroc/wrls/customer_file_presenter.js
+ */
+
+class CustomerFileTailPresenter extends BasePresenter {
+  _presentation (data) {
+    return {
+      col01: 'T',
+      col02: this._leftPadZeroes(data.index, 7),
+      col03: this._numberOfLinesInFile(data.index)
+    }
+  }
+
+  /**
+   * Returns the number of lines in the file, which we calculate as the current row index + 1 (since index starts at 0)
+   */
+  _numberOfLinesInFile (index) {
+    return index + 1
+  }
+}
+
+module.exports = CustomerFileTailPresenter
