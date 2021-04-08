53ee37cefba8799290317e44cc1904a9ee0e8c90
Stuart Adair <stu@silverka.co.uk>
2021-04-08 16:12:37 +0100

    Complete file tail presenter tests

--- test/presenters/customer_file_tail.presenter.test.js
+++ test/presenters/customer_file_tail.presenter.test.js
@@ -10,32 +10,28 @@ const { expect } = Code
 const { PresenterHelper } = require('../support/helpers')
 
 // Thing under test
-const { TransactionFileTailPresenter } = require('../../app/presenters')
+const { CustomerFileTailPresenter } = require('../../app/presenters')
 
-describe('Transaction File Tail Presenter', () => {
+describe('Customer File Tail Presenter', () => {
   const data = {
-    index: 3,
-    invoiceValue: 12345,
-    creditNoteValue: 67890
+    index: 3
   }
 
   it('returns the required columns', () => {
-    const presenter = new TransactionFileTailPresenter(data)
+    const presenter = new CustomerFileTailPresenter(data)
     const result = presenter.go()
 
-    const expectedFields = PresenterHelper.generateNumberedCols(5)
+    const expectedFields = PresenterHelper.generateNumberedCols(3)
 
     expect(result).to.only.include(expectedFields)
   })
 
   it('returns the correct values', () => {
-    const presenter = new TransactionFileTailPresenter(data)
+    const presenter = new CustomerFileTailPresenter(data)
     const result = presenter.go()
 
     expect(result.col01).to.equal('T')
     expect(result.col02).to.equal('0000003')
     expect(result.col03).to.equal(4)
-    expect(result.col04).to.equal(data.invoiceValue)
-    expect(result.col05).to.equal(-data.creditNoteValue)
   })
 })
