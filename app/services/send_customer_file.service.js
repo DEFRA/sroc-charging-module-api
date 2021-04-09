cfa6fc360cb68690ff8ac522307b22f4aedf3840
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-04-09 17:07:04 +0100

    Create Send Customer File Service (#346)
    https://trello.com/c/CSrOSDTP/1942-s-develop-generate-customer-files-v2
    
    Next we want to create SendCustomerFileService which can generate and send a customer file.
    
    The service is similar to SendTransactionFileService with a few key differences:
    - Multiple files can be generated if more than one region is passed in (intended to be used by the weekly automated file generation);
    - Entries are deleted from the customers table after being sent;
    - The file reference for the filename is obtained within the service, whereas transaction files take the filename from the bill run.

--- app/services/send_customer_file.service.js
+++ app/services/send_customer_file.service.js
@@ -4,11 +4,112 @@
  * @module SendCustomerFileService
  */
 
+const path = require('path')
+
+const { ServerConfig } = require('../../config')
+const { removeTemporaryFiles } = ServerConfig
+
+const { CustomerModel } = require('../../app/models')
+
+const GenerateCustomerFileService = require('./generate_customer_file.service')
+const SendFileToS3Service = require('./send_file_to_s3.service')
+const DeleteFileService = require('./delete_file.service')
+const NextCustomerFileReferenceService = require('./next_customer_file_reference.service')
+
 class SendCustomerFileService {
   /**
-   * Placeholder service
+   * Organises the generation and sending of a customer file.
+   *
+   * The service accepts an array of regions. It is expected that when it is called during the "send bill run" process
+   * we will receive an array containing just the region of the bill run, and an array will only be passed to it by the
+   * automated once-a-week "send all customer files" process.
+   *
+   * For each given region it:
+   * - Checks if a file is needed (ie. if there are any customer changes in the db for the given regime and region);
+   * - Calls GenerateCustomerFileService to generate the customer file;
+   * - Calls SendFileToS3Service to send the customer file to the S3 bucket;
+   * - Deletes the customer records for the regime and region from the db;
+   * - Deletes the file if ServerConfig.removeTemporaryFiles is set to `true`.
+   *
+   * @param {module:RegimeModel} regime The regime that the customer file is to be generated for.
+   * @param {array} regions An arry of regions we want to send a customer file for.
+   * @param {@module:Notifier} [notifier] Instance of `Notifier` class. Used to report any errors that occur.
    */
-  static async go () {}
+  static async go (regime, regions, notifier) {
+    let generatedFile
+
+    for (const region of regions) {
+      try {
+        const fileNeeded = await this._checkIfFileNeeded(regime, region)
+        if (!fileNeeded) {
+          // No file is needed for this region so break and move on to the next region
+          break
+        }
+
+        generatedFile = await this._generateAndSend(regime, region)
+
+        await this._clearCustomerTable(regime, region)
+
+        if (this._removeTemporaryFiles()) {
+          await DeleteFileService.go(generatedFile)
+        }
+      } catch (error) {
+        notifier.omfg('Error sending customer file', { generatedFile, error })
+      }
+    }
+  }
+
+  /**
+   * Returns true if there are customer records for this regime and region, or false if there aren't
+   */
+  static async _checkIfFileNeeded (regime, region) {
+    const customers = await CustomerModel.query()
+      .select('id')
+      .where('regimeId', regime.id)
+      .where('region', region)
+
+    return customers.length !== 0
+  }
+
+  /**
+   * Generate and send the customer file. Returns the path and filename of the generated file.
+   */
+  static async _generateAndSend (regime, region) {
+    const filename = await this._filename(regime, region)
+    const generatedFile = await GenerateCustomerFileService.go(regime, region, filename)
+
+    // The key is the remote path and filename in the S3 bucket, eg. 'wrls/customer/nalac50001.dat'
+    const key = path.join(regime.slug, 'customer', filename)
+
+    await SendFileToS3Service.go(generatedFile, key)
+
+    return generatedFile
+  }
+
+  /**
+   * Obtains a file reference for the given regime and region and returns the resulting filename
+   */
+  static async _filename (regime, region) {
+    const fileReference = await NextCustomerFileReferenceService.go(regime, region)
+    return `${fileReference}.dat`
+  }
+
+  /**
+   * Returns the state of the `removeTemporaryFiles` config setting. Kept in a separate method for ease of testing.
+   */
+  static _removeTemporaryFiles () {
+    return removeTemporaryFiles
+  }
+
+  /**
+   * Deletes customer records for the given regime and region
+   */
+  static async _clearCustomerTable (regime, region) {
+    await CustomerModel.query()
+      .where('regimeId', regime.id)
+      .where('region', region)
+      .delete()
+  }
 }
 
 module.exports = SendCustomerFileService
---
+++ test/services/send_customer_file.service.test.js
@@ -0,0 +1,231 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, before, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const {
+  DatabaseHelper,
+  RegimeHelper
+} = require('../support/helpers')
+
+const { CustomerModel } = require('../../app/models')
+
+const { CreateCustomerDetailsService } = require('../../app/services')
+
+// Things we need to stub
+const {
+  DeleteFileService,
+  GenerateCustomerFileService,
+  NextCustomerFileReferenceService,
+  SendFileToS3Service
+} = require('../../app/services')
+
+// Thing under test
+const { SendCustomerFileService } = require('../../app/services')
+
+describe('Send Customer File service', () => {
+  let regime
+  let deleteStub
+  let generateStub
+  let sendStub
+  let notifierFake
+
+  const payload = {
+    customerReference: 'AB12345678',
+    customerName: 'CUSTOMER_NAME',
+    addressLine1: 'ADDRESS_LINE_1',
+    addressLine2: 'ADDRESS_LINE_2',
+    addressLine3: 'ADDRESS_LINE_3',
+    addressLine4: 'ADDRESS_LINE_4',
+    addressLine5: 'ADDRESS_LINE_5',
+    addressLine6: 'ADDRESS_LINE_6',
+    postcode: 'POSTCODE'
+  }
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    regime = await RegimeHelper.addRegime('wrls', 'WRLS')
+
+    await CreateCustomerDetailsService.go({ ...payload, region: 'A' }, regime)
+    await CreateCustomerDetailsService.go({ ...payload, region: 'W' }, regime)
+
+    deleteStub = Sinon.stub(DeleteFileService, 'go').returns(true)
+    generateStub = Sinon.stub(GenerateCustomerFileService, 'go').returns('stubFilename')
+    sendStub = Sinon.stub(SendFileToS3Service, 'go').returns(true)
+    Sinon.stub(NextCustomerFileReferenceService, 'go').callsFake((_, region) => `nal${region.toLowerCase()}c50001`)
+
+    // Create a fake function to stand in place of Notifier.omfg()
+    notifierFake = { omfg: Sinon.fake() }
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('When a single region is specified', () => {
+    describe('and a customer file is required', () => {
+      it('generates a customer file', async () => {
+        await SendCustomerFileService.go(regime, ['A'])
+
+        expect(generateStub.calledOnce).to.be.true()
+        expect(generateStub.getCall(0).args[0]).to.equal(regime)
+        expect(generateStub.getCall(0).args[1]).to.equal('A')
+      })
+
+      it('sends the customer file', async () => {
+        await SendCustomerFileService.go(regime, ['A'])
+
+        expect(sendStub.calledOnce).to.be.true()
+        expect(sendStub.getCall(0).firstArg).to.equal('stubFilename')
+        expect(sendStub.getCall(0).args[1]).to.equal(`${regime.slug}/customer/nalac50001.dat`)
+      })
+
+      it('clears the correct region customers from the customer table', async () => {
+        await SendCustomerFileService.go(regime, ['A'])
+
+        const customersRegionA = await CustomerModel.query()
+          .select('id')
+          .where('region', 'A')
+
+        const customersRegionW = await CustomerModel.query()
+          .select('id')
+          .where('region', 'W')
+
+        expect(customersRegionA).to.be.empty()
+        expect(customersRegionW).to.not.be.empty()
+      })
+
+      describe('and removeTemporary files is set to `true`', () => {
+        before(async () => {
+          Sinon.stub(SendCustomerFileService, '_removeTemporaryFiles').returns(true)
+        })
+
+        it('deletes the file', async () => {
+          await SendCustomerFileService.go(regime, ['A'])
+
+          expect(deleteStub.calledOnce).to.equal(true)
+        })
+      })
+
+      describe('and removeTemporary files is set to `false`', () => {
+        before(async () => {
+          Sinon.stub(SendCustomerFileService, '_removeTemporaryFiles').returns(false)
+        })
+
+        it("doesn't delete the file", async () => {
+          await SendCustomerFileService.go(regime, ['A'])
+
+          expect(deleteStub.called).to.equal(false)
+        })
+      })
+    })
+
+    describe("and a customer file isn't required", () => {
+      it("doesn't try to generate a file", async () => {
+        await SendCustomerFileService.go(regime, ['X', 'Y'])
+
+        expect(generateStub.notCalled).to.be.true()
+      })
+
+      it("doesn't try to send the file", async () => {
+        await SendCustomerFileService.go(regime, ['X', 'Y'])
+
+        expect(sendStub.notCalled).to.be.true()
+      })
+    })
+  })
+
+  describe('When multiple regions are specified', () => {
+    describe('and a customer file is required', () => {
+      it('generates a customer file', async () => {
+        await SendCustomerFileService.go(regime, ['A', 'W'])
+
+        expect(generateStub.calledTwice).to.be.true()
+        expect(generateStub.getCall(0).args[0]).to.equal(regime)
+        expect(generateStub.getCall(0).args[1]).to.equal('A')
+        expect(generateStub.getCall(1).args[0]).to.equal(regime)
+        expect(generateStub.getCall(1).args[1]).to.equal('W')
+      })
+
+      it('sends the customer file', async () => {
+        await SendCustomerFileService.go(regime, ['A', 'W'])
+
+        expect(sendStub.calledTwice).to.be.true()
+        expect(sendStub.getCall(0).args[0]).to.equal('stubFilename')
+        expect(sendStub.getCall(0).args[1]).to.equal(`${regime.slug}/customer/nalac50001.dat`)
+        expect(sendStub.getCall(1).args[0]).to.equal('stubFilename')
+        expect(sendStub.getCall(1).args[1]).to.equal(`${regime.slug}/customer/nalwc50001.dat`)
+      })
+
+      it('clears the correct region customers from the customer table', async () => {
+        await SendCustomerFileService.go(regime, ['A', 'W'])
+
+        const customers = await CustomerModel.query()
+          .select('id')
+
+        expect(customers).to.be.empty()
+      })
+
+      describe('and removeTemporary files is set to `true`', () => {
+        before(async () => {
+          Sinon.stub(SendCustomerFileService, '_removeTemporaryFiles').returns(true)
+        })
+
+        it('deletes the file', async () => {
+          await SendCustomerFileService.go(regime, ['A', 'W'])
+
+          expect(deleteStub.calledTwice).to.equal(true)
+        })
+      })
+
+      describe('and removeTemporary files is set to `false`', () => {
+        before(async () => {
+          Sinon.stub(SendCustomerFileService, '_removeTemporaryFiles').returns(false)
+        })
+
+        it("doesn't delete the file", async () => {
+          await SendCustomerFileService.go(regime, ['A', 'W'])
+
+          expect(deleteStub.called).to.equal(false)
+        })
+      })
+    })
+
+    describe("and a customer file isn't required", () => {
+      it("doesn't try to generate a file", async () => {
+        await SendCustomerFileService.go(regime, ['X', 'Y'])
+
+        expect(generateStub.notCalled).to.be.true()
+      })
+
+      it("doesn't try to send a file", async () => {
+        await SendCustomerFileService.go(regime, ['X', 'Y'])
+
+        expect(sendStub.notCalled).to.be.true()
+      })
+    })
+  })
+
+  describe('When an error occurs', () => {
+    it('throws an error', async () => {
+      // We stub within the test and not in a before() to ensure it happens after the beforeEach() that first defines
+      // the stub
+      Sinon.restore()
+      Sinon
+        .stub(GenerateCustomerFileService, 'go')
+        .throws()
+
+      await SendCustomerFileService.go(regime, ['A'], notifierFake)
+
+      expect(notifierFake.omfg.callCount).to.equal(1)
+      expect(notifierFake.omfg.firstArg).to.equal('Error sending customer file')
+    })
+  })
+})
