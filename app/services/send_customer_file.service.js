1e260338ac863e282f6caeaf2ca5b1083b59cab9
Stuart Adair <stu@silverka.co.uk>
2021-04-09 16:34:48 +0100

    Add controller and route

---
+++ app/services/send_customer_file.service.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * @module SendCustomerFileService
+ */
+
+class SendCustomerFileService {
+  /**
+   * Placeholder service
+   */
+  static async go (regime, regions, notifier) {
+    return true
+  }
+}
+
+module.exports = SendCustomerFileService
