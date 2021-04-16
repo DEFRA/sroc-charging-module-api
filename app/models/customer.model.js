486293d4e1edc9632228b19e13db923a08f33c69
Stuart Adair <stu@silverka.co.uk>
2021-04-16 16:34:01 +0100

    Update customer model

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -4,11 +4,25 @@
  * @module CustomerModel
  */
 
+const { Model } = require('objection')
 const BaseModel = require('./base.model')
 
 class CustomerModel extends BaseModel {
   static get tableName () {
     return 'customers'
+  }
+
+  static get relationMappings () {
+    return {
+      customerFile: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'customer_file.model',
+        join: {
+          from: 'customers.customerFileId',
+          to: 'customerFiles.id'
+        }
+      }
+    }
   }
 }
 
