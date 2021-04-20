3f876cc33cb9fce926f234446f7fb1b7a3e8f4ce
Stuart Adair <stu@silverka.co.uk>
2021-04-20 16:08:43 +0100

    Create exported customers table

---
+++ db/migrations/20210420141638_create_exported_customers.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const tableName = 'exported_customers'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .createTable(tableName, table => {
+      // Primary Key
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+
+      // Data
+      table.string('customer_reference').notNullable()
+      table.uuid('customer_file_id').references('customer_files.id').notNullable()
+
+      // Automatic timestamps
+      table.timestamps(false, true)
+    })
+
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON ${tableName}
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `)
+}
+
+exports.down = async function (knex) {
+  return knex
+    .schema
+    .dropTableIfExists(tableName)
+}
