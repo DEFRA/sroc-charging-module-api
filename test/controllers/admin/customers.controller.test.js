6ced16aae349e2ff9f177c47a748b9307b8f866f
Stuart Adair <stu@silverka.co.uk>
2021-04-22 17:41:28 +0100

    Restore service auth stub separately from service stub

--- test/controllers/admin/customers.controller.test.js
+++ test/controllers/admin/customers.controller.test.js
@@ -5,7 +5,7 @@ const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const Sinon = require('sinon')
 
-const { describe, it, before, beforeEach, afterEach } = exports.lab = Lab.script()
+const { describe, it, before, beforeEach, after, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // For running our service
@@ -45,6 +45,10 @@ describe('Customers controller', () => {
   })
 
   afterEach(async () => {
+    serviceStub.restore()
+  })
+
+  after(async () => {
     Sinon.restore()
   })
 
