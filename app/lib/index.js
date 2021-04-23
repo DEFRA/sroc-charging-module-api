3a3adcf6710fbd1f06330ff823636cb15835df50
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-04-23 10:59:49 +0100

    Rename existing Notifier class to RequestNotifier
    This gives it a more expressive name and makes it more distinct from TaskNotifier.

--- app/lib/index.js
+++ app/lib/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const { BaseNotifier } = require('./base_notifier')
-const Notifier = require('./notifier')
+const RequestNotifier = require('./request_notifier')
 const StaticLookup = require('./static_lookup')
 const TaskNotifier = require('./task_notifier')
 
 module.exports = {
   BaseNotifier,
-  Notifier,
+  RequestNotifier,
   StaticLookup,
   TaskNotifier
 }
