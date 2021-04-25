e279b9e53decabb4be16b14833b402df0fabfa0e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-04-25 23:29:46 +0100

    Add code documentation

--- app/tasks/customer_files.task.js
+++ app/tasks/customer_files.task.js
@@ -9,6 +9,15 @@ const { RegimeModel } = require('../models')
 const { SendCustomerFileService } = require('../services')
 
 class CustomerFilesTask {
+  /**
+   * Check and if required generate a 'customer file' for each regime in the system
+   *
+   * Intended to be called as a scheduled task from Jenkins using a package.json script and via `TaskRunner`. It gets
+   * all regimes in the service and then calls `SendCustomerFileService` for each one.
+   *
+   * @param {@module:BaseNotifier} notifier Instance of a `Notifier` class. Expected to be an instance of `TaskNotifier`
+   * but anything that implements `BaseNotifier` is supported.
+   */
   static async go (notifier) {
     notifier.omg('Starting sending customer files task')
 
--- app/tasks/task_runner.js
+++ app/tasks/task_runner.js
@@ -6,7 +6,33 @@
 
 const CustomerFilesTask = require('./customer_files.task')
 
+/**
+ * Use to run any one of our 'tasks'
+ *
+ * It manages instantiating the notifier they'll use for logging progress and sending errors to Errbit then exiting
+ * once the work is complete.
+ *
+ * ## Usage
+ *
+ * As new tasks are added there is a process that must be followed to implement them properly. The first 2 steps are no
+ * different to what we do when adding services, translators etc.
+ *
+ * - add your new task to `app/tasks/` as a standard JS class using the 'service' pattern we have adopted
+ * - add a reference to it in `app/tasks/index.js`
+ *
+ * The next steps are specific to tasks
+ *
+ * - add a reference to it in this file
+ * - update the switch statement in `_determineTaskClass()` to handle your new task class
+ * - add an entry to package.json, for example, `"my-new-task": "node app/tasks/index.js MyNewTask"` ensuring you
+ * include the arg which is your tasks's class name
+ */
 class TaskRunner {
+  /**
+   * Execute the task runner for the task identified by the `className`
+   *
+   * @param {String} className Name of the task (class name) to be run
+   */
   static async go (className) {
     const notifier = this._notifier()
 
