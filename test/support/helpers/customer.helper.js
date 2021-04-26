5fbe96b91efaacfa2248ce86a6e1f854f100a920
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-04-26 12:07:43 +0100

    Add CustomersHelper
    We have other customer related records which is why we are giving this a generic name of `CustomerHelper`. But to begin with it helps us create `customer_file` records we can then use in our tests.

---
+++ test/support/helpers/customer.helper.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const { CustomerFileModel } = require('../../../app/models')
+const GeneralHelper = require('./general.helper')
+
+/**
+ * Use to help with creating customer records, for example, 'CustomerFiles'
+ */
+class CustomerFileHelper {
+  /**
+   * Create a `customer_file` record
+   *
+   * @param {module:RegimeModel} [regime] Instance of `RegimeModel` to assign  the customer file to. If nulll it will
+   * generate a random UUID and use that instead
+   * @param {string} [region] Region to use. Defaults to 'A'
+   * @param {string} [fileReference] File reference to use. Defaults to 'nalac50001'
+   * @param {string} [status] Status to use. Defaults to 'exported'. If set to `exported` it will also default
+   * `exported_at` to `Date.now()`
+   *
+   * @returns {module:CustomerFileModel} The newly created instance of `CustomerFileModel`
+   */
+  static addCustomerFile (regime = null, region = 'A', fileReference = 'nalac50001', status = 'exported') {
+    return CustomerFileModel.query()
+      .insert({
+        regimeId: this._regimeId(regime),
+        region,
+        fileReference,
+        exportedAt: this._exportedAt(status)
+      })
+      .returning('*')
+  }
+
+  static _regimeId (regime) {
+    if (!regime) {
+      return GeneralHelper.uuid4()
+    }
+
+    return regime.id
+  }
+
+  static _exportedAt (status) {
+    if (status === 'exported') {
+      return new Date().toISOString()
+    }
+
+    return null
+  }
+}
+
+module.exports = CustomerFileHelper
