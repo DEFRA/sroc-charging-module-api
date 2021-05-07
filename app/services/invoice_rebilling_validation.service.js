db3de5521dc3b3e343ab039532c44766fbfead58
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-05-07 16:08:14 +0100

    Add rebilling validation for cancel invoices (#407)
    When an invoice is rebilled, two rebill invoices are created: the cancel invoice, which cancels out the original invoice, and the rebill invoice, which bills the customer. We have had confirmation that rebill invoices can be rebilled but cancel invoices cannot; this change adds additional validation for this.

--- app/services/invoice_rebilling_validation.service.js
+++ app/services/invoice_rebilling_validation.service.js
@@ -13,7 +13,8 @@ class InvoiceRebillingValidationService {
   * Validates that an invoice exists and that it is suitable for rebilling:
   * - The invoice does not already belong to the bill run it is being rebilled to;
   * - The status of its current bill run is 'billed';
-  * - The region of the bill run it is being rebilled to matches the region of its current bill run.
+  * - The region of the bill run it is being rebilled to matches the region of its current bill run;
+  * - It is not a rebill cancel invoice.
   *
   * Note that we do not validate the status of the bill run we will rebill to as this will have already been handled by
   * our bill run plugin (since we pass the new bill run in the url).
@@ -29,6 +30,7 @@ class InvoiceRebillingValidationService {
     this._validateNotOnNewBillRun(currentBillRun, newBillRun, invoice.id)
     this._validateCurrentBillRunStatus(currentBillRun)
     this._validateRegion(currentBillRun, newBillRun, invoice.id)
+    this._validateNotCancelInvoice(invoice)
 
     return true
   }
@@ -53,6 +55,14 @@ class InvoiceRebillingValidationService {
     if (currentBillRun.region !== newBillRun.region) {
       throw Boom.conflict(
           `Invoice ${invoiceId} is for region ${currentBillRun.region} but bill run ${newBillRun.id} is for region ${newBillRun.region}.`
+      )
+    }
+  }
+
+  static _validateNotCancelInvoice (invoice) {
+    if (invoice.rebilledType === 'C') {
+      throw Boom.conflict(
+          `Invoice ${invoice.id} is a rebill cancel invoice and cannot be rebilled.`
       )
     }
   }
