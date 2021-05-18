18ccc1aef56ffadf4a7a4125e5fde46582d0c232
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-05-18 12:25:11 +0100

    Squash bill runs

--- db/migrations/20201127163958_create_bill_runs.js
+++ db/migrations/20201127163958_create_bill_runs.js
@@ -12,7 +12,21 @@ exports.up = async function (knex) {
       // Data
       table.uuid('regime_id').notNullable()
       table.string('region').notNullable()
-      table.string('status').notNullable()
+      table.string('status').notNullable().defaultTo('initialised')
+      table.integer('bill_run_number')
+      table.string('file_reference')
+      table.integer('credit_note_count').notNullable().defaultTo(0)
+      table.bigInteger('credit_note_value').notNullable().defaultTo(0)
+      table.integer('invoice_count').notNullable().defaultTo(0)
+      table.bigInteger('invoice_value').notNullable().defaultTo(0)
+      table.integer('credit_line_count').notNullable().defaultTo(0)
+      table.bigInteger('credit_line_value').notNullable().defaultTo(0)
+      table.integer('debit_line_count').notNullable().defaultTo(0)
+      table.bigInteger('debit_line_value').notNullable().defaultTo(0)
+      table.integer('zero_line_count').notNullable().defaultTo(0)
+      table.integer('subject_to_minimum_charge_count').notNullable().defaultTo(0)
+      table.bigInteger('subject_to_minimum_charge_credit_value').notNullable().defaultTo(0)
+      table.bigInteger('subject_to_minimum_charge_debit_value').notNullable().defaultTo(0)
       table.uuid('created_by').notNullable()
 
       // Automatic timestamps
