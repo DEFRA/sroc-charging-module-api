fa5c354b1a5421c889309af9cb18409bad35c355
Stuart Adair <stu@silverka.co.uk>
2021-05-19 11:19:57 +0100

    Create BoomNotifier

---
+++ app/lib/boom_notifier.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * @module BoomNotifier
+ */
+
+const RequestNotifier = require('./request_notifier')
+
+const Boom = require('@hapi/boom')
+
+/**
+ * A combined logging and Airbrake (Errbit) notification manager which extends RequestNotifier to also throw a Boom
+ * `400` error if the .omfg method is called.
+ *
+ * The use case for RequestNotifier is to log errors which occur in syncronous "background" tasks -- ie. where a
+ * controller calls a service then immediately returns a response while the service continues to run. Any errors thrown
+ * within the background task are caught and logged using the notifier.
+ *
+ * The use case for BoomNotifier is for when we wish to run one of these background tasks asyncronously -- for example,
+ * sending a bill run would normally be done in the background but we want the 'send bill run' admin endpoint to run it
+ * in the foreground so any errors which occur are returned straight away so we don't need to dig through the logs.
+ *
+ * We therefore create a notifier which inherits everything from RequestNotifier and uses super to extend the omfg
+ * method so it throws an error after logging.
+ */
+class BoomNotifier extends RequestNotifier {
+  omfg (message, data = {}) {
+    super.omfg(message, data)
+    throw Boom.badData(message, data)
+  }
+}
+
+module.exports = BoomNotifier
---
+++ test/lib/boom_notifier.test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Thing under test
+const { BoomNotifier } = require('../../app/lib')
+
+describe('BoomNotifier class', () => {
+  const id = '1234567890'
+  let airbrakeFake
+  let pinoFake
+
+  beforeEach(async () => {
+    airbrakeFake = { notify: Sinon.fake.resolves({ id: 1 }), flush: Sinon.fake() }
+    pinoFake = { info: Sinon.fake(), error: Sinon.fake() }
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when an airbrake notification is sent', () => {
+    const message = 'hell no test'
+    const data = { offTheChart: true }
+
+    it('formats it as expected', () => {
+      const expectedArgs = {
+        message,
+        session: {
+          ...data,
+          req: {
+            id
+          }
+        }
+      }
+      const testNotifier = new BoomNotifier(id, pinoFake, airbrakeFake)
+
+      // We wrap the call in this assertion so the thrown error doesn't cause the test to fail
+      expect(() => testNotifier.omfg(message, data)).to.throw()
+      expect(airbrakeFake.notify.calledOnceWith(expectedArgs)).to.be.true()
+    })
+
+    it('throws a Boom error with the correct message and data', async () => {
+      const testNotifier = new BoomNotifier(id, pinoFake, airbrakeFake)
+
+      expect(() => testNotifier.omfg(message, data)).to.throw(Error, { message, data })
+    })
+  })
+})
