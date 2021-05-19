2ff2a6a214c4a2445760fefba7bab4325b0e1597
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-05-19 17:01:16 +0100

    Create AdminSendTransactionFileService (#434)
    https://trello.com/c/ntPvucIj/1974-enable-admin-endpoint-to-manually-trigger-a-transaction-file-v2
    
    We continue with our work to have the admin endpoint run file generation syncronously by creating a new AdminSendTransactionFileService which performs initial status validation, throws an error if it fails, then calls the existing SendTransactionFileService, passing in our new BoomNotifier so that any errors which occurred will be thrown as well as logged (and therefore will result in a response being passed back to the admin user).

---
+++ test/services/admin_send_transaction_file.service.test.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const {
+  BillRunHelper,
+  DatabaseHelper,
+  GeneralHelper,
+  RegimeHelper
+} = require('../support/helpers')
+
+// Things to stub
+const { SendTransactionFileService } = require('../../app/services')
+
+// Thing under test
+const { AdminSendTransactionFileService } = require('../../app/services')
+
+describe('Admin Send Transaction File service', () => {
+  let regime
+  let billRun
+  let sendTransactionFileStub
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    regime = await RegimeHelper.addRegime('wrls', 'WRLS')
+    billRun = await BillRunHelper.addBillRun(GeneralHelper.uuid4(), regime.id)
+
+    sendTransactionFileStub = Sinon.stub(SendTransactionFileService, 'go')
+  })
+
+  afterEach(async () => {
+    Sinon.restore()
+  })
+
+  describe('When the transaction file can be sent', () => {
+    describe('because the bill run status is `pending`', () => {
+      it('calls SendTransactionFileService with the correct params', async () => {
+        billRun.status = 'pending'
+
+        await AdminSendTransactionFileService.go(regime, billRun)
+
+        expect(sendTransactionFileStub.calledOnceWith(regime, billRun)).to.be.true()
+      })
+    })
+  })
+
+  describe('When the transaction file cannot be sent', () => {
+    describe('because the bill run status is not `pending`', () => {
+      it('throws an error', async () => {
+        const err = await expect(AdminSendTransactionFileService.go(regime, billRun)).to.reject()
+
+        expect(err).to.be.an.error()
+        expect(err.output.payload.message).to.equal(`Bill run ${billRun.id} does not have a status of 'pending'.`)
+      })
+    })
+  })
+})
