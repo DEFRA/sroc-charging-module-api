8a6cf2ffe831462b067efe6a2692b8168a6d1887
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-06-21 10:30:05 +0100

    Update bill run on delete licence

--- app/services/delete_licence.service.js
+++ app/services/delete_licence.service.js
@@ -6,6 +6,8 @@
 
 const { LicenceModel } = require('../models')
 
+const DeleteInvoiceService = require('./delete_invoice.service')
+
 class DeleteLicenceService {
   /**
    * Deletes a licence along with its transactions and updates the invoice accordingly. Intended to be run as a
@@ -31,14 +33,13 @@ class DeleteLicenceService {
 
         const invoice = await licence.$relatedQuery('invoice', trx)
         const licences = await invoice.$relatedQuery('licences', trx)
+        const billRun = await licence.$relatedQuery('billRun', trx)
 
         if (licences.length) {
           await this._handleInvoice(invoice, licence, trx)
-          // const billRun = await licence.$relatedQuery('billRun', trx)
-          // await this._handleBillRun(billRun, licence, trx)
+          await this._handleBillRun(billRun, licence, trx)
         } else {
-          // TODO: Replace this with DeleteInvoiceService to ensure bill run level stats are updated
-          await invoice.$query(trx).delete()
+          await DeleteInvoiceService.go(invoice, billRun.id, notifier, trx)
         }
       })
     } catch (error) {
@@ -47,7 +48,7 @@ class DeleteLicenceService {
   }
 
   /**
-   * Patches the specified invoice if there are licences remaining on it, otherwise it deletes the invoice
+   * Patches the invoice that the specified licence belongs to
    */
   static async _handleInvoice (invoice, licence, trx) {
     this._updateInstance(invoice, licence)
@@ -55,17 +56,13 @@ class DeleteLicenceService {
     await invoice.$query(trx).patch(invoicePatch)
   }
 
-  static _entityPatch (entity) {
-    return {
-      creditLineCount: entity.creditLineCount,
-      creditLineValue: entity.creditLineValue,
-      debitLineCount: entity.debitLineCount,
-      debitLineValue: entity.debitLineValue,
-      zeroLineCount: entity.zeroLineCount,
-      subjectToMinimumChargeCount: entity.subjectToMinimumChargeCount,
-      subjectToMinimumChargeCreditValue: entity.subjectToMinimumChargeCreditValue,
-      subjectToMinimumChargeDebitValue: entity.subjectToMinimumChargeDebitValue
-    }
+  /**
+   * Patches the bill run that the specified licence belongs to
+   */
+  static async _handleBillRun (billRun, licence, trx) {
+    this._updateBillRunInstance(billRun, licence)
+    const billRunPatch = this._billRunPatch(billRun)
+    await billRun.$query(trx).patch(billRunPatch)
   }
 
   static _invoicePatch (invoice) {
@@ -87,30 +84,25 @@ class DeleteLicenceService {
     }
   }
 
-  static _additionalBillRunFields () {
-    return [
-      'creditNoteValue',
-      'invoiceValue'
-    ]
-  }
-
-  /**
-   * Patches the bill run that the specified licence belongs to
-   */
-  static async _handleBillRun (billRun, licence, trx) {
-    this._updateInstance(billRun, licence, this._additionalBillRunFields())
-    const billRunPatch = this._billRunPatch(billRun)
-    await billRun.$query(trx).patch(billRunPatch)
+  static _entityPatch (entity) {
+    return {
+      creditLineCount: entity.creditLineCount,
+      creditLineValue: entity.creditLineValue,
+      debitLineCount: entity.debitLineCount,
+      debitLineValue: entity.debitLineValue,
+      zeroLineCount: entity.zeroLineCount,
+      subjectToMinimumChargeCount: entity.subjectToMinimumChargeCount,
+      subjectToMinimumChargeCreditValue: entity.subjectToMinimumChargeCreditValue,
+      subjectToMinimumChargeDebitValue: entity.subjectToMinimumChargeDebitValue
+    }
   }
 
   /**
    * Receives an entity (ie. an invoice or a bill run) and subtracts the licence's stats from the entity's stats. We
    * update the figures on the instance in this way so we can then use the entity's instance methods to determine
-   * whether deminimis etc. applies and then persist the values and flags in one go. We optionally accept an array of
-   * additional fields to be updated, which allows us to handle fields common across each entity plus fields specific
-   * to either one.
+   * whether deminimis etc. applies and then persist the values and flags in one go.
    */
-  static _updateInstance (entity, licence, additionalFields = []) {
+  static _updateInstance (entity, licence) {
     // Define the fields to be updated and for each one, subtract the licence value from the entity value
     const fieldsToUpdate = [
       'creditLineCount',
@@ -121,11 +113,21 @@ class DeleteLicenceService {
       'subjectToMinimumChargeCount',
       'subjectToMinimumChargeCreditValue',
       'subjectToMinimumChargeDebitValue'
-    ].concat(additionalFields)
+    ]
 
     fieldsToUpdate.forEach(field => {
       entity[field] -= licence[field]
     })
+  }
+
+  /**
+   * Updates the bill run instance by first calling _updateInstance to update the standard fields, then updates two
+   * additional fields unique to the bill run.
+   */
+  static _updateBillRunInstance (billRun, licence) {
+    this._updateInstance(billRun, licence)
+    billRun.creditNoteValue -= licence.creditLineValue
+    billRun.invoiceValue -= licence.debitLineValue
   }
 }
 
--- test/services/delete_licence.service.test.js
+++ test/services/delete_licence.service.test.js
@@ -79,7 +79,7 @@ describe('Delete Licence service', () => {
     Sinon.restore()
   })
 
-  describe.only('When a valid licence is supplied', () => {
+  describe('When a valid licence is supplied', () => {
     it('deletes the licence', async () => {
       await DeleteLicenceService.go(licence, notifierFake)
 
@@ -96,130 +96,130 @@ describe('Delete Licence service', () => {
       expect(transactions).to.be.empty()
     })
 
-    it('updates the invoice level figures', async () => {
-      // Create a second licence on the invoice to ensure the invoice isn't deleted due to it being empty
-      await LicenceHelper.addLicence(billRun.id, 'SECOND_LICENCE', transaction.invoiceId, 'TH230000222', 2019)
-      // Generate the bill run to ensure its values are updated before we delete the licence
-      await GenerateBillRunService.go(billRun)
-
-      await DeleteLicenceService.go(licence, notifierFake)
-
-      const result = await InvoiceModel.query().findById(transaction.invoiceId)
-      expect(result.debitLineCount).to.equal(0)
-      expect(result.debitLineValue).to.equal(0)
-    })
-
-    describe('sets zeroValueInvoice', () => {
-      let secondTransaction
-      let secondLicence
-
-      beforeEach(async () => {
-        // Add a transaction to a second invoice
-        rulesServiceStub.restore()
-        RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
-        const { transaction: secondTransactionResult } = await CreateTransactionService.go(
-          { ...payload, customerReference: 'CUST2' }, billRun, authorisedSystem, regime
-        )
-        secondTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
-        secondLicence = await LicenceModel.query().findById(secondTransaction.licenceId)
-
-        // Add a credit to the original invoice to cancel out its original debit
-        rulesServiceStub.restore()
-        RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
-        await CreateTransactionService.go({ ...payload, credit: true }, billRun, authorisedSystem, regime)
-
-        // Generate the bill run to ensure its values are updated before we delete the licence
-        await GenerateBillRunService.go(billRun)
-      })
-
-      it('to `true` when still applicable', async () => {
-        await DeleteLicenceService.go(secondLicence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(transaction.invoiceId)
-        expect(result.zeroValueInvoice).to.be.true()
-      })
-
-      it('to `false` when not applicable', async () => {
-        await DeleteLicenceService.go(licence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(secondTransaction.invoiceId)
-        expect(result.zeroValueInvoice).to.be.false()
-      })
-    })
-
-    describe('sets deminimisInvoice', () => {
-      let secondTransaction
-      let secondLicence
-
-      beforeEach(async () => {
-        // Add a transaction to a second invoice
-        rulesServiceStub.restore()
-        RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
-        const { transaction: secondTransactionResult } = await CreateTransactionService.go(
-          { ...payload, customerReference: 'CUST2' }, billRun, authorisedSystem, regime
-        )
-        secondTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
-        secondLicence = await LicenceModel.query().findById(secondTransaction.licenceId)
-
-        // Add a credit to the original invoice to take its value below the deminimis limit
-        rulesServiceStub.restore()
-        RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 4750)
-        await CreateTransactionService.go({ ...payload, credit: true }, billRun, authorisedSystem, regime)
-
-        // Generate the bill run to ensure its values are updated before we delete the licence
-        await GenerateBillRunService.go(billRun)
-      })
-
-      it('to `true` when still applicable', async () => {
-        await DeleteLicenceService.go(secondLicence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(transaction.invoiceId)
-        expect(result.deminimisInvoice).to.be.true()
-      })
-
-      it('to `false` when not applicable', async () => {
-        await DeleteLicenceService.go(licence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(secondTransaction.invoiceId)
-        expect(result.deminimisInvoice).to.be.false()
-      })
-    })
-
-    describe('sets minimumChargeInvoice', () => {
-      let minimumChargeLicence
-
-      beforeEach(async () => {
-        // Add a minimum charge transaction to a second invoice
-        rulesServiceStub.restore()
-        RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5)
-        const { transaction: secondTransactionResult } = await CreateTransactionService.go(
-          { ...payload, customerReference: 'CUST2', subjectToMinimumCharge: true }, billRun, authorisedSystem, regime
-        )
-        const minimumChargeTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
-        minimumChargeLicence = await LicenceModel.query().findById(minimumChargeTransaction.licenceId)
-
-        // Generate the bill run to ensure its values are updated before we delete the licence
-        await GenerateBillRunService.go(billRun)
-      })
-
-      it('to `true` when still applicable', async () => {
-        await DeleteLicenceService.go(licence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(minimumChargeLicence.invoiceId)
-        expect(result.minimumChargeInvoice).to.be.true()
-      })
-
-      it('to `false` when not applicable', async () => {
-        await DeleteLicenceService.go(minimumChargeLicence, notifierFake)
-
-        const result = await InvoiceModel.query().findById(licence.invoiceId)
-        expect(result.minimumChargeInvoice).to.be.false()
-      })
-    })
-
-    describe('at the bill run level', () => {
-      it('updates the figures', async () => {
+    describe('when there are licences left', () => {
+      it('updates the invoice level figures', async () => {
         // Create a second licence on the invoice to ensure the invoice isn't deleted due to it being empty
+        await LicenceHelper.addLicence(billRun.id, 'SECOND_LICENCE', transaction.invoiceId, 'TH230000222', 2019)
+        // Generate the bill run to ensure its values are updated before we delete the licence
+        await GenerateBillRunService.go(billRun)
+
+        await DeleteLicenceService.go(licence, notifierFake)
+
+        const result = await InvoiceModel.query().findById(transaction.invoiceId)
+        expect(result.debitLineCount).to.equal(0)
+        expect(result.debitLineValue).to.equal(0)
+      })
+
+      describe('sets zeroValueInvoice', () => {
+        let secondTransaction
+        let secondLicence
+
+        beforeEach(async () => {
+        // Add a transaction to a second invoice
+          rulesServiceStub.restore()
+          RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
+          const { transaction: secondTransactionResult } = await CreateTransactionService.go(
+            { ...payload, customerReference: 'CUST2' }, billRun, authorisedSystem, regime
+          )
+          secondTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
+          secondLicence = await LicenceModel.query().findById(secondTransaction.licenceId)
+
+          // Add a credit to the original invoice to cancel out its original debit
+          rulesServiceStub.restore()
+          RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
+          await CreateTransactionService.go({ ...payload, credit: true }, billRun, authorisedSystem, regime)
+
+          // Generate the bill run to ensure its values are updated before we delete the licence
+          await GenerateBillRunService.go(billRun)
+        })
+
+        it('to `true` when still applicable', async () => {
+          await DeleteLicenceService.go(secondLicence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(transaction.invoiceId)
+          expect(result.zeroValueInvoice).to.be.true()
+        })
+
+        it('to `false` when not applicable', async () => {
+          await DeleteLicenceService.go(licence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(secondTransaction.invoiceId)
+          expect(result.zeroValueInvoice).to.be.false()
+        })
+      })
+
+      describe('sets deminimisInvoice', () => {
+        let secondTransaction
+        let secondLicence
+
+        beforeEach(async () => {
+        // Add a transaction to a second invoice
+          rulesServiceStub.restore()
+          RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5000)
+          const { transaction: secondTransactionResult } = await CreateTransactionService.go(
+            { ...payload, customerReference: 'CUST2' }, billRun, authorisedSystem, regime
+          )
+          secondTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
+          secondLicence = await LicenceModel.query().findById(secondTransaction.licenceId)
+
+          // Add a credit to the original invoice to take its value below the deminimis limit
+          rulesServiceStub.restore()
+          RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 4750)
+          await CreateTransactionService.go({ ...payload, credit: true }, billRun, authorisedSystem, regime)
+
+          // Generate the bill run to ensure its values are updated before we delete the licence
+          await GenerateBillRunService.go(billRun)
+        })
+
+        it('to `true` when still applicable', async () => {
+          await DeleteLicenceService.go(secondLicence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(transaction.invoiceId)
+          expect(result.deminimisInvoice).to.be.true()
+        })
+
+        it('to `false` when not applicable', async () => {
+          await DeleteLicenceService.go(licence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(secondTransaction.invoiceId)
+          expect(result.deminimisInvoice).to.be.false()
+        })
+      })
+
+      describe('sets minimumChargeInvoice', () => {
+        let minimumChargeLicence
+
+        beforeEach(async () => {
+        // Add a minimum charge transaction to a second invoice
+          rulesServiceStub.restore()
+          RulesServiceHelper.mockValue(Sinon, RulesService, rulesServiceResponse, 5)
+          const { transaction: secondTransactionResult } = await CreateTransactionService.go(
+            { ...payload, customerReference: 'CUST2', subjectToMinimumCharge: true }, billRun, authorisedSystem, regime
+          )
+          const minimumChargeTransaction = await TransactionModel.query().findById(secondTransactionResult.id)
+          minimumChargeLicence = await LicenceModel.query().findById(minimumChargeTransaction.licenceId)
+
+          // Generate the bill run to ensure its values are updated before we delete the licence
+          await GenerateBillRunService.go(billRun)
+        })
+
+        it('to `true` when still applicable', async () => {
+          await DeleteLicenceService.go(licence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(minimumChargeLicence.invoiceId)
+          expect(result.minimumChargeInvoice).to.be.true()
+        })
+
+        it('to `false` when not applicable', async () => {
+          await DeleteLicenceService.go(minimumChargeLicence, notifierFake)
+
+          const result = await InvoiceModel.query().findById(licence.invoiceId)
+          expect(result.minimumChargeInvoice).to.be.false()
+        })
+      })
+
+      it('updates the bill run level figures', async () => {
+      // Create a second licence on the invoice to ensure the invoice isn't deleted due to it being empty
         await LicenceHelper.addLicence(billRun.id, 'SECOND_LICENCE', transaction.invoiceId, 'TH230000222', 2019)
 
         await GenerateBillRunService.go(billRun)
@@ -233,11 +233,21 @@ describe('Delete Licence service', () => {
       })
     })
 
-    it('deletes the invoice if there are no licences left', async () => {
-      await DeleteLicenceService.go(licence, notifierFake)
+    describe('when there are no licences left', () => {
+      it('deletes the invoice', async () => {
+        await DeleteLicenceService.go(licence, notifierFake)
 
-      const result = await InvoiceModel.query().findById(transaction.invoiceId)
-      expect(result).to.be.undefined()
+        const result = await InvoiceModel.query().findById(transaction.invoiceId)
+        expect(result).to.be.undefined()
+      })
+
+      it('updates the bill run level figures', async () => {
+        await DeleteLicenceService.go(licence, notifierFake)
+
+        const result = await BillRunModel.query().findById(transaction.billRunId)
+        expect(result.debitLineCount).to.equal(0)
+        expect(result.debitLineValue).to.equal(0)
+      })
     })
   })
 
