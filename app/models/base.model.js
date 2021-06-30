7e86a8e1663f6fe7ef777ece67bf97ab43c895ad
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-07-01 00:17:13 +0100

    Add excuse for having an empty class!

--- app/models/base.model.js
+++ app/models/base.model.js
@@ -11,5 +11,16 @@ import { db } from '../../db/index.js'
 // it. https://vincit.github.io/objection.js/api/model/static-methods.html#static-knex
 Model.knex(db)
 
+/**
+ * Base class for all our {@link https://vincit.github.io/objection.js/|Objection} based models
+ *
+ * > Most of the time you want the same configuration for all models and a good pattern is to create a `BaseModel` class
+ * > and inherit all your models from that. You can then add all shared configuration to `BaseModel`.
+ * > {@link https://vincit.github.io/objection.js/api/model/#models}
+ *
+ * Having the `BaseModel` class gives us a place to import both Objection and our `db` config in one place and make the
+ * one time call to `Model.knex()` even if the `BaseModel` itself does not currently do anything. Having all our models
+ * extend from this base class does mean should the need arise we are all set up to implement that shared config.
+ */
 export default class BaseModel extends Model {
 }
