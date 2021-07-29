6977476a39e935de05766624f85aab795ab2b2e5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-07-29 13:17:14 +0100

    Add test to check for expected behaviour
    We've not added tests for _every_ invalid character before because we are confident the logic is the same for each. But as we are making a change adding this test just to prove it's covering the new invalid characters.

--- test/features/invalid_characters.test.js
+++ test/features/invalid_characters.test.js
@@ -44,17 +44,34 @@ describe('Reject requests with invalid characters', () => {
   })
 
   describe('When a payload does include an invalid character', () => {
-    it('rejects the request with the appropriate error message', async () => {
-      const requestPayload = {
-        reference: 'BESESAME001',
-        customerName: 'Bert & Ernie Ltd?'
-      }
+    describe('for example a question mark', () => {
+      it('rejects the request with the appropriate error message', async () => {
+        const requestPayload = {
+          reference: 'BESESAME001',
+          customerName: 'Bert & Ernie Ltd?'
+        }
 
-      const response = await server.inject(options(requestPayload))
-      const responsePayload = JSON.parse(response.payload)
+        const response = await server.inject(options(requestPayload))
+        const responsePayload = JSON.parse(response.payload)
 
-      expect(response.statusCode).to.equal(422)
-      expect(responsePayload.message).to.equal('We cannot accept any request that contains the following characters: ? £ ≤ ≥')
+        expect(response.statusCode).to.equal(422)
+        expect(responsePayload.message).to.equal('We cannot accept any request that contains the following characters: ? £ ≤ ≥')
+      })
+    })
+
+    describe('for example a double (inverted) quotation mark', () => {
+      it('rejects the request with the appropriate error message', async () => {
+        const requestPayload = {
+          reference: 'BESESAME001',
+          customerName: 'Bert & “Ernie” Ltd'
+        }
+
+        const response = await server.inject(options(requestPayload))
+        const responsePayload = JSON.parse(response.payload)
+
+        expect(response.statusCode).to.equal(422)
+        expect(responsePayload.message).to.equal('We cannot accept any request that contains the following characters: ? £ ≤ ≥')
+      })
     })
   })
 })
