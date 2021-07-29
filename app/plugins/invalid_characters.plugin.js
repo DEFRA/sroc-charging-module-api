e55e7c18ab343bd0c86b65b760d8abf2443507d2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-07-29 22:20:29 +0100

    Further update to comments and error message
    It was further highlighted that we were also missing the em dash from our error messaging. So, this time we took a pause and ensured not only the error message but the code comments accurately reflected what we were checking for.
    
    It also adds links to the specific characters in question so there can be no doubt what we are checking for.

--- app/plugins/invalid_characters.plugin.js
+++ app/plugins/invalid_characters.plugin.js
@@ -3,9 +3,18 @@
 const Boom = require('@hapi/boom')
 
 /**
- * Converts an object to a JSON string and then tests if it contains any of the following characters: ? £ ≤ ≥
+ * Converts an object to a JSON string and then tests if it contains any of the following characters:
+ *
+ * - {@link https://unicode-table.com/en/00A3| £ Pound Sign}
+ * - {@link https://unicode-table.com/en/003F| ? Question Mark}
+ * - {@link https://unicode-table.com/en/2014| — Em dash}
+ * - {@link https://unicode-table.com/en/2264| ≤ Less-Than or Equal To}
+ * - {@link https://unicode-table.com/en/2265| ≥ Greater-Than or Equal To}
+ * - {@link https://unicode-table.com/en/201C| “ Left Double Quotation Mark}
+ * - {@link https://unicode-table.com/en/201D| ” Right Double Quotation Mark}
  *
  * @param {Object} obj The object you wish to check for invalid characters
+ *
  * @returns {boolean} `true` if the object contains an invalid character, else `false`
  */
 const invalidCharacters = obj => {
@@ -27,7 +36,7 @@ const InvalidCharactersPlugin = {
       }
 
       if (invalidCharacters(request.payload)) {
-        throw Boom.badData('We cannot accept any request that contains the following characters: ? £ ≤ ≥ “ ”')
+        throw Boom.badData('We cannot accept any request that contains the following characters: ? £ — ≤ ≥ “ ”')
       }
 
       return h.continue
