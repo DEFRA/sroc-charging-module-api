c94c635cdd6bd42111b3255febd30ccc3b42f81e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-07-30 10:11:45 +0100

    Reject requests with inverted quotation marks (#492)
    https://eaflood.atlassian.net/browse/CMEA-11
    
    During initial business acceptance testing of the WRLS system and its integration with the CM API found a transaction file that was rejected by SOP.
    
    This was tracked down to the line description containing 'inverted quotation marks'. It wasn't possible to get an exact example. But based on the description (and Google!) we have interpreted this as
    
    - **“** [Left Double Quotation Mark](https://unicode-table.com/en/201C/) `U+201C`
    - **”** [Right Double Quotation Mark](https://unicode-table.com/en/201D/) `U+201D`
    
    This change adds these to our `InvalidCharactersPlugin` which will mean any `POST` request that contains them anywhere in its content will be rejected.
    
    **Notes
    
    We've not added tests for _every_ invalid character before because we are confident the logic is the same for each. But to confirm our change we did add a test just to prove it's covering the new invalid characters.
    
    We also spotted the comments and error message didn't actually match what was being checked so we tidied those up at the same time.

--- app/plugins/invalid_characters.plugin.js
+++ app/plugins/invalid_characters.plugin.js
@@ -3,15 +3,24 @@
 const Boom = require('@hapi/boom')
 
 /**
- * Converts an object to a JSON string and then tests if it contains any of the following characters: ? £ ≤ ≥
+ * Converts an object to a JSON string and then tests if it contains any of the following characters:
+ *
+ * - {@link https://unicode-table.com/en/00A3| £ Pound Sign}
+ * - {@link https://unicode-table.com/en/003F| ? Question Mark}
+ * - {@link https://unicode-table.com/en/2014| — Em dash}
+ * - {@link https://unicode-table.com/en/2264| ≤ Less-Than or Equal To}
+ * - {@link https://unicode-table.com/en/2265| ≥ Greater-Than or Equal To}
+ * - {@link https://unicode-table.com/en/201C| “ Left Double Quotation Mark}
+ * - {@link https://unicode-table.com/en/201D| ” Right Double Quotation Mark}
  *
  * @param {Object} obj The object you wish to check for invalid characters
+ *
  * @returns {boolean} `true` if the object contains an invalid character, else `false`
  */
 const invalidCharacters = obj => {
   const jsonObj = JSON.stringify(obj)
 
-  if (jsonObj.match(/[?£\u2014\u2264\u2265]/)) {
+  if (jsonObj.match(/[?£\u2014\u2264\u2265\u201C\u201D]/)) {
     return true
   }
 
@@ -27,7 +36,7 @@ const InvalidCharactersPlugin = {
       }
 
       if (invalidCharacters(request.payload)) {
-        throw Boom.badData('We cannot accept any request that contains the following characters: ? £ ≤ ≥')
+        throw Boom.badData('We cannot accept any request that contains the following characters: ? £ — ≤ ≥ “ ”')
       }
 
       return h.continue
