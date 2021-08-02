95cd84005c046a891cf1cd306c236c78a8a8e9b2
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-08-02 14:20:05 +0100

    Refactor `NewInvoiceHelper` tests

--- test/support/test/helpers/new_invoice.helper.test.js
+++ test/support/test/helpers/new_invoice.helper.test.js
@@ -9,48 +9,37 @@ const { expect } = Code
 
 // Test helpers
 const { DatabaseHelper } = require('../../helpers')
+const { BillRunModel } = require('../../../../app/models')
 
 // Thing under test
 const { NewInvoiceHelper } = require('../../helpers')
-const { BillRunModel } = require('../../../../app/models')
 
 describe('New Invoice helper', () => {
+  let invoice
+
   beforeEach(async () => {
     await DatabaseHelper.clean()
+
+    invoice = await NewInvoiceHelper.add(null, {
+      debitLineCount: 5,
+      subjectToMinimumChargeDebitValue: 5000
+    })
   })
 
   describe('#add method', () => {
-    let invoice
-
-    beforeEach(async () => {
-      invoice = await NewInvoiceHelper.add(null, {
-        debitLineCount: 5,
-        subjectToMinimumChargeDebitValue: 5000
-      })
-    })
-
     it('updates the parent bill run', async () => {
-      const billRun = await BillRunModel.query().findById(invoice.billRunId)
+      const result = await BillRunModel.query().findById(invoice.billRunId)
 
-      expect(billRun.debitLineCount).to.equal(5)
-      expect(billRun.subjectToMinimumChargeDebitValue).to.equal(5000)
+      expect(result.debitLineCount).to.equal(5)
+      expect(result.subjectToMinimumChargeDebitValue).to.equal(5000)
     })
   })
 
   describe('#update method', () => {
-    let invoice
-
-    beforeEach(async () => {
-      invoice = await NewInvoiceHelper.add(null, {
-        debitLineCount: 1,
-        subjectToMinimumChargeDebitValue: 1000
-      })
-    })
-
     it('adds supplied values to the existing invoice', async () => {
       const result = await NewInvoiceHelper.update(invoice, {
-        debitLineCount: 5,
-        subjectToMinimumChargeDebitValue: 5000
+        debitLineCount: 1,
+        subjectToMinimumChargeDebitValue: 1000
       })
 
       expect(result.debitLineCount).to.equal(6)
