c94c947bcc6e0dff4034098faa18ec8c9b3763fc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-08-11 14:51:16 +0100

    Add new task and test
    Everything is setup and ready to go but nothing will work until the actual service PR is finished and merged.

---
+++ test/tasks/data_export.task.test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Thing under test
+const { DataExportTask } = require('../../app/tasks')
+
+// Things we need to stub
+const { ReportingExportService } = require('../../app/services')
+
+describe('Customer Files Task', () => {
+  let notifierFake
+  let serviceStub
+
+  beforeEach(async () => {
+    notifierFake = { omg: Sinon.fake(), omfg: Sinon.fake() }
+
+    serviceStub = Sinon.stub(ReportingExportService, 'go')
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('When the task is run', () => {
+    it("calls the 'ReportingExportService'", async () => {
+      await DataExportTask.go(notifierFake)
+
+      expect(serviceStub.calledOnce).to.be.true()
+    })
+  })
+})
