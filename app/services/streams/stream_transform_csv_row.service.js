a0adb81ddafd04e7bbcbeed3090398be0a05b819
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-09-07 16:01:36 +0100

    Update app/services/streams/stream_transform_csv_row.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/streams/stream_transform_csv_row.service.js
+++ app/services/streams/stream_transform_csv_row.service.js
@@ -8,8 +8,8 @@ const { Transform } = require('stream')
 const ConvertToCSVService = require('../convert_to_csv.service')
 
 /**
- * Returns a Transform stream which turns an incoming array into a CSV row, ie. comma-separated, with each element
- * wrapped in double quotes, and ending with a newline.
+ * Returns a Transform stream which turns an incoming array into a CSV row, ie. comma-separated, with text elements
+ * wrapped in double-quotes, any quote chars escaped and ending with a newline.
  *
  * @returns {TransformStream} A stream of data.
  */
