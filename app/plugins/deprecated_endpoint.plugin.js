79f979116a3dd299a63ea4c00016c20aa98ce7da
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-09-09 17:41:23 +0100

    Document plugin and service

--- app/plugins/deprecated_endpoint.plugin.js
+++ app/plugins/deprecated_endpoint.plugin.js
@@ -7,7 +7,35 @@
 const { DeprecatedEndpointService } = require('../services')
 
 /**
+ * As part of our versioning strategy, we need to be able to mark endpoints as being deprecated:
+ * https://blog.stoplight.io/deprecating-api-endpoints
  *
+ * We do this by adding the deprecation info to the route inside `options.app` (the recommended place to store our
+ * service's options), eg:
+ *
+ *   const routes = [
+ *     {
+ *       method: 'GET',
+ *       path: '/deprecated',
+ *       handler: Controller.handler,
+ *       options: {
+ *         app: {
+ *           deprecated: {
+ *             succeeded: true,
+ *             successor: '/not-deprecated'
+ *           }
+ *         }
+ *       }
+ *     }
+ *   ]
+ *
+ * This plugin passes the route to DeprecatedEndpointService, which will determine whether the route is deprecated and
+ * if so, returns the required headers to be added to the response. As defined in the link above, we set the following
+ * headers:
+ *
+ * - deprecation: This is `true` if the endpoint is deprecated, or will not be added if it isn't deprecated.
+ * - link: This is a link in the form `</not-deprecated>; rel="successor-version"` if the endpoint is marked as
+ *   succeeded and a successor endpoint is defined; otherwise, it is not added.
  */
 
 const addHeaders = (response, headers) => {
--- app/services/plugins/deprecated_endpoint.service.js
+++ app/services/plugins/deprecated_endpoint.service.js
@@ -7,10 +7,15 @@
 
 class DeprecatedEndpointService {
   /**
+   * Receives a route, checks to see if it has been deprecated (based on the options added to
+   * `route.options.app.deprecated` in the route file), and if so returns an object containing headers to be added to
+   * the request:
+   * - If the `deprecated` object exists in `route.options.app` then `headers.deprecation` will be set to `true`.
+   * - If `deprecated.succeeded` is `true` and `deprecated.successor` exists then `headers.link` will contain a link
+   *   to the succeeding endpoint.
    *
-   *
-   * @param {*} route
-   * @returns
+   * @param {object} route The route object to be checked.
+   * @returns {object} Headers to be added to the request.
    */
   static go (route) {
     const headers = {}
