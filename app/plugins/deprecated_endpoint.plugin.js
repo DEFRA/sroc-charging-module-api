1f76b62c75857b26fefec8e7647fd7801141d50e
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-09-09 17:24:16 +0100

    Develop `DeprecatedEndpointPlugin`

--- app/plugins/deprecated_endpoint.plugin.js
+++ app/plugins/deprecated_endpoint.plugin.js
@@ -4,15 +4,27 @@
  * @module DeprecatedEndpointPlugin
  */
 
+const { DeprecatedEndpointService } = require('../services')
+
 /**
  *
  */
+
+const addHeaders = (response, headers) => {
+  for (const header in headers) {
+    response.header(header, headers[header])
+  }
+}
 
 const DeprecatedEndpointPlugin = {
   name: 'deprecated_endpoint',
   register: (server, _options) => {
     server.ext('onPreResponse', (request, _h) => {
-      const response = request.response
+      const { route, response } = request
+
+      const headers = DeprecatedEndpointService.go(route)
+      addHeaders(response, headers)
+
       return response
     })
   }
---
+++ test/features/deprecated_endpoint.test.js
@@ -0,0 +1,85 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// For running our service
+const { init } = require('../../app/server')
+
+// Test helpers
+const { RouteHelper } = require('../support/helpers')
+
+const requestOptions = {
+  method: 'GET',
+  url: '/test/public'
+}
+
+const appDeprecatedOptions = options => {
+  return {
+    app: {
+      deprecated: options
+    }
+  }
+}
+
+describe('Add deprecated endpoint info', () => {
+  let server
+
+  // Create server before each test
+  beforeEach(async () => {
+    server = await init()
+  })
+
+  describe('When a request is made', () => {
+    describe('if the endpoint is marked as deprecated', () => {
+      describe('and the endpoint has a successor', () => {
+        const deprecated = appDeprecatedOptions({
+          succeeded: true,
+          successor: '/replacement-path'
+        })
+
+        it('sets the `deprecated` and `link` headers', async () => {
+          await RouteHelper.addPublicRoute(server, deprecated)
+
+          const response = await server.inject(requestOptions)
+
+          expect(response.statusCode).to.equal(200)
+          expect(response.headers.deprecation).to.be.true()
+          expect(response.headers.link).to.equal('</replacement-path>; rel="successor-version"')
+        })
+      })
+
+      describe('and the endpoint does not have a successor', () => {
+        const deprecated = appDeprecatedOptions({
+          succeeded: false
+        })
+
+        it('sets the `deprecated` header only', async () => {
+          await RouteHelper.addPublicRoute(server, deprecated)
+
+          const response = await server.inject(requestOptions)
+
+          expect(response.statusCode).to.equal(200)
+          expect(response.headers.deprecation).to.be.true()
+          expect(response.headers.link).to.not.exist()
+        })
+      })
+    })
+
+    describe('if the endpoint is not marked as deprecated', () => {
+      it('sets neither the `deprecation` nor the `link` header', async () => {
+        await RouteHelper.addPublicRoute(server)
+
+        const response = await server.inject(requestOptions)
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.headers.deprecation).to.not.exist()
+        expect(response.headers.link).to.not.exist()
+      })
+    })
+  })
+})
