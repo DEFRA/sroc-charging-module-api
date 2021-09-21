0c2d879f8bc8498ad26a406f965ee89fb06d8d26
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-09-21 13:02:40 +0100

    Initial implementation

--- app/services/files/customers/list_customer_files.service.js
+++ app/services/files/customers/list_customer_files.service.js
@@ -4,9 +4,45 @@
  * @module ListCustomerFilesService
  */
 
+const { CustomerFileModel } = require('../../../models')
+// NOTE: REPLACE THIS WITH BETTER PRESENTER
+const { JsonPresenter } = require('../../../presenters')
+
 class ListCustomerFilesService {
-  static async go () {
-    return true
+  static async go (regime, days = 30) {
+    const customerFiles = await this._customerFiles(regime.id, days)
+
+    return this._response(customerFiles)
+  }
+
+  static _customerFiles (regimeId, days) {
+    const startDate = this._startDate(days)
+
+    return CustomerFileModel
+      .query()
+      .where('regimeId', regimeId)
+      // do we want a modifier here?
+      .where('status', 'exported')
+      .where('exportedAt', '>', startDate)
+  }
+
+  /**
+   * Returns a date object of a given number of days ago, set to midnight (ie. the start of the day)
+   */
+  static _startDate (days) {
+    const date = new Date()
+
+    // Set time to midnight and date to `days` days ago
+    date.setHours(0, 0, 0, 0)
+    date.setDate(date.getDate() - days)
+
+    return date
+  }
+
+  static _response (customerFiles) {
+    const presenter = new JsonPresenter(customerFiles)
+
+    return presenter.go()
   }
 }
 
