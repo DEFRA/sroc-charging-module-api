72c07d52f495aa7aa90c21b97fcb80f420585677
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-09-21 17:16:14 +0100

    Remove service from controller
    The controller test fails now that we have fully developed the service. Rather than continuing to develop the controller in this branch, we temporarily remove the service from the controller.

--- app/controllers/customer_files.controller.js
+++ app/controllers/customer_files.controller.js
@@ -1,12 +1,8 @@
 'use strict'
 
-const { ListCustomerFilesService } = require('../services')
-
 class CustomerFilesController {
   static async index (req, h) {
-    const result = await ListCustomerFilesService.go()
-
-    return h.response(result).code(200)
+    return h.response().code(204)
   }
 }
 
