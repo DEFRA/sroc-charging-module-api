ebf682b8f1defd4284c5c417379d83d33fbbf2e2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-09-23 00:48:03 +0100

    Update presenter test

--- test/presenters/list_customer_files.presenter.test.js
+++ test/presenters/list_customer_files.presenter.test.js
@@ -7,6 +7,9 @@ const Code = require('@hapi/code')
 const { describe, it } = exports.lab = Lab.script()
 const { expect } = Code
 
+// Test helpers
+const { GeneralHelper } = require('../support/helpers')
+
 // Thing under test
 const { ListCustomerFilesPresenter } = require('../../app/presenters')
 
@@ -14,19 +17,29 @@ describe('List Customer Files presenter', () => {
   it('correctly presents the data', () => {
     const data = [
       {
+        id: GeneralHelper.uuid4(),
         fileReference: 'FILE1',
+        status: 'initialised',
+        exportedAt: null,
         exportedCustomers: [
           { customerReference: 'CUST11' }, { customerReference: 'CUST12' }, { customerReference: 'CUST13' }
         ]
       },
       {
+        id: GeneralHelper.uuid4(),
         fileReference: 'FILE2',
+        status: 'pending',
+        exportedAt: null,
         exportedCustomers: [
           { customerReference: 'CUST21' }, { customerReference: 'CUST22' }, { customerReference: 'CUST23' }
         ]
       },
       {
+        id: GeneralHelper.uuid4(),
+
         fileReference: 'FILE3',
+        status: 'exported',
+        exportedAt: '2021-01-12T14:41:10.511Z',
         exportedCustomers: [
           { customerReference: 'CUST31' }, { customerReference: 'CUST32' }, { customerReference: 'CUST33' }
         ]
@@ -38,15 +51,24 @@ describe('List Customer Files presenter', () => {
 
     expect(result).to.have.length(3)
     expect(result[0]).to.equal({
+      id: data[0].id,
       fileReference: 'FILE1',
+      status: data[0].status,
+      exportedAt: data[0].exportedAt,
       exportedCustomers: ['CUST11', 'CUST12', 'CUST13']
     })
     expect(result[1]).to.equal({
+      id: data[1].id,
       fileReference: 'FILE2',
+      status: data[1].status,
+      exportedAt: data[1].exportedAt,
       exportedCustomers: ['CUST21', 'CUST22', 'CUST23']
     })
     expect(result[2]).to.equal({
+      id: data[2].id,
       fileReference: 'FILE3',
+      status: data[2].status,
+      exportedAt: data[2].exportedAt,
       exportedCustomers: ['CUST31', 'CUST32', 'CUST33']
     })
   })
