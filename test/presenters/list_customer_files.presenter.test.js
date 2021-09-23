5f5a53d3b4343d37412e549315781b3960548e5e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-09-23 11:10:39 +0100

    Use data object consistently
    Good shout from @sadair to be consistent!

--- test/presenters/list_customer_files.presenter.test.js
+++ test/presenters/list_customer_files.presenter.test.js
@@ -52,21 +52,21 @@ describe('List Customer Files presenter', () => {
     expect(result).to.have.length(3)
     expect(result[0]).to.equal({
       id: data[0].id,
-      fileReference: 'FILE1',
+      fileReference: data[0].fileReference,
       status: data[0].status,
       exportedAt: data[0].exportedAt,
       exportedCustomers: ['CUST11', 'CUST12', 'CUST13']
     })
     expect(result[1]).to.equal({
       id: data[1].id,
-      fileReference: 'FILE2',
+      fileReference: data[1].fileReference,
       status: data[1].status,
       exportedAt: data[1].exportedAt,
       exportedCustomers: ['CUST21', 'CUST22', 'CUST23']
     })
     expect(result[2]).to.equal({
       id: data[2].id,
-      fileReference: 'FILE3',
+      fileReference: data[2].fileReference,
       status: data[2].status,
       exportedAt: data[2].exportedAt,
       exportedCustomers: ['CUST31', 'CUST32', 'CUST33']
