3d29cb1b1d486b800b57531c6002719b6a7be526
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-10-04 12:12:24 +0100

    Initial pass at covering all invoice changes
    We started this work fixing cases where an invoice that is overall a credit or debit becomes zero value following deletion of licence. Further investigation revealed that all cases where an invoice changes 'type' -- for example, credit to debit and vice versa, zero value to credit/debit, etc. -- were being calculated incorrectly at the bill run level. In order to resolve this, we start by creating unit tests to cover all situations. We then develop a naive solution which checks every combination of invoice change and acts accordingly, which we will refactor into something more elegant.

--- app/services/licences/delete_licence.service.js
+++ app/services/licences/delete_licence.service.js
@@ -32,7 +32,7 @@ class DeleteLicenceService {
         const { status: initialBillRunStatus } = billRun
 
         await this._setBillRunStatusPending(billRun, trx)
-        await this._deleteLicence(licence, notifier, trx)
+        await this._deleteLicence(licence, notifier, initialBillRunStatus, trx)
         await this._revertBillRunStatus(billRun, initialBillRunStatus, trx)
       })
     } catch (error) {
@@ -63,7 +63,7 @@ class DeleteLicenceService {
     return status
   }
 
-  static async _deleteLicence (licence, notifier, trx) {
+  static async _deleteLicence (licence, notifier, initialBillRunStatus, trx) {
     // We only need to delete the licence as it will cascade down to the transaction level.
     await LicenceModel
       .query(trx)
@@ -76,9 +76,10 @@ class DeleteLicenceService {
 
     if (licences.length) {
       // Store the invoice's current transaction type (C=Credit, I=Invoice) as we may need it to adjust the bill run
-      const invoiceTransactionType = invoice.$transactionType()
+      const invoiceTransactionType = this._transactionType(invoice)
+      const invoiceNetTotal = invoice.$netTotal()
       await this._handleInvoice(billRun, invoice, licence, trx)
-      await this._handleBillRun(billRun, invoice, licence, invoiceTransactionType, trx)
+      await this._handleBillRun(billRun, invoice, licence, invoiceTransactionType, invoiceNetTotal, initialBillRunStatus, trx)
     } else {
       await DeleteInvoiceService.go(invoice, billRun, notifier, trx)
     }
@@ -99,8 +100,8 @@ class DeleteLicenceService {
    * Updates the bill run instance that the licence belongs to, then uses the updated fields to generate a patch which
    * is applied to the bill run in the db.
    */
-  static async _handleBillRun (billRun, invoice, licence, invoiceTransactionType, trx) {
-    this._updateBillRunInstance(billRun, invoice, licence, invoiceTransactionType)
+  static async _handleBillRun (billRun, invoice, licence, invoiceTransactionType, previousNetTotal, initialBillRunStatus, trx) {
+    this._updateBillRunInstance(billRun, invoice, licence, invoiceTransactionType, previousNetTotal, initialBillRunStatus)
     const billRunPatch = this._billRunPatch(billRun)
     await billRun.$query(trx).patch(billRunPatch)
   }
@@ -205,29 +206,83 @@ class DeleteLicenceService {
    * additional fields unique to the bill run, and finally adjusts the credit note/invoice count if the invoice that
    * the licence belongs to is now zero value.
    */
-  static _updateBillRunInstance (billRun, invoice, licence, invoiceTransactionType) {
+  static _updateBillRunInstance (billRun, invoice, licence, previousTransactionType, previousInvoiceNetTotal, initialBillRunStatus) {
     this._updateInstance(billRun, licence)
-    billRun.creditNoteValue -= licence.creditLineValue
-    billRun.invoiceValue -= licence.debitLineValue
-    this._handleZeroValueInvoice(billRun, invoice, invoiceTransactionType)
-  }
 
-  /**
-   * If the invoice is now zero value then we need to remove it from the bill run-level counts. We use the transaction
-   * type that we retrieved prior to adjusting the invoice to determine which count (invoice/credit note) we decrement.
-   */
-  static _handleZeroValueInvoice (billRun, invoice, invoiceTransactionType) {
-    if (!invoice.$zeroValueInvoice()) {
+    // We only update the bill run stats if the bill run is generated
+    if (initialBillRunStatus !== 'generated') {
       return
     }
 
-    if (invoiceTransactionType === 'C') {
+    const currentLicenceNetTotal = licence.$netTotal()
+    const currentInvoiceNetTotal = invoice.$netTotal()
+    const currentTransactionType = this._transactionType(invoice)
+
+    /**
+     * if the updated invoice was a credit note and is still a credit note then subtract the licence net total from billRun.creditNoteValue
+     * if the updated invoice was an invoice and still is an invoice then subtract the licence net total from billRun.invoiceValue
+     * if the updated invoice was a credit note but is now an invoice then subtract net total from creditNoteValue and add to invoiceValue
+     *  and subtract 1 from creditNoteCount and increment invoiceCount
+     * if the updated invoice was an invoice but is now a credit note then subtract net total from invoiceValue and add to creditNoteValue
+     *  and subtract 1 from invoiceCount and increment creditNoteCount
+     * if the updated invoice was a credit note but is now zero value then subtract net total from creditNoteValue
+     *  and subtract 1 from creditNoteCount
+     * if the updated invoice was an invoice but is now zero value then subtract net total from invoiceValue
+     *  and subtract 1 from invoiceCount
+     */
+
+    if (previousTransactionType === 'C' && currentTransactionType === 'C') {
+      billRun.creditNoteValue += currentLicenceNetTotal
+    }
+
+    if (previousTransactionType === 'C' && currentTransactionType === 'I') {
+      billRun.creditNoteValue += previousInvoiceNetTotal
+      billRun.invoiceValue += currentInvoiceNetTotal
+      billRun.creditNoteCount -= 1
+      billRun.invoiceCount += 1
+    }
+
+    if (previousTransactionType === 'C' && currentTransactionType === 'Z') {
+      billRun.creditNoteValue -= previousInvoiceNetTotal
       billRun.creditNoteCount -= 1
     }
 
-    if (invoiceTransactionType === 'I') {
+    if (previousTransactionType === 'I' && currentTransactionType === 'I') {
+      billRun.invoiceValue -= currentLicenceNetTotal
+    }
+
+    if (previousTransactionType === 'I' && currentTransactionType === 'C') {
+      billRun.invoiceValue -= previousInvoiceNetTotal
+      billRun.creditNoteValue -= currentInvoiceNetTotal
+      billRun.invoiceCount -= 1
+      billRun.creditNoteCount += 1
+    }
+
+    if (previousTransactionType === 'I' && currentTransactionType === 'Z') {
+      billRun.invoiceValue -= previousInvoiceNetTotal
       billRun.invoiceCount -= 1
     }
+
+    if (previousTransactionType === 'Z' && currentTransactionType === 'Z') {
+      // Nothing needed
+    }
+
+    if (previousTransactionType === 'Z' && currentTransactionType === 'I') {
+      billRun.invoiceValue += currentInvoiceNetTotal
+      billRun.invoiceCount += 1
+    }
+
+    if (previousTransactionType === 'Z' && currentTransactionType === 'C') {
+      billRun.creditNoteCount += 1
+      billRun.creditNoteValue -= currentInvoiceNetTotal
+    }
+  }
+
+  /**
+   * Returns 'C' if the invoice is a credit, 'I' if it is a debit, and 'Z' if it is zero value
+   */
+  static _transactionType (invoice) {
+    return invoice.$zeroValueInvoice() ? 'Z' : invoice.$transactionType()
   }
 }
 
