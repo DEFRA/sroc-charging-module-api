5abe130fa5b90eba4e32117b7edc77297547f4c1
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-10-15 11:08:47 +0100

    Remove migration default
    We created the `ruleset` migration to set the default value to `sroc`. However now that we've developed the rest of the v3 endpoint we realise that we're setting the default ruleset elsewhere so it isn't needed in the migration -- we can simply set it to `notNullable` instead.

--- db/migrations/20211014095056_alter_bill_runs.js
+++ db/migrations/20211014095056_alter_bill_runs.js
@@ -7,9 +7,10 @@ exports.up = async function (knex) {
     .schema
     .alterTable(tableName, table => {
       // We want to create a ruleset column with the value `presroc` for all existing entries in the table. However in
-      // future we want new entries to default to `sroc`. We therefore set the default twice.
+      // future we simply want this column to be not nullable. We therefore set the default, then alter it straight away
+      // (which, as we aren't stating a default, will remove it).
       table.string('ruleset').defaultTo('presroc')
-      table.string('ruleset').alter().defaultTo('sroc')
+      table.string('ruleset').alter().notNullable()
     })
 }
 
