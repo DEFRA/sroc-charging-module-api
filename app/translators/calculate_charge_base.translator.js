c2a2cc958705c9d470cc51ea699d6450cc611ea0
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2021-12-01 16:05:44 +0000

    Refactor Calculate Charge translators

---
+++ app/translators/calculate_charge_base.translator.js
@@ -0,0 +1,77 @@
+'use strict'
+
+/**
+ * @module CalculateChargePresrocTranslator
+ */
+
+const BaseTranslator = require('./base.translator')
+const Joi = require('joi').extend(require('@joi/date'))
+const Boom = require('@hapi/boom')
+
+class CalculateChargeBaseTranslator extends BaseTranslator {
+  _schema () {
+    const rules = this._rules()
+
+    return Joi.object(rules)
+  }
+
+  _baseRules () {
+    return {
+      authorisedDays: Joi.number().integer().min(0).max(366).required(),
+      billableDays: Joi.number().integer().min(0).max(366).required(),
+      compensationCharge: Joi.boolean().required(),
+      credit: Joi.boolean().required(),
+      periodEnd: Joi.date().format(this._validDateFormats()).required(),
+      section130Agreement: Joi.boolean().required(),
+
+      // Dependent on `compensationCharge`
+      waterUndertaker: Joi.boolean()
+        .when('compensationCharge', { is: true, then: Joi.required() }),
+
+      // Needed to determine which endpoints to call in the rules service
+      regime: Joi.string().required()
+    }
+  }
+
+  _validDateFormats () {
+    return ['DD-MMM-YYYY', 'DD-MM-YYYY', 'YYYY-MM-DD', 'DD/MM/YYYY', 'YYYY/MM/DD']
+  }
+
+  _validateFinancialYear () {
+    const schema = Joi.object({
+      periodEndFinancialYear: Joi.number().equal(this.chargeFinancialYear)
+    })
+
+    const data = {
+      periodEndFinancialYear: this._financialYear(this.chargePeriodEnd)
+    }
+
+    const { error } = schema.validate(data)
+
+    if (error) {
+      throw Boom.badData(error)
+    }
+  }
+
+  /**
+   * Returns the calculated financial year for a given date
+   *
+   * If the date is January to March then the financial year is the previous year. Otherwise, the financial year is the
+   * current year.
+   *
+   * For example, if the date is 01-MAR-2022 then the financial year will be 2021. If the it's 01-MAY-2022 then the
+   * financial year will be 2022.
+   *
+   * @param {String} date
+   * @returns {Number} The calculated financial year
+  */
+  _financialYear (date) {
+    const periodDate = new Date(date)
+    const month = periodDate.getMonth()
+    const year = periodDate.getFullYear()
+
+    return (month <= 2 ? year - 1 : year)
+  }
+}
+
+module.exports = CalculateChargeBaseTranslator
--- app/translators/calculate_charge_sroc.translator.js
+++ app/translators/calculate_charge_sroc.translator.js
@@ -4,11 +4,10 @@
  * @module CalculateChargeSrocTranslator
  */
 
-const BaseTranslator = require('./base.translator')
+const CalculateChargeBaseTranslator = require('./calculate_charge_base.translator')
 const Joi = require('joi').extend(require('@joi/date'))
-const Boom = require('@hapi/boom')
 
-class CalculateChargeSrocTranslator extends BaseTranslator {
+class CalculateChargeSrocTranslator extends CalculateChargeBaseTranslator {
   constructor (data) {
     super(data)
 
@@ -18,79 +17,23 @@ class CalculateChargeSrocTranslator extends BaseTranslator {
     this._validateFinancialYear()
   }
 
-  _validateFinancialYear () {
-    const schema = Joi.object({
-      periodEndFinancialYear: Joi.number().equal(this.chargeFinancialYear)
-    })
-
-    const data = {
-      periodEndFinancialYear: this._financialYear(this.chargePeriodEnd)
-    }
-
-    const { error } = schema.validate(data)
-
-    if (error) {
-      throw Boom.badData(error)
-    }
-  }
-
-  _schema () {
-    const rules = this._rules()
-
-    return Joi.object({
-      abatementFactor: rules.abatementFactor,
-      actualVolume: rules.actualVolume,
-      authorisedVolume: rules.authorisedVolume,
-      aggregateProportion: rules.aggregateProportion,
-      authorisedDays: rules.authorisedDays,
-      billableDays: rules.billableDays,
-      chargeCategoryCode: rules.chargeCategoryCode,
-      compensationCharge: rules.compensationCharge,
-      credit: rules.credit,
-      // Note that financialYear is added in constructor()
-      loss: rules.loss,
-      periodEnd: rules.periodEnd,
-      periodStart: rules.periodStart,
-      regime: rules.regime,
-      regionalChargingArea: rules.regionalChargingArea,
-      ruleset: rules.ruleset,
-      section127Agreement: rules.section127Agreement,
-      section130Agreement: rules.section130Agreement,
-      supportedSource: rules.supportedSource,
-      supportedSourceName: rules.supportedSourceName,
-      twoPartTariff: rules.twoPartTariff,
-      waterCompanyCharge: rules.waterCompanyCharge,
-      waterUndertaker: rules.waterUndertaker,
-      winterOnly: rules.winterOnly
-    })
-  }
-
   _rules () {
-    const validDateFormats = ['DD-MMM-YYYY', 'DD-MM-YYYY', 'YYYY-MM-DD', 'DD/MM/YYYY', 'YYYY/MM/DD']
-
     return {
+      ...this._baseRules(),
       ruleset: Joi.string().valid('sroc').required(),
 
       abatementFactor: Joi.number().allow(null).empty(null).default(1.0),
       aggregateProportion: Joi.number().allow(null).empty(null).default(1.0),
-      authorisedDays: Joi.number().integer().min(0).max(366).required(),
-      billableDays: Joi.number().integer().min(0).max(366).required(),
-      credit: Joi.boolean().required(),
       // Case-insensitive validation matches and returns the correctly-capitalised string
       loss: Joi.string().valid(...this._validLosses()).insensitive().required(),
-      periodEnd: Joi.date().format(validDateFormats).required(),
-      periodStart: Joi.date().format(validDateFormats).min('01-APR-2021').max(Joi.ref('periodEnd')).required(),
-      section130Agreement: Joi.boolean().required(),
+      periodStart: Joi.date().format(this._validDateFormats()).min('01-APR-2021').max(Joi.ref('periodEnd')).required(),
       authorisedVolume: Joi.number().greater(0).required(),
       waterCompanyCharge: Joi.boolean().required(),
       winterOnly: Joi.boolean().required(),
 
       // Dependent on `compensationCharge`
-      compensationCharge: Joi.boolean().required(),
       // Case-insensitive validation matches and returns the correctly-capitalised string
       regionalChargingArea: Joi.string().valid(...this._validRegionalChargingAreas()).insensitive()
-        .when('compensationCharge', { is: true, then: Joi.required() }),
-      waterUndertaker: Joi.boolean()
         .when('compensationCharge', { is: true, then: Joi.required() }),
 
       // Dependent on `twoPartTariff`
@@ -113,10 +56,7 @@ class CalculateChargeSrocTranslator extends BaseTranslator {
         .when('supportedSource', { is: false, then: Joi.forbidden().default('Not Applicable') }),
 
       // Validated by the rules service
-      chargeCategoryCode: Joi.string().required(),
-
-      // Needed to determine which endpoints to call in the rules service
-      regime: Joi.string().required()
+      chargeCategoryCode: Joi.string().required()
     }
   }
 
@@ -146,26 +86,6 @@ class CalculateChargeSrocTranslator extends BaseTranslator {
       waterUndertaker: 'regimeValue14',
       winterOnly: 'headerAttr8'
     }
-  }
-
-  /**
-   * Returns the calculated financial year for a given date
-   *
-   * If the date is January to March then the financial year is the previous year. Otherwise, the financial year is the
-   * current year.
-   *
-   * For example, if the date is 01-MAR-2022 then the financial year will be 2021. If the it's 01-MAY-2022 then the
-   * financial year will be 2022.
-   *
-   * @param {String} date
-   * @returns {Number} The calculated financial year
-  */
-  _financialYear (date) {
-    const periodDate = new Date(date)
-    const month = periodDate.getMonth()
-    const year = periodDate.getFullYear()
-
-    return (month <= 2 ? year - 1 : year)
   }
 
   _validLosses () {
