62ee43724e4ce5b4aba2e7c82b4656e4232e346d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2021-12-15 16:06:53 +0000

    Add v3 delete invoice route

--- app/routes/bill_run_invoice.routes.js
+++ app/routes/bill_run_invoice.routes.js
@@ -9,6 +9,11 @@ const routes = [
     handler: BillRunsInvoicesController.delete
   },
   {
+    method: 'DELETE',
+    path: '/v3/{regimeSlug}/bill-runs/{billRunId}/invoices/{invoiceId}',
+    handler: BillRunsInvoicesController.delete
+  },
+  {
     method: 'GET',
     path: '/v2/{regimeSlug}/bill-runs/{billRunId}/invoices/{invoiceId}',
     handler: BillRunsInvoicesController.view
