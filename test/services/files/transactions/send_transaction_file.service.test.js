bc8f99905dcb628503468ea19f0ae2acaa15de20
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-02-09 23:21:21 +0000

    Get ready to test generator selection
    `SendTransactionFileService` is now going to need to pick the appropriate GenerateTransactionFileService depending on the ruleset of the bill run.
    
    We get the tests ready for this by renaming the existing stub (to make it clearer). We don't think there is a need to repeat _all_ the tests for each ruleset. But we do need a dedicated unit tests for the selector.
    
    And though new bill runs created using the `/v3` endpoint will have `sroc` as the default ruleset currently our helper method creates them as `presroc` unless you express otherwise. This is why we have left all the tests using the presroc generate stub.

--- test/services/files/transactions/send_transaction_file.service.test.js
+++ test/services/files/transactions/send_transaction_file.service.test.js
@@ -17,7 +17,12 @@ const {
 } = require('../../../support/helpers')
 
 // Things we need to stub
-const { DeleteFileService, GeneratePresrocTransactionFileService, SendFileToS3Service } = require('../../../../app/services')
+const {
+  DeleteFileService,
+  GeneratePresrocTransactionFileService,
+  GenerateSrocTransactionFileService,
+  SendFileToS3Service
+} = require('../../../../app/services')
 
 // Thing under test
 const { SendTransactionFileService } = require('../../../../app/services')
@@ -26,7 +31,7 @@ describe('Send Transaction File service', () => {
   let regime
   let billRun
   let deleteStub
-  let generateStub
+  let generatePresrocStub
   let sendStub
   let notifierFake
 
@@ -37,7 +42,7 @@ describe('Send Transaction File service', () => {
     billRun = await BillRunHelper.addBillRun(regime.id, GeneralHelper.uuid4())
 
     deleteStub = Sinon.stub(DeleteFileService, 'go')
-    generateStub = Sinon.stub(GeneratePresrocTransactionFileService, 'go').returns('stubFilename')
+    generatePresrocStub = Sinon.stub(GeneratePresrocTransactionFileService, 'go').returns('stubPresrocFilename')
     sendStub = Sinon.stub(SendFileToS3Service, 'go')
 
     // Create a fake function to stand in place of Notifier.omfg()
@@ -61,15 +66,15 @@ describe('Send Transaction File service', () => {
       it('generates a transaction file', async () => {
         await SendTransactionFileService.go(regime, billRun)
 
-        expect(generateStub.calledOnce).to.be.true()
-        expect(generateStub.getCall(0).args[1]).to.equal(`${billRun.fileReference}.dat`)
+        expect(generatePresrocStub.calledOnce).to.be.true()
+        expect(generatePresrocStub.getCall(0).args[1]).to.equal(`${billRun.fileReference}.dat`)
       })
 
       it('sends the transaction file', async () => {
         await SendTransactionFileService.go(regime, billRun)
 
         expect(sendStub.calledOnce).to.be.true()
-        expect(sendStub.getCall(0).firstArg).to.equal('stubFilename')
+        expect(sendStub.getCall(0).firstArg).to.equal('stubPresrocFilename')
         expect(sendStub.getCall(0).args[1]).to.equal(`export/${regime.slug}/transaction/${billRun.fileReference}.dat`)
       })
 
@@ -92,7 +97,7 @@ describe('Send Transaction File service', () => {
       it("doesn't try to generate a transaction file", async () => {
         await SendTransactionFileService.go(regime, billRun)
 
-        expect(generateStub.notCalled).to.be.true()
+        expect(generatePresrocStub.notCalled).to.be.true()
       })
 
       it("doesn't try to send the transaction file", async () => {
