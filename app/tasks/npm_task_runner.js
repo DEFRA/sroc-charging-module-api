ad20ff55769ae2b572b59f88450730472054551f
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2022-03-14 17:18:54 +0000

    Fix linting error
    Our linter throws an error for `npm_task_runner.js` as we are defining constants which are not referred to within the file. They do need to be defined though as they are used when calling it via `npm start`. We therefore disable the linting rule for these lines.

--- app/tasks/npm_task_runner.js
+++ app/tasks/npm_task_runner.js
@@ -5,8 +5,16 @@
  * accepts the name of a task (defined below) and will pass that task to TaskRunner to be executed.
  */
 
+/**
+ * Note that we disable the no-unused-vars linting rule as these constants are not used within this file; they are
+ * instead used when calling it from `npm start`
+ */
+
+/* eslint-disable no-unused-vars */
 const CustomerFilesTask = require('./customer_files.task.js')
 const DataExportTask = require('./data_export.task.js')
+/* eslint-enable no-unused-vars */
+
 const TaskRunner = require('./task_runner.js')
 
 TaskRunner.go(process.argv[2])
